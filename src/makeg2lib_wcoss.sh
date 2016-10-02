32166544c2d7e00f2118270941b3a7f4bad88828
boi.vuong@noaa.gov <boi.vuong@noaa.gov>
2016-10-02 19:58:24 +0000

    initial setting for G2 v3.0.0

---
+++ src/makeg2lib_wcoss.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+export version=3.0.0
+
+mkdir -p ../include/g2_v${version}_4
+mkdir -p ../include/g2_v${version}_d
+
+make -f makefile_4_wcoss
+make -f makefile_d_wcoss
+
+#
+#  Build unit_test
+#
+echo " "
+echo " "
+
+cd ../unit_test
+mkdir -p ../exec
+make -f Makefile_wcoss
