d5a0f5d8724ce361f1db1c6eb2a3ce1eeeac755b
Hang.Lei <Hang.Lei@noaa.gov>
2021-07-16 22:00:34 +0000

    add doxygen into getgb2.f, Part of #55

--- src/getgb2.f
+++ src/getgb2.f
@@ -30,7 +30,7 @@ C>    - 1994-04-01 Mark Iredell
 C>    - 1995-10-31 Mark Iredell modularized portions of code into subprograms
 C>    and allowed for unspecified index file
 C>    - 2002-01-11 Stephen Gilbert modified from getgb and getgbm to work with grib2
-C>    - 2015-11-10 Boi Vuong modified doc block for gfld%ngrdpts and gfld%ndpts
+C>    - 2015-11-10 Boi Vuong modified doc block for gfld\%ngrdpts and gfld\%ndpts
 C>
 C>    @param[in] LUGB integer unit of the unblocked grib data file.
 C>    file must be opened with baopen or baopenr before calling
@@ -91,31 +91,31 @@ C>    - .FALSE. do not unpack bitmap and data values
 C>    @param[out] K integer field number unpacked
 C>    @param[out] GFLD derived type gribfield (defined in module grib_mod)
 C>    (NOTE: See Remarks Section)
-C>    - gfld%version GRIB edition number (currently 2)
-C>    - gfld%discipline Message Discipline (see Code Table 0.0)
-C>    - gfld%idsect Contains the entries in the Identification Section
+C>    - gfld\%version GRIB edition number (currently 2)
+C>    - gfld\%discipline Message Discipline (see Code Table 0.0)
+C>    - gfld\%idsect Contains the entries in the Identification Section
 C>    (Section 1) This element is actually a pointer to an array
 C>    that holds the data.
-C>    - gfld%idsect(1) Identification of originating Centre
+C>    - gfld\%idsect(1) Identification of originating Centre
 C>    (see Common Code Table C-1) 7 US National Weather Service
-C>    - gfld%idsect(2) Identification of originating Sub-centre
-C>    - gfld%idsect(3) GRIB Master Tables Version Number
+C>    - gfld\%idsect(2) Identification of originating Sub-centre
+C>    - gfld\%idsect(3) GRIB Master Tables Version Number
 C>    (see Code Table 1.0) 0 Experimental; 1 Initial operational version number
-C>    - gfld%idsect(4) GRIB Local Tables Version Number (see Code Table 1.1)
+C>    - gfld\%idsect(4) GRIB Local Tables Version Number (see Code Table 1.1)
 C>    0 Local tables not used; 1-254 Number of local tables version used
-C>    - gfld%idsect(5) Significance of Reference Time (Code Table 1.2)
+C>    - gfld\%idsect(5) Significance of Reference Time (Code Table 1.2)
 C>    0 Analysis; 1 Start of forecast; 2 Verifying time of forecast;
 C>    3 Observation time.
-C>    - gfld%idsect(6) Year (4 digits)
-C>    - gfld%idsect(7) Month
-C>    - gfld%idsect(8) Day
-C>    - gfld%idsect(9) Hour
-C>    - gfld%idsect(10) Minute
-C>    - gfld%idsect(11) Second
-C>    - gfld%idsect(12) Production status of processed data (see Code Table 1.3)
+C>    - gfld\%idsect(6) Year (4 digits)
+C>    - gfld\%idsect(7) Month
+C>    - gfld\%idsect(8) Day
+C>    - gfld\%idsect(9) Hour
+C>    - gfld\%idsect(10) Minute
+C>    - gfld\%idsect(11) Second
+C>    - gfld\%idsect(12) Production status of processed data (see Code Table 1.3)
 C>    0 Operational products; 1 Operational test products;
 C>    2 Research products; 3 Re-analysis products.
-C>    - gfld%idsect(13) Type of processed data (see Code Table 1.4)
+C>    - gfld\%idsect(13) Type of processed data (see Code Table 1.4)
 C>    0 Analysis products
 C>    1 Forecast products
 C>    2 Analysis and forecast products
@@ -124,84 +124,84 @@ C>    4 Perturbed forecast products
 C>    5 Control and perturbed forecast products
 C>    6 Processed satellite observations
 C>    7 Processed radar observations
-C>    - gfld%idsectlen Number of elements in gfld%idsect
-C>    - gfld%local Pointer to character array containing contents
+C>    - gfld\%idsectlen Number of elements in gfld\%idsect
+C>    - gfld\%local Pointer to character array containing contents
 C>    of Local Section 2, if included
-C>    - gfld%locallen length of array gfld%local
-C>    - gfld%ifldnum field number within GRIB message
-C>    - gfld%griddef Source of grid definition (see Code Table 3.0)
+C>    - gfld\%locallen length of array gfld\%local
+C>    - gfld\%ifldnum field number within GRIB message
+C>    - gfld\%griddef Source of grid definition (see Code Table 3.0)
 C>    0 Specified in Code table 3.1
 C>    1 Predetermined grid Defined by originating centre
-C>    - gfld%ngrdpts Number of grid points in the defined grid.
+C>    - gfld\%ngrdpts Number of grid points in the defined grid.
 C>    Note that the number of actual data values returned from getgb2
-C>    (in gfld%ndpts) may be less than this value if a logical bitmap
+C>    (in gfld\%ndpts) may be less than this value if a logical bitmap
 C>    is in use with grid points that are being masked out.
-C>    - gfld%numoct_opt Number of octets needed for each additional grid
+C>    - gfld\%numoct_opt Number of octets needed for each additional grid
 C>    points definition. Used to define number of points in each row (or column)
 C>    for non-regular grids. = 0, if using regular grid.
-C>    - gfld%interp_opt Interpretation of list for optional points definition.(Code Table 3.11)
-C>    - gfld%igdtnum Grid Definition Template Number (Code Table 3.1)
-C>    - gfld%igdtmpl Contains the data values for the specified Grid
-C>    Definition Template (NN=gfld%igdtnum). Each element of this integer array
+C>    - gfld\%interp_opt Interpretation of list for optional points definition.(Code Table 3.11)
+C>    - gfld\%igdtnum Grid Definition Template Number (Code Table 3.1)
+C>    - gfld\%igdtmpl Contains the data values for the specified Grid
+C>    Definition Template (NN=gfld\%igdtnum). Each element of this integer array
 C>    contains an entry (in the order specified) of Grid Defintion
 C>    Template 3.NN This element is actually a pointer to an array
 C>    that holds the data.
-C>    - gfld%igdtlen Number of elements in gfld%igdtmpl. i.e. number of
-C>    entries in Grid Defintion Template 3.NN (NN=gfld%igdtnum).
-C>    - gfld%list_opt (Used if gfld%numoct_opt .ne. 0) This array contains the
+C>    - gfld\%igdtlen Number of elements in gfld\%igdtmpl. i.e. number of
+C>    entries in Grid Defintion Template 3.NN (NN=gfld\%igdtnum).
+C>    - gfld\%list_opt (Used if gfld\%numoct_opt .ne. 0) This array contains the
 C>    number of grid points contained in each row (or column).
 C>    (part of Section 3) This element is actually a pointer to
 C>    an array that holds the data. This pointer is nullified
-C>    if gfld%numoct_opt=0.
-C>    - gfld%num_opt (Used if gfld%numoct_opt .ne. 0) The number of entries in
+C>    if gfld\%numoct_opt=0.
+C>    - gfld\%num_opt (Used if gfld\%numoct_opt .ne. 0) The number of entries in
 C>    array ideflist. i.e. number of rows (or columns) for which
 C>    optional grid points are defined. This value is set to zero,
-C>    if gfld%numoct_opt=0.
-C>    - gfdl%ipdtnum Product Definition Template Number (see Code Table 4.0)
-C>    - gfld%ipdtmpl Contains the data values for the specified Product Definition
-C>    Template (N=gfdl%ipdtnum). Each element of this integer array
+C>    if gfld\%numoct_opt=0.
+C>    - gfdl\%ipdtnum Product Definition Template Number (see Code Table 4.0)
+C>    - gfld\%ipdtmpl Contains the data values for the specified Product Definition
+C>    Template (N=gfdl\%ipdtnum). Each element of this integer array
 C>    contains an entry (in the order specified) of Product Defintion
 C>    Template 4.N. This element is actually a pointer to an array
 C>    that holds the data.
-C>    - gfld%ipdtlen Number of elements in gfld%ipdtmpl. i.e. number of
-C>    entries in Product Defintion Template 4.N (N=gfdl%ipdtnum).
-C>    - gfld%coord_list Real array containing floating point values
+C>    - gfld\%ipdtlen Number of elements in gfld\%ipdtmpl. i.e. number of
+C>    entries in Product Defintion Template 4.N (N=gfdl\%ipdtnum).
+C>    - gfld\%coord_list Real array containing floating point values
 C>    intended to document the vertical discretisation associated to
 C>    model data on hybrid coordinate vertical levels.(part of Section 4)
 C>    This element is actually a pointer to an array
 C>    that holds the data.
-C>    - gfld%num_coord number of values in array gfld%coord_list.
-C>    - gfld%ndpts Number of data points unpacked and returned.
+C>    - gfld\%num_coord number of values in array gfld\%coord_list.
+C>    - gfld\%ndpts Number of data points unpacked and returned.
 C>    Note that this number may be different from the value of
-C>    - gfld%ngrdpts if a logical bitmap is in use with grid points
+C>    - gfld\%ngrdpts if a logical bitmap is in use with grid points
 C>    that are being masked out.
-C>    - gfld%idrtnum Data Representation Template Number (see Code Table 5.0)
-C>    - gfld%idrtmpl Contains the data values for the specified Data
-C>    Representation Template (N=gfld%idrtnum). Each element of this
+C>    - gfld\%idrtnum Data Representation Template Number (see Code Table 5.0)
+C>    - gfld\%idrtmpl Contains the data values for the specified Data
+C>    Representation Template (N=gfld\%idrtnum). Each element of this
 C>    integer array contains an entry (in the order specified) of
 C>    Product Defintion Template 5.N. This element is actually a
 C>    pointer to an array that holds the data.
-C>    - gfld%idrtlen Number of elements in gfld%idrtmpl. i.e. number
-C>    of entries in Data Representation Template 5.N (N=gfld%idrtnum).
-C>    - gfld%unpacked logical value indicating whether the bitmap and
-C>    data values were unpacked. If false, gfld%bmap and gfld%fld
+C>    - gfld\%idrtlen Number of elements in gfld\%idrtmpl. i.e. number
+C>    of entries in Data Representation Template 5.N (N=gfld\%idrtnum).
+C>    - gfld\%unpacked logical value indicating whether the bitmap and
+C>    data values were unpacked. If false, gfld\%bmap and gfld\%fld
 C>    pointers are nullified.
-C>    - gfld%expanded Logical value indicating whether the data field
+C>    - gfld\%expanded Logical value indicating whether the data field
 C>    was expanded to the grid in the case where a bit-map is present.
-C>    If true, the data points in gfld%fld match the grid points and
+C>    If true, the data points in gfld\%fld match the grid points and
 C>    zeros were inserted at grid points where data was bit-mapped out.
-C>    If false, the data values in gfld%fld were not expanded to the
+C>    If false, the data values in gfld\%fld were not expanded to the
 C>    grid and are just a consecutive array of data points corresponding
-C>    to each value of "1" in gfld%bmap.
-C>    - gfld%ibmap Bitmap indicator (see Code Table 6.0)
+C>    to each value of "1" in gfld\%bmap.
+C>    - gfld\%ibmap Bitmap indicator (see Code Table 6.0)
 C>    0 bitmap applies and is included in Section 6.
 C>    1-253 Predefined bitmap applies
 C>    254 Previously defined bitmap applies to this field
 C>    255 Bit map does not apply to this product.
-C>    - gfld%bmap Logical*1 array containing decoded bitmap, if ibmap=0
+C>    - gfld\%bmap Logical*1 array containing decoded bitmap, if ibmap=0
 C>    or ibap=254. Otherwise nullified. This element is actually a
 C>    pointer to an array that holds the data.
-C>    - gfld%fld Array of gfld%ndpts unpacked data points. This element
+C>    - gfld\%fld Array of gfld\%ndpts unpacked data points. This element
 C>    is actually a pointer to an array that holds the data.
 C>    @param[out] IRET integer return code
 C>    - 0 all ok
