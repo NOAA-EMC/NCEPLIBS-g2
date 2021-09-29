bb7b94fc9e012630408cc5597b522c970fc99d03
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-09-29 12:23:43 -0600

    adding test

--- tests/test_g2_encode.f90
+++ tests/test_g2_encode.f90
@@ -6,11 +6,34 @@
 program test_g2_encode
   implicit none
 
+  ! For gribcreate().
   integer, parameter :: MAX_MSG_LEN = 256
   character (len = MAX_MSG_LEN) :: msg
   integer :: listsec0(2)
   integer :: listsec1(13)
   integer :: msg_len
+
+  ! For addgrid().
+  integer :: idgs(5)
+  integer :: idgstmpl(5)
+  integer :: igdstmplen
+  integer :: ideflist(5)
+  integer :: idefnum
+
+  ! For addfield().
+  integer :: ipdsnum
+  integer :: ipdstmpl(3)
+  integer :: ipdstmplen
+  integer, parameter :: numcoord = 3
+  real :: coordlist(numcoord)
+  integer :: idrsnum
+  integer :: idrstmpl
+  integer :: idrstmplen
+  integer, parameter :: ngrdpts = 3
+  real :: fld(ngrdpts)
+  integer :: ibmap
+  logical*1 :: bmap
+
   integer :: ierr
 
   print *, 'Testing g2 library encoding.'
@@ -51,9 +74,79 @@ program test_g2_encode
   call gribcreate(msg, MAX_MSG_LEN, listsec0, listsec1, ierr)
   if (ierr .ne. 0) stop 2
 
+  ! igds(1)=Source of grid definition (see Code Table 3.0)
+  ! igds(2)=Number of grid points in the defined grid.
+  ! igds(3)=Number of octets needed for each additional grid points
+  ! definition.Used to define number of points in each row (or column)
+  ! for non-regular grids. = 0, if using regular grid.
+  ! igds(4)=Interpretation of list for optional points definition. (Code Table 3.11)
+  ! igds(5)=Grid Definition Template Number (Code Table 3.1)
+  idgs(1) = 0
+  idgs(2) = 0
+  idgs(3) = 0
+  idgs(4) = 0
+  idgs(5) = 0
+
+  ! Contains the data values for the specified Grid Definition
+  ! Template ( NN=igds(5) ). Each element of this integer array
+  ! contains an entry (in the order specified) of Grid Defintion
+  ! Template 3.NN.
+  idgstmpl(1) = 0
+  idgstmpl(2) = 0
+  idgstmpl(3) = 0
+  idgstmpl(4) = 0
+  idgstmpl(5) = 0
+
+  ! Length of idgstmpl.
+  igdstmplen = 5
+
+  ! What's this?
+  idefnum = 0
+  
+  ! Add a grid to the GRIB2 message.
+  call addgrid(msg, MAX_MSG_LEN, idgs, idgstmpl, igdstmplen, ideflist, idefnum, ierr)
+  if (ierr .ne. 0) then
+     print *, 'ierr = ', ierr
+     stop 2
+  endif
+
+  ! Product Definition Template Number ( see Code Table 4.0).
+  ipdsnum = 0
+
+  ! Contains the data values for the specified Product Definition
+  ! Template (N=ipdsnum). Each element of this integer array contains
+  ! an entry (in the order specified) of Product Defintion Template
+  ! 4.N.
+  ipdstmpl(1) = 42
+  ipdstmpl(2) = 42
+  ipdstmpl(3) = 42
+
+  ! Max dimension of ipdstmpl coordlist Array containg floating point
+  ! values intended to document the vertical discretisation associated
+  ! to model data on hybrid coordinate vertical levels.
+  ipdstmplen = 3
+
+  ! Array containg floating point values intended to document the
+  ! vertical discretisation associated to model data on hybrid
+  ! coordinate vertical levels. (part of Section 4) The dimension of
+  ! this array can be obtained in advance from maxvals(5), which is
+  ! returned from subroutine gribinfo.
+  coordlist(1) = 1.0
+  coordlist(2) = 2.0
+  coordlist(3) = 3.0
+  
+  ! Add a field to the GRIB2 message.
+  call addfield(msg, MAX_MSG_LEN, ipdsnum, ipdstmpl, ipdstmplen, &
+       coordlist, numcoord, idrsnum, idrstmpl, idrstmplen, fld, &
+       ngrdpts, ibmap, bmap, ierr)
+  if (ierr .ne. 0) then
+     print *, 'ierr = ', ierr
+     stop 3
+  endif
+
   ! Finilize the GRIB2 message.
   call gribend(msg, MAX_MSG_LEN, msg_len, ierr)
-  if (ierr .ne. 0) stop 3
+  if (ierr .ne. 0) stop 4
   print *, 'msg_len = ', msg_len
   
   print *, 'SUCESSS!'
