d4d100a710704d4b0c80c166c221df991b5bd1b1
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-10-01 05:52:54 -0600

    adding grib2 file for testing

---
+++ tests/test_gdas_file.f90
@@ -0,0 +1,14 @@
+! This program tests the NCEPLIBS-g2 library with a GRIB2 file from
+! GDAS.
+!
+! Ed Hartnett 10/1/21
+program test_gdas_file
+  implicit none
+
+  print *, 'Testing reading a GDAS sample file.'
+
+  print *, 'Testing...'
+  
+  print *, 'SUCCESS!'
+  
+end program test_gdas_file
