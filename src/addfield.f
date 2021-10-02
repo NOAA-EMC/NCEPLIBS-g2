bb142ccaa1c1726c784c5b736008f1241c946a0d
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-10-02 08:07:31 -0600

    more test development, turn on address santizer

--- src/addfield.f
+++ src/addfield.f
@@ -110,7 +110,7 @@
       real,pointer,dimension(:) :: pfld
       real(4) :: coordieee(numcoord),re00
       integer(4) :: ire00,allones
-      integer :: intbmap(ngrdpts),mappds(ipdstmplen),mapdrs(idrstmplen)
+      integer :: mappds(ipdstmplen),intbmap(ngrdpts),mapdrs(idrstmplen)
       integer,parameter :: zero=0,one=1,four=4,five=5,six=6,seven=7
       integer,parameter :: minsize=50000
       integer iofst,ibeg,lencurr,len,mappdslen,mapdrslen,lpos3
