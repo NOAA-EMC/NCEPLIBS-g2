63e1cda195625086de640dbb25ba6a59388b2b06
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-10-03 06:49:18 -0600

    adding test for simpack

---
+++ tests/test_simpack.f90
@@ -0,0 +1,15 @@
+! This program tests the simple packing subroutine of the NCEPLIBS-g2
+! project.
+!
+! Ed Hartnett 10/3/21
+program test_simpack
+  use drstemplates
+  implicit none
+
+  print *, 'Testing simpack.'
+
+  print *, 'Testing...'
+  
+  print *, 'SUCCESS!'
+  
+end program test_simpack
