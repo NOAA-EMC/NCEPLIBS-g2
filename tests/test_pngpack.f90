ee11526bec110dc33c6b4608c78722c22dc47a28
Brian Curtis <Brian.Curtis@noaa.gov>
2021-10-18 10:55:39 -0400

    Don't know if idrstmpl is correct, committing most recent code

--- tests/test_pngpack.f90
+++ tests/test_pngpack.f90
@@ -6,30 +6,44 @@ program test_pngpack
 
   implicit none
 
-  integer :: width=5
-  integer :: height=5
-  integer :: len=25, ndpts=25
-  real, dimension(25):: fld
-  real, dimension(25) :: fld2
+  integer :: width=3
+  integer :: height=3
+  integer :: ndpts=9
+  integer(kind=8), dimension(9) :: fld
+  integer(kind=8), dimension(9) :: fld2
   integer :: idrstmpl(7)
-  character(len=1), dimension(25) :: cpack
+  character(len=1), dimension(9) :: cpack
   integer :: lcpack
-  integer :: i
+  integer :: i, j
 
   ! Create the fld variable with data to pack
-  fld = (/0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 4/)
+  fld = (/0, 0, 0, 1, 1, 1, 2, 2, 2/)
 
+  do i = 1, 9
+    print *, 'fld is', fld(i)
+  end do
   ! No idea what this needs to be, documentation confusing
-  idrstmpl = (/41, 0, 0, 0, 0, 0, 0/)
+  idrstmpl(1)=41
+  idrstmpl(2)=1
+  idrstmpl(3)=1
+  idrstmpl(4)=8
+  idrstmpl(5)=1
+  idrstmpl(6)=0
+  idrstmpl(7)=0
+
   print *, 'Testing pngpack/unpack'
   call pngpack(fld, width, height, idrstmpl, cpack, lcpack)
   print *, 'Call to pngpack successful'
-  call pngunpack(cpack, len, idrstmpl, ndpts, fld2)
-  print *, 'fld2 is:', fld2
-  do i = 1, ndpts
-   if (abs(fld2(i) - fld(i)) .gt. .1) stop 10
+  print *, 'cpack is: ', cpack
+  print *, 'lcpack is: ', lcpack
+  call pngunpack(cpack, lcpack, idrstmpl, ndpts, fld2)
+  do i = 1, 9
+    print *, 'fld2 is', fld2(i), 'fld is', fld(i)
+    if (fld2(i) .ne. fld(i)) then
+        print *, 'fld2 and fld mismatch at point', i
+        stop 10
+    endif
   end do
-  !if (fld2 .ne. fld) stop 3
 
   print *, 'SUCCESS!'
 
