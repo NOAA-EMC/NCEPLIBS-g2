6ebc15038453fe2c8c76938f706d0bbb995b3ccd
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-10-22 09:48:11 -0600

    working on test

--- tests/test_gdas_file.f90
+++ tests/test_gdas_file.f90
@@ -10,20 +10,24 @@ program test_gdas_file
   integer :: jids(13)
   integer :: k
   type(gribfield) :: gfld
+  integer :: lu = 1
   integer :: ierr
   
   print *, 'Testing reading a GDAS sample file ', filename
 
   ! Open the file.
   print *, 'Opening file...'
-  open(12, file = filename, form = 'unformatted', status = 'old')
+  call baopenr(lu, filename, ierr)
+  if (ierr .ne. 0) stop 10
 
   print *, 'Testing getgb2()...'
-  call getgb2(12, 0, 0, -1, jids, -1, -9999, -1, -9999, .true., k, gfld, ierr)
+!  call getgb2(lu, 0, 0, -1, jids, -1, -9999, -1, -9999, .true., k, gfld, ierr)
   if (ierr .ne. 0) stop 20
 
+  ! Close the test file.
   print *, 'Closing file...'
-  close(12)
+  call baclose(lu, ierr)
+  if (ierr .ne. 0) stop 114
   
   print *, 'SUCCESS!'
 end program test_gdas_file
