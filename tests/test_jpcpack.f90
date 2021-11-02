4d811e56ecb64603ef61197eb4a914989e9135be
Brian Curtis <Brian.Curtis@noaa.gov>
2021-11-02 14:07:08 -0400

    Add stop's for failed real comparisons

--- tests/test_jpcpack.f90
+++ tests/test_jpcpack.f90
@@ -4,40 +4,41 @@
 ! Brian Curtis 2021-10-18
 program test_jpcpack
 
-  implicit none
+    implicit none
 
-  integer, parameter :: width=2, height=2, ndpts=4
-  real(kind=8) :: fld(ndpts), fld2(ndpts)
-  integer :: idrstmpl(7)
-  character(len=1) :: cpack(200)
-  integer :: lcpack
-  integer :: i
+    integer, parameter :: width=2, height=2, ndpts=4
+    real(kind=8) :: fld(ndpts), fld2(ndpts)
+    integer :: idrstmpl(7)
+    character(len=1) :: cpack(200)
+    integer :: lcpack
+    integer :: i
 
-  ! Create the fld variable with data to pack
-  fld = (/1.0, 2.0, 3.0, 4.0/)
+    ! Create the fld variable with data to pack
+    fld = (/1.0, 2.0, 3.0, 4.0/)
 
-  ! No idea what this needs to be, documentation confusing
-  idrstmpl(1)=0
-  idrstmpl(2)=1
-  idrstmpl(3)=1
-  idrstmpl(4)=21
-  idrstmpl(5)=0
-  idrstmpl(6)=0
-  idrstmpl(7)=0
+    ! No idea what this needs to be, documentation confusing
+    idrstmpl(1)=0
+    idrstmpl(2)=1
+    idrstmpl(3)=1
+    idrstmpl(4)=21
+    idrstmpl(5)=0
+    idrstmpl(6)=0
+    idrstmpl(7)=0
 
-  print *, 'Testing jpcpack/unpack'
-  call jpcpack(fld, width, height, idrstmpl, cpack, lcpack)
-  print *, 'Call to jpcpack successful'
-  print *, 'lcpack is: ', lcpack
-  call jpcunpack(cpack, lcpack, idrstmpl, ndpts, fld2)
-  do i = 1, ndpts
-    print *, 'fld2 is', fld2(i), 'fld is', fld(i)
-    !if (fld2(i) .ne. fld(i)) then
-    !    print *, 'fld2 and fld mismatch at point', i
-    !    stop 10
-    !endif
-  end do
+    ! Testing jpcpack
+    call jpcpack(fld, width, height, idrstmpl, cpack, lcpack)
 
-  print *, 'SUCCESS!'
+    ! Testing jpcunpack
+    call jpcunpack(cpack, lcpack, idrstmpl, ndpts, fld2)
+    
+    ! Compare each value to see match, reals do not compare well
+    do i = 1, ndpts
+        if (fld(i) .ne. fld2(i)) then
+            print *, fld(i), fld2(i), 'do not match'
+            stop 4
+        end if
+    end do
+
+    print *, 'SUCCESS!'
 
 end program test_jpcpack
--- tests/test_pngpack.f90
+++ tests/test_pngpack.f90
@@ -4,38 +4,41 @@
 ! Brian Curtis 2021-10-04
 program test_pngpack
 
-  implicit none
+    implicit none
 
-  integer, parameter :: width=2, height=2, ndpts=4
-  real(kind=8) :: fld(ndpts), fld2(ndpts)
-  integer :: idrstmpl(7)
-  character(len=1) :: cpack(100)
-  integer :: lcpack
-  integer :: i
+    integer, parameter :: width=2, height=2, ndpts=4
+    real(kind=8) :: fld(ndpts), fld2(ndpts)
+    integer :: idrstmpl(7)
+    character(len=1) :: cpack(100)
+    integer :: lcpack
+    integer :: i
 
-  ! Create the fld variable with data to pack
-  fld = (/1.0, 2.0, 3.0, 4.0/)
+    ! Create the fld variable with data to pack
+    fld = (/1.0, 2.0, 3.0, 4.0/)
 
-  ! No idea what this needs to be, documentation confusing
-  idrstmpl(1)=0
-  idrstmpl(2)=1
-  idrstmpl(3)=1
-  idrstmpl(4)=8
-  idrstmpl(5)=0
-  idrstmpl(6)=0
-  idrstmpl(7)=1
+    ! No idea what this needs to be, documentation confusing
+    idrstmpl(1)=0
+    idrstmpl(2)=1
+    idrstmpl(3)=1
+    idrstmpl(4)=8
+    idrstmpl(5)=0
+    idrstmpl(6)=0
+    idrstmpl(7)=1
 
-  print *, 'Testing pngpack'
-  call pngpack(fld, width, height, idrstmpl, cpack, lcpack)
-  print *, 'Call to pngpack completed'
-  print *, 'lcpack is: ', lcpack
-  print *, 'Testing pngunpack'
-  call pngunpack(cpack, lcpack, idrstmpl, ndpts, fld2)
-  print *, 'Call to pngunpack completed'
-  do i = 1, ndpts
-    print *, fld(i), fld2(i)
-  end do
+    ! Testing pngpack
+    call pngpack(fld, width, height, idrstmpl, cpack, lcpack)
 
-  print *, 'SUCCESS!'
+    ! Testing pngunpack
+    call pngunpack(cpack, lcpack, idrstmpl, ndpts, fld2)
+
+    ! Compare each value to see match, reals do not compare well
+    do i = 1, ndpts
+        if (fld(i) .ne. fld2(i)) then
+            print *, fld(i), fld2(i), 'do not match'
+            stop 4
+        end if
+    end do
+
+    print *, 'SUCCESS!'
 
 end program test_pngpack
