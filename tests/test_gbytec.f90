0b55bf4911f5905a5bfee65c559218a7fdb69396
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-11-12 08:16:11 -0700

    fixed test

--- tests/test_gbytec.f90
+++ tests/test_gbytec.f90
@@ -2,8 +2,9 @@ program test_gbytec
   implicit none
 
   character*1 :: out(1)
-  character*1 :: out5(20)
+  character*1 :: out5(5)
   character*1 :: out2(8)
+  character*1 :: out10(10)
   integer, parameter :: n = 1
   integer :: in(n)
   integer, parameter :: n2 = 2
@@ -15,43 +16,70 @@ program test_gbytec
   integer :: nskip = 0
   integer :: i
 
-  print *, 'Testing gbytec.'
+  print *, 'Testing g2_gbytesc.f subroutines.'
 
-  print *, 'Testing sbytec()...'
+  print *, 'Testing g2_sbytec()...'
   in(1) = 3
+  out(1) = char(0)
   call g2_sbytec(out, in, iskip, nbyte)
   if (ichar(out(1)) .ne. in(1)) stop 10
 
-  print *, 'Testing sbytesc()...'
+  print *, 'Testing g2_sbytesc()...'
   in(1) = 3
+  out(1) = char(0)
   call g2_sbytesc(out, in, iskip, nbyte, nskip, n)
-  if (ichar(out(1)) .ne. in(1)) stop 10
+  if (ichar(out(1)) .ne. in(1)) stop 20
 
-  print *, 'Testing sbytec() with more bytes...'
-  
+  ! This will pack the numbers 1 and 2 into the first two chars of the
+  ! buffer. The rest of the output buffer will remain zeros.
+  print *, 'Testing g2_sbytesc() with more bytes...'
   in2(1) = 1
   in2(2) = 2
-  nbyte = 16
-  nskip = 0
-  call g2_sbytec(out2, in2, iskip, nbyte)
   do i = 1, 8
-     print *, ichar(out2(i))
+     out2(i) = char(0)
+  end do
+  nbyte = 8
+  call g2_sbytesc(out2, in2, iskip, nbyte, nskip, n2)
+  do i = 1, 8
+     if (i .le. 2) then
+        if (ichar(out2(i)) .ne. in2(i)) stop 30;
+     else
+        if (ichar(out2(i)) .ne. 0) stop 31;
+     endif
   end do
 
-  ! in5(1) = 1
-  ! in5(2) = 2
-  ! in5(3) = 3
-  ! in5(4) = 4
-  ! in5(5) = 5
-  ! nbyte = 40
-  ! nskip = 0
-  ! call g2_sbytec(out5, in5, iskip, nbyte)
-  ! do i = 1, 20
-  !    print *, ichar(out5(i))
-  ! end do
-  !if (ichar(out(1)) .ne. in(1)) stop 10
+  ! Now pack 5 values into the 5 character array out5.
+  in5(1) = 1
+  in5(2) = 2
+  in5(3) = 3
+  in5(4) = 4
+  in5(5) = 5
+  nbyte = 8
+  nskip = 0
+  do i = 1, 5
+     out5(i) = char(0)
+  end do
+  call g2_sbytesc(out5, in5, iskip, nbyte, nskip, n5)
+  do i = 1, 5
+     if (ichar(out5(i)) .ne. in5(i)) stop 40;     
+  end do
 
-  
-
+  ! Now pack 5 values into the 10 character array out10. Skip every
+  ! other byte in the output.
+  nbyte = 8
+  nskip = 0
+  do i = 1, 10
+     out10(i) = char(0)
+  end do
+  call g2_sbytesc(out10, in5, iskip, nbyte, 8, 5)
+  do i = 1, 10
+     ! print '(z2.2)', out10(i)
+     if (mod(i, 2) .gt. 0) then
+        if (ichar(out10(i)) .ne. in5(int(i/2) + 1)) stop 51;
+     else
+        if (ichar(out10(i)) .ne. 0) stop 50;
+     endif
+  end do
   print *, 'SUCCESS!'
+
 end program test_gbytec
