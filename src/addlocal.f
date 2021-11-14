0644ae491d0d22fa3bcfed50b467a059c208f796
Edward Hartnett <Edward.Hartnett@noaa.gov>
2021-11-14 06:53:37 -0700

    whitespace cleanup

--- src/addlocal.f
+++ src/addlocal.f
@@ -2,28 +2,29 @@
 !>    @brief This subroutine adds a Local Use Section (Section 2) to
 !>    a GRIB2 message.
 !>    @author Stephen Gilbert @date 2000-05-01
-!>    
-
-!>    This subroutine adds a Local Use Section (Section 2) to a GRIB2
-!>    message. This routine is used with routines gribcreate(),
-!>    addlocal(), addfield(), and gribend() to create a complete GRIB2
-!>    message. Subroutine gribcreate must be called first to initialize
-!>    a new GRIB2 message.
-!>    
-!>    @param[inout] cgrib Character array to contain the GRIB2 message
-!>    @param[in] lcgrib Maximum length (bytes) of array cgrib.
-!>    @param[in] csec2 Character array containing information to be added to
-!>    Section 2.
-!>    @param[in] lcsec2 Number of bytes of character array csec2 to be added to
-!>    Section 2.
-!>    @param[out] ierr Error return code.
-!>    - 0 = no error
-!>    - 1 = GRIB message was not initialized.  Need to call routine gribcreate first.
-!>    - 2 = GRIB message already complete.  Cannot add new section.
-!>    - 3 = Sum of Section byte counts doesn't add to total byte count.
-!>    - 4 = Previous Section was not 1 or 7.
 !>
-!>    @note Note that the Local Use Section ( Section 2 ) can only follow
+
+!>    This subroutine adds a [Local Use Section (Section
+!>    2)](https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_sect2.shtml)
+!>    to a GRIB2 message. This routine is used with routines
+!>    gribcreate(), addlocal(), addfield(), and gribend() to create a
+!>    complete GRIB2 message. Subroutine gribcreate must be called first
+!>    to initialize a new GRIB2 message.
+!>
+!>    @param[inout] cgrib Character array to contain the GRIB2 message.
+!>    @param[in] lcgrib Maximum length (bytes) of array cgrib.
+!>    @param[in] csec2 Character array containing information to be
+!>    added to Section 2.
+!>    @param[in] lcsec2 Number of bytes of character array csec2 to be
+!>    added to Section 2.
+!>    @param[out] ierr Error return code.
+!>    - 0 no error
+!>    - 1 GRIB message was not initialized. Need to call routine gribcreate first.
+!>    - 2 GRIB message already complete. Cannot add new section.
+!>    - 3 Sum of Section byte counts doesn't add to total byte count.
+!>    - 4 Previous Section was not 1 or 7.
+!>
+!>    @note Note that the Local Use Section (Section 2) can only follow
 !>    Section 1 or Section 7 in a GRIB2 message.
 !>
 !>    @author Stephen Gilbert @date 2000-05-01
@@ -34,12 +35,12 @@
       character(len=1), intent(in) :: csec2(lcsec2)
       integer, intent(in) :: lcgrib, lcsec2
       integer, intent(out) :: ierr
-      
+
       character(len=4), parameter :: grib = 'GRIB', c7777 = '7777'
       character(len=4):: ctemp
       integer, parameter :: two = 2
       integer lensec2, iofst, ibeg, lencurr, len
-      
+
       ierr = 0
 
 !     Check to see if beginning of GRIB message exists
@@ -47,22 +48,22 @@
       ctemp = cgrib(1) // cgrib(2) // cgrib(3) // cgrib(4)
       if (ctemp .ne. grib) then
           print *, 'addlocal: GRIB not found in given message.'
-          print *, 'addlocal: Call to routine gribcreate required', 
+          print *, 'addlocal: Call to routine gribcreate required',
      &         ' to initialize GRIB messge.'
           ierr = 1
           return
       endif
 
 !     Get current length of GRIB message
-      
+
       call g2_gbytec(cgrib, lencurr, 96, 32)
 
 !     Check to see if GRIB message is already complete
-      
-      ctemp = cgrib(lencurr - 3) // cgrib(lencurr - 2) // 
+
+      ctemp = cgrib(lencurr - 3) // cgrib(lencurr - 2) //
      &     cgrib(lencurr - 1) // cgrib(lencurr)
       if (ctemp .eq. c7777) then
-          print *, 'addlocal: GRIB message already complete.  Cannot', 
+          print *, 'addlocal: GRIB message already complete.  Cannot',
      &         ' add new section.'
           ierr = 2
           return
@@ -72,7 +73,7 @@
 !     find the last section number.
 
       len = 16                  ! length of Section 0
-      do 
+      do
 !     Get section number and length of next section
           iofst = len * 8
           call g2_gbytec(cgrib, ilen, iofst, 32)
@@ -87,7 +88,7 @@
               print *, 'addlocal: Section byte counts don''t ' //
      &             'add to total.'
               print *, 'addlocal: Sum of section byte counts = ', len
-              print *, 'addlocal: Total byte count in Section 0 = ', 
+              print *, 'addlocal: Total byte count in Section 0 = ',
      &             lencurr
               ierr = 3
               return
@@ -112,7 +113,7 @@
       call g2_sbytec(cgrib, two, iofst, 8) ! Store section number ( 2 )
       istart = lencurr + 5
       cgrib(istart + 1:istart + lcsec2) = csec2(1:lcsec2)
-      
+
 !     Calculate length of section 2 and store it in octets
 !     1-4 of section 2.
       lensec2 = lcsec2 + 5      !  bytes
@@ -123,5 +124,3 @@
 
       return
       end
-
-
