cdafd8bc3ade639b6b098d52996fbc1763b4f2c6
Brian Curtis <Brian.Curtis@noaa.gov>
2021-12-07 15:05:25 -0500

    Finished most of getgb2 comparisons in test

--- tests/test_getgb2.f90
+++ tests/test_getgb2.f90
@@ -18,6 +18,7 @@ program test_getgb2
   integer :: jpdt(ipdstmplen)
   integer, parameter :: jgdtn = 0
   integer, parameter :: igdstmplen = 19
+  integer :: idrstmpl(5)
   integer :: jgdt(igdstmplen) = (/ 0, 1, 1, 1, 1, 1, 1, 2, 2, 0, 0, 45, 91, 0, 55, 101, 5, 5, 0 /)
   logical, parameter :: unpack = .false.
   integer :: k
@@ -26,6 +27,7 @@ program test_getgb2
   integer :: i
   integer, parameter :: lcsec2 = 3
   character :: csec2(lcsec2) = (/ achar(1), achar(2), achar(3) /)
+  real(8) :: fld(4) = (/ 1.1, 1.2, 1.3, 1.4 /)
 
   jpdt(1) = 0
   jpdt(2) = 0
@@ -58,6 +60,12 @@ program test_getgb2
   jids(12) = 1 !  Operational Test Products
   jids(13) = 0 ! Analysis Products
 
+  idrstmpl(1) = 0
+  idrstmpl(2) = 1
+  idrstmpl(3) = 1
+  idrstmpl(4) = 8
+  idrstmpl(5) = 0
+
   ! WHAT I NEED
   !#jids = listsec1
   !#jpdtn = ipdsnum
@@ -65,6 +73,7 @@ program test_getgb2
   !#jgdtn = idefnum
   !#jgdt = igdstmpl
 
+  print *, 'Testing open/read/close of GRIB2 file created with creategrib.f90..'
   call baopenr(1, "testgrib.grb2", iret)
   if (iret .ne. 0) then
       print *, 'baopenr failed with iret value: ', iret
@@ -83,7 +92,9 @@ program test_getgb2
       print *, 'baclose failed with iret value: ', iret
       stop 5
   end if
+  print *, 'Success!...'
 
+  print *, 'Testing all gfld outputs to match what we created with creategrib.f90..'
   if (gfld%version .ne. 2) stop 10
 
   if (gfld%discipline .ne. 0) stop 20
@@ -100,135 +111,62 @@ program test_getgb2
 
   if (gfld%locallen .ne. lcsec2) stop 60
 
-              !>    Field number within GRIB message.
-  print *, 'ifldnum: ', gfld%ifldnum
-!              integer :: ifldnum
+  if (gfld%ifldnum .ne. 1) stop 70
 
-              !>    Source of grid definition (see [Code Table 3.0]
-              !>    (https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table3-0.shtml))
-              !>     - 0 Specified in field igdtnum (as defined in [Code table 3.1]
-              !>     (https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table3-1.shtml).
-              !>     - 1 Predetermined grid Defined by originating centre.
-  print *, 'griddef: ', gfld%griddef
-!              integer :: griddef
+  if (gfld%griddef .ne. 0) stop 80
 
-              !>    Number of grid points in the defined grid. Note that the number of
-              !>    actual data values returned from getgb2() (in ndpts) may be less
-              !>    than this value if a logical bitmap is in use with grid points
-              !>    that are being masked out.
-!              integer :: ngrdpts
+  if (gfld%ngrdpts .ne. 4) stop 90
 
-              !>    Number of octets needed for each additional grid points
-              !>    definition. Used to define number of points in each row
-              !>    (or column) for non-regular grids. Equal to 0, if using
-              !>    regular grid.
-!              integer :: numoct_opt
+  if (gfld%numoct_opt .ne. 0) stop 91
 
-              !>    Interpretation of list of numbers at end of section
-              !>    3 - the Grid Definiton Section. (See [Code Table 3.11]
-              !>    (https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table3-11.shtml)).
-!              integer :: interp_opt
+  if (gfld%interp_opt .ne. 0) stop 92
 
-              !>    (Used if numoct_opt .ne. 0.) The number of entries in
-              !>    array ideflist. i.e. number of rows (or columns) for
-              !>    which optional grid points are defined. This value is
-              !>    set to zero, if umoct_opt = 0.
-!              integer :: num_opt
+  if (gfld%num_opt .ne. 0) stop 100
 
-              !>    (Used if numoct_opt .ne. 0.) This array contains the
-              !>    number of grid points contained in each row (or
-              !>    column) (part of Section 3), This element is actually a
-              !>    pointer to an array that holds the data. This pointer is
-              !>    null if numoct_opt equals 0.
-!              integer, pointer, dimension(:) :: list_opt => null()
+  if (gfld%igdtnum .ne. 0) stop 101
 
-              !>    Grid Definition Template Number ([Code Table 3.1]
-              !>    (https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table3-1.shtml)).
-!              integer :: igdtnum
+  if (gfld%igdtlen .ne. 19) stop 110
 
-              !>    Number of elements in the Grid Defintion Template
-              !>    specified by igdtnum.
-!              integer :: igdtlen
+  do i = 1, 19
+    if (gfld%igdtmpl(i) .ne. jgdt(i)) stop 120
+  end do
 
-              !>    Contains the data values for the Grid Definition
-              !>    Template specified by igdtnum. This element is actually
-              !>    a pointer to an array that holds the data.
-!              integer, pointer, dimension(:) :: igdtmpl => null()
+  if (gfld%ipdtnum .ne. 0) stop 130
 
-              !>    Product Definition Template Number ([Code Table 4.0]
-              !>    (https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table4-0.shtml)).
-!              integer :: ipdtnum
+  if (gfld%ipdtlen .ne. 15) stop 140
 
-              !>    Number of elements in the Product Defintion Template
-              !>    specified by ipdtnum.
-!              integer :: ipdtlen
+  do i = 1, 15
+    if (gfld%ipdtmpl(i) .ne. jpdt(i)) stop 150
+  end do
 
-              !>    Contains the data values for the Product Definition
-              !>    Template specified by ipdtnum. This element is actually
-              !>    a pointer to an array that holds the data.
-!              integer, pointer, dimension(:) :: ipdtmpl => null()
+  if (gfld%num_coord .ne. 0) stop 160
 
-              !>    Number of values in array coord_list. This is part of
-              !>    Section 3 - the Grid Definition Section.
-!              integer :: num_coord
+  if (gfld%ndpts .ne. 4) stop 170
 
-              !>    Real array containing floating point values intended to
-              !>    document the vertical discretisation associated to model
-              !>    data on hybrid coordinate vertical levels (part of
-              !>    Section 3 - the Grid Definition Section). This element
-              !>    is actually a pointer to an array that holds the data.
-!              real, pointer, dimension(:) :: coord_list => null()
+  if (gfld%idrtnum .ne. 0) stop 180
 
-              !>    Number of data points unpacked and returned. Note that
-              !>    this number may be different from the value of ngrdpts
-              !>    if a logical bitmap is in use with grid points that are
-              !>    being masked out.
-!              integer :: ndpts
+  if (gfld%idrtlen .ne. 5) stop 190
 
-              !>    Data Representation Template Number ([Code Table 5.0]
-              !>    (https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table5-0.shtml)).
-!              integer :: idrtnum
+  ! NOT WORKING
+  do i = 1, 5
+    print *, gfld%idrtmpl(i), idrstmpl(i)
+    ! if (gfld%idrtmpl(i) .ne. idrstmpl(i)) stop 200
+  end do
 
-              !>    Number of elements in the Data Representation Template
-              !>    array specified by idrtnum.
-!              integer :: idrtlen
+  if (gfld%unpacked .neqv. .false.) stop 201
 
-              !>    Contains the data values for the Data Representation
-              !>    Template specified by idrtnum. This element is actually
-              !>    a pointer to an array that holds the data.
-!              integer, pointer, dimension(:) :: idrtmpl => null()
+  if (gfld%expanded .neqv. .false.) stop 202
 
-              !>    Logical value indicating whether the bitmap and data
-              !>    values were unpacked. If false, bmap and fld pointers
-              !>    are null.
-!              logical :: unpacked
+  if (gfld%ibmap .ne. 255) stop 203
 
-              !>    Logical value indicating whether the data field was
-              !>    expanded to the grid in the case where a bit-map is
-              !>    present. If true, the data points in fld match the grid
-              !>    points and zeros were inserted at grid points where data
-              !>    was bit-mapped out. If false, the data values in fld
-              !>    were not expanded to the grid and are just a consecutive
-              !>    array of data points corresponding to each value of "1"
-              !>    in bmap.
-!              logical :: expanded
+  !REMAINING ITEMS NOT WORKING RIGHT
+  ! if (gfld%bmap(1) .neqv. .false.) stop 204
 
-              !>    Bitmap indicator (see [Code Table 6.0]
-              !>    (https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table6-0.shtml)).
-              !>     - 0 bitmap applies and is included in Section 6.
-              !>     - 1-253 Predefined bitmap applies.
-              !>     - 254 Previously defined bitmap applies to this field.
-              !>     - 255 Bit map does not apply to this product.
-!              integer :: ibmap
+  ! do i = 1, 4
+  !     print *, gfld%fld(i), fld(i)
+  ! !   ! if (gfld%fld(i) .ne. fld(i)) stop 205
+  ! end do
 
-              !>    Logical*1 array containing decoded bitmap, if ibmap
-              !>    equals 0 or 254 - otherwise null. This element is
-              !>    actually a pointer to an array that holds the data.
-!              logical*1, pointer, dimension(:) :: bmap => null()
-
-              !>    Array of ndpts unpacked data points. This element is
-              !>    actually a pointer to an array that holds the data.
-!              real, pointer, dimension(:) :: fld => null()
 
   print *, 'SUCCESS!'
 
