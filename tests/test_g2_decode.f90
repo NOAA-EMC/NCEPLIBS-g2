b2a140614d438c04277190a5c954e1bf08a06ed2
Hang.Lei <Hang.Lei@noaa.gov>
2021-12-21 05:23:44 +0000

    address test issue #101 #147 #207

--- tests/test_g2_decode.f90
+++ tests/test_g2_decode.f90
@@ -1,6 +1,9 @@
-!> This test is for decoding full grib2 message. Code was translated
-!> from the C version originated from Dusan. 
-!> Dusan Jovic July 2021
+!>    This test is a test for the NCEPLIBS-g2 library.
+!>    It fully decode all grib2 message. Code referenced to g2c test by Dusan.
+!>    Extra functions and code for fortran are added. Data was designed for 
+!>    a typical grib2 file in fortran, with zero values to the cariables. 
+!>    
+!>    Hang Lei 2021-11-21
 !>
 
        program tst_decode
@@ -9,99 +12,135 @@
        use params
        use gridtemplates
 
-       integer fgrib_len, iret, i
-       character, dimension(195) :: fgrib
-       real, dimension(121) :: fld_ok
-       integer G2_ERROR, numfields, numlocal, maxlocal
-       character, dimension(195) :: hex
+
+       integer :: fgrib_len, iret, i, Nan
+       character, dimension(269) :: fgrib
+       real, dimension(225) :: fld_ok
+       integer :: G2_ERROR, numfields, numlocal, maxlocal
        integer, dimension(3) :: listsec0, listsec0_ok
        integer, dimension(13) :: listsec1, listsec1_ok
        type(gribfield) :: gfld
-    
+       integer :: k1, k2, k3, k4, k5, k6, k7, k8, k9, k10
+
        print *,"Testing decoding full grib2 message.\n"
 
-!        read(fgrib(1:75),'(z4)')
-         fgrib(:)=(/                    & 
-     &  "G", "R", "I", "B", "2", "0", &
-     &  "2", "2", "0", "0", "0", "0", &
-     &  "0", "0", "0", "3", "0", "0", &
-     &  "0", "4", "7", "0", "1", "0", &
-     &  "0", "2", "1", "1", "1", "2", &
-     &  "1", "5", "4", "0", "0", "0", &
-     &  "3", "0", "0", "0", "H", "6", &
-     &  "0", "0", "0", "0", "y", "0", &
-     &  "0", "0", "0", "4", "0", "0", &
-     &  "0", "0", "0", "0", "0", "0", &
-     &  "0", "0", "0", "0", "0", "0", &
-     &  "0", "0", "0", "0", "5", "0", &
-     &  "0", "0", "5", "0", "0", "0", &
-     &  "0", "0", "0", "0", "0", "3", &
-     &  "I", "C", "B", "3", "1", "5", &
-     &  "0", "0", "2", "9", "Z", "0", &
-     &  "3", "1", "1", "1", "0", "S", &
-     &  "B", "6", "0", "1", "B", "0", &
-     &  "0", "0", "0", "0", "4", "E", &
-     &  "0", "0", "0", "0", "0", "0", &
-     &  "2", "0", "`", "0", "0", "0", &
-     &  "3", "0", "0", "0", "4", "3", &
-     &  "0", "0", "0", "0", "0", "8", &
-     &  "0", "0", "0", "0", "0", "0", &
-     &  "0", "0", "9", "7", "0", "0", &
-     &  "0", "y", "0", "0", "0", "0", &
-     &  "0", "0", "0", "0", "0", "0", &
-     &  "3", "0", "0", "0", "0", "4", &
-     &  "4", "ÿ", "0", "0", "0","7", &
-     &  "0", "0", "0", "0", "0", "0", &
-     &  "0", "0", "5", "0", "3", "8", &
-     &  "8", "5", "6", "9", "7", "7", &
-     &  "7", "7", "7" /)
-        print*, 101
-!       read(fgrib(1:195),'(195z4)')hex(1:195)      
-        fgrib_len = 195
-        G2_ERROR = 2
-       listsec0_ok(:) = (/ 2, 2, 195 /)
-       listsec1_ok(:) = (/ 7, 0, 2, 1, 1, 2021, 7, 14, 6, 0, 0, 0, 1 /)
-        print*, 102
-       fld_ok(:) = (/                    &
-     &  1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, &
-     &  0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, &
-     &  1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, &
-     &  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, &
-     &  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, &
-     &  0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, &
-     &  0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, &
-     &  0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1, &
-     &  0, 0, 1, 1, 1, 0, 0, 0, 1, 1, 1, &
-     &  0, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, &
-     &  1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1 /)
+       ! To initialize large parameters in typical grib2 file.       
+       k1=6371200                   ! Radius of the earth
+       k2=1073                      ! Nx for lambert 5km
+       k3=689                       ! Ny for lambert 5km
+       k4=2019199                   ! La1 - latitude of first grid point 
+       k5=2384459                   ! Lo1 - longitude of first grid point
+       k6=25000000                  ! Latitude where grid spacing is defined
+       k7=265000000                 ! Orientation longitude
+       k8=2539703                   ! X & Y direction grid length
+       k9=-90000000                 ! Latitude of pole 
+       k10=739297                   ! Number of grid Nx*Ny
+       Nan=9999                     ! Missing value
+       fgrib_len=269                ! Grib message length
+
+       ! The grib message for a typical grib2 file in fortran.
+       fgrib(:)=(/                                                   &
+       ! section 0
+     & "G", "R", "I", "B", achar(0), achar(0),                        &
+     & achar(0), achar(2), achar(0), achar(0), achar(0), achar(0),    &
+     & achar(0), achar(0), achar(1), achar(13),                       &
+       ! section 1
+     & achar(0), achar(0), achar(0), achar(21), achar(1), achar(0),   &
+     & achar(8), achar(0), achar(0), achar(1), achar(0), achar(1),    &
+     & achar(7), achar(229), achar(11), achar(15), achar(10),         &
+     & achar(10), achar(10), achar(0), achar(1),                      &
+       ! section 2
+     & achar(0), achar(0), achar(0), achar(11), achar(2),             &
+     & achar(1), "H", "K", "RW", "A", "ZR",                           &
+       ! section 3
+     & achar(0), achar(0), achar(0), achar(81), achar(3), achar(0),   &
+     & achar(0), achar(0), achar(0), achar(k10), achar(0), achar(0),  &
+     & achar(0), achar(30), achar(1), achar(0), achar(k1), achar(k1), &
+     & achar(k1), achar(k1), achar(0), achar(0), achar(0), achar(0),  &
+     & achar(0), achar(0), achar(0), achar(0), achar(0), achar(0),    &
+     & achar(k2), achar(k2), achar(k2), achar(k2), achar(k3),         &
+     & achar(k3), achar(k3), achar(k3), achar(k4), achar(k4),         &
+     & achar(k4), achar(k4),achar(k5),achar(k5), achar(k5),           &
+     & achar(k5), achar(0), achar(k6), achar(k6), achar(k6),          &
+     & achar(k6), achar(k7), achar(k7), achar(k7), achar(k7),         &
+     & achar(k8), achar(k8), achar(k8), achar(k8), achar(k8),         &
+     & achar(k8), achar(k8), achar(k8), achar(0), achar(80),          &
+     & achar(k6), achar(k6), achar(k6), achar(k6), achar(k6),         &
+     & achar(k6), achar(k6), achar(k6), achar(k9), achar(k9),         &
+     & achar(k9), achar(k9), achar(0), achar(0), achar(0), achar(0),  &
+       ! section 4
+     & achar(0), achar(0), achar(0), achar(71), achar(4), achar(0),   &
+     & achar(0), achar(0), achar(9), achar(1), achar(8), achar(2),    &
+     & achar(0), achar(0), achar(0), achar(0), achar(0), achar(0),    &
+     & achar(0), achar(3), achar(6), achar(9), achar(1), achar(0),    &
+     & achar(0), achar(0), achar(0), achar(0), achar(0), achar(0),    &
+     & achar(0), achar(0), achar(0),  achar(0), achar(0), achar(0),   &
+     & achar(1), achar(0), achar(0), achar(0), achar(0), achar(0),    &
+     & achar(3), achar(0), achar(0), achar(0), achar(254), achar(7),  &
+     & achar(229), achar(11), achar(15), achar(12), achar(20),        &
+     & achar(10), achar(1), achar(0), achar(0), achar(0), achar(0),   &
+     & achar(1), achar(0), achar(1), achar(0), achar(0), achar(0),    &
+     & achar(12), achar(1), achar(0), achar(0), achar(0), achar(0),   &
+       ! section 5
+     & achar(0), achar(0), achar(0), achar(47), achar(5),  achar(0),  &
+     & achar(0), achar(0), achar(k10), achar(0), achar(2),  achar(0), &
+     & achar(0), achar(0), achar(0), achar(0), achar(0),  achar(0),   &
+     & achar(1), achar(8), achar(1), achar(1), achar(1), achar(Nan),  &
+     & achar(Nan), achar(Nan), achar(Nan), achar(0), achar(0),        &
+     & achar(0), achar(0), achar(0), achar(0), achar(0), achar(0),    &
+     & achar(0), achar(0), achar(0), achar(0), achar(0), achar(0),    &
+     & achar(0), achar(0), achar(0), achar(0), achar(0), achar(0),    &
+       ! section 6
+     & achar(0), achar(0), achar(0), achar(6), achar(6),  achar(255), &
+       ! section 7
+     & achar(0), achar(0), achar(0), achar(12), achar(7),  achar(1),  &
+     & achar(0), achar(0), achar(0), achar(0), achar(0),  achar(0),   &
+       ! section 8
+     & "7", "7", "7", "7" /) 
+   
+      ! Test g2_gbytesc.f90 to get grib header info.
+       call g2_gbytec(fgrib,i,32,8)
+       if (i .ne. 0) stop 101
+       call G2_GBYTESC(fgrib,i,56,8,0,1)
+       if (i .ne. 2) stop 102    ! check grib version.
+
+      ! Test gb_info.f90 to get full grib2 messages.
+
+       G2_ERROR = 2
+       listsec0_ok(:) = (/ 0, 2, 269 /)
+       listsec1_ok(:) = (/ 8, 0, 1, 0, 1, 2021, 11, 15, 10, 10, 10, 0, 1 /)
+       fld_ok(:) = 0                  ! zero data field.
        
-        print*, 03
-        call gb_info(fgrib, fgrib_len, listsec0, listsec1, &
+       call gb_info(fgrib, fgrib_len, listsec0, listsec1,             &
      &  numfields, numlocal, maxlocal, iret)
-       if (iret .ne. 0) stop 101
+        
+       if (iret .ne. 0) stop 103
 
        do i =0,2
-         if (listsec0(i) .ne. listsec0_ok(i)) stop 102
+         if (listsec0(i) .ne. listsec0_ok(i)) stop 104
        end do
 
        do i =0,12
-         if (listsec1(i) .ne. listsec1_ok(i)) stop 103
+         if (listsec1(i) .ne. listsec1_ok(i)) stop 105
        end do
 
-       if (numfields .ne. 1) stop 104
-       if (numlocal .ne. 0)  stop 105
-        print*, 04
+       if (numfields .ne. 1) stop 106
+       if (numlocal .ne. 1)  stop 107
+
+      ! Test gf_getfld.f90 to get data from grib2 message.       
+
        call gf_getfld(fgrib, fgrib_len, 1, 1, 1, gfld, iret)
-       if (iret .ne. 0) stop 106
 
-       if (gfld%version .ne. 2) stop 107
+       if (iret .ne. 0) stop 108
 
-       if (gfld%ndpts .ne. 121) stop 108
+       if (gfld%version .ne. 2) stop 109
 
-       do i =0,(gfld%ndpts-1) 
-        if (gfld%fld(i) .ne. fld_ok(i)) stop 109
+       if (gfld%ndpts .ne. 225) stop 110
+
+       do i =1,(gfld%ndpts) 
+        if (gfld%fld(i) .ne. fld_ok(i)) stop 111
        end do
-       print*, 05
+      ! Release gfld
        call gf_free(gfld)
 
        print *,"SUCCESS!\n"
