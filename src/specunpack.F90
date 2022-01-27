5db615e4c80de18d0b38b91ce8ff9cd8a5ed9488
Brian Curtis <Brian.Curtis@noaa.gov>
2022-01-27 11:49:36 -0500

    Formatting adjustments

--- src/specunpack.F90
+++ src/specunpack.F90
@@ -17,65 +17,65 @@
 !>    @param[out] fld Contains the unpacked data values.
 !>
 !>    @author Stephen Gilbert @date 2002-12-19
-subroutine specunpack(cpack,len,idrstmpl,ndpts,JJ,KK,MM,fld)
+subroutine specunpack(cpack, len, idrstmpl, ndpts, JJ, KK, MM, fld)
 
   character(len=1),intent(in) :: cpack(len)
-  integer,intent(in) :: ndpts,len,JJ,KK,MM
+  integer,intent(in) :: ndpts, len, JJ, KK, MM
   integer,intent(in) :: idrstmpl(*)
   real,intent(out) :: fld(ndpts)
 
-  integer :: ifld(ndpts),Ts
+  integer :: ifld(ndpts), Ts
   integer(4) :: ieee
-  real :: ref,bscale,dscale,unpk(ndpts)
+  real :: ref, bscale, dscale, unpk(ndpts)
   real,allocatable :: pscale(:)
 
   ieee = idrstmpl(1)
-  call rdieee(ieee,ref,1)
-  bscale = 2.0**real(idrstmpl(2))
-  dscale = 10.0**real(-idrstmpl(3))
+  call rdieee(ieee, ref, 1)
+  bscale = 2.0 ** real(idrstmpl(2))
+  dscale = 10.0 ** real(-idrstmpl(3))
   nbits = idrstmpl(4)
-  Js=idrstmpl(6)
-  Ks=idrstmpl(7)
-  Ms=idrstmpl(8)
-  Ts=idrstmpl(9)
+  Js = idrstmpl(6)
+  Ks = idrstmpl(7)
+  Ms = idrstmpl(8)
+  Ts = idrstmpl(9)
 
-  if (idrstmpl(10).eq.1) then           ! unpacked floats are 32-bit IEEE
-     !call g2_gbytesc(cpack,ifld,0,32,0,Ts)
-     call rdieee(cpack,unpk,Ts)          ! read IEEE unpacked floats
-     iofst=32*Ts
-     call g2_gbytesc(cpack,ifld,iofst,nbits,0,ndpts-Ts)  ! unpack scaled data
+  if (idrstmpl(10) .eq. 1) then           ! unpacked floats are 32-bit IEEE
+     !call g2_gbytesc(cpack, ifld, 0, 32, 0, Ts)
+     call rdieee(cpack, unpk, Ts)          ! read IEEE unpacked floats
+     iofst = 32 * Ts
+     call g2_gbytesc(cpack, ifld, iofst, nbits, 0, ndpts - Ts)  ! unpack scaled data
      !
      !   Calculate Laplacian scaling factors for each possible wave number.
      !
-     allocate(pscale(JJ+MM))
-     tscale=real(idrstmpl(5))*1E-6
-     do n=Js,JJ+MM
-        pscale(n)=real(n*(n+1))**(-tscale)
+     allocate(pscale(JJ + MM))
+     tscale = real(idrstmpl(5)) * 1E-6
+     do n = Js, JJ + MM
+        pscale(n) = real(n *( n + 1)) ** (-tscale)
      enddo
      !
      !   Assemble spectral coeffs back to original order.
      !
-     inc=1
-     incu=1
-     incp=1
-     do m=0,MM
-        Nm=JJ      ! triangular or trapezoidal
-        if ( KK .eq. JJ+MM ) Nm=JJ+m          ! rhombodial
-        Ns=Js      ! triangular or trapezoidal
-        if ( Ks .eq. Js+Ms ) Ns=Js+m          ! rhombodial
-        do n=m,Nm
-           if (n.le.Ns .AND. m.le.Ms) then    ! grab unpacked value
-              fld(inc)=unpk(incu)         ! real part
-              fld(inc+1)=unpk(incu+1)     ! imaginary part
-              inc=inc+2
-              incu=incu+2
+     inc = 1
+     incu = 1
+     incp = 1
+     do m = 0, MM
+        Nm = JJ      ! triangular or trapezoidal
+        if ( KK .eq. JJ + MM ) Nm = JJ + m          ! rhombodial
+        Ns = Js      ! triangular or trapezoidal
+        if ( Ks .eq. Js + Ms ) Ns = Js + m          ! rhombodial
+        do n = m, Nm
+           if (n .le. Ns .AND. m .le. Ms) then    ! grab unpacked value
+              fld(inc) = unpk(incu)         ! real part
+              fld(inc + 1) = unpk(incu + 1)     ! imaginary part
+              inc = inc + 2
+              incu = incu + 2
            else                         ! Calc coeff from packed value
-              fld(inc)=((real(ifld(incp))*bscale)+ref)* &
-                   dscale*pscale(n)           ! real part
-              fld(inc+1)=((real(ifld(incp+1))*bscale)+ref)* &
-                   dscale*pscale(n)           ! imaginary part
-              inc=inc+2
-              incp=incp+2
+              fld(inc) = ((real(ifld(incp)) * bscale) + ref) * &
+                   dscale * pscale(n)           ! real part
+              fld(inc + 1)=((real(ifld(incp + 1)) * bscale) + ref) * &
+                   dscale * pscale(n)           ! imaginary part
+              inc = inc + 2
+              incp = incp + 2
            endif
         enddo
      enddo
@@ -83,8 +83,8 @@ subroutine specunpack(cpack,len,idrstmpl,ndpts,JJ,KK,MM,fld)
      deallocate(pscale)
 
   else
-     print *,'specunpack: Cannot handle 64 or 128-bit floats.'
-     fld=0.0
+     print *, 'specunpack: Cannot handle 64 or 128-bit floats.'
+     fld = 0.0
      return
   endif
 
