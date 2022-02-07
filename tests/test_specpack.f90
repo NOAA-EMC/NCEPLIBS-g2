9c30882e99ada9e41fed9c34f49878ba52031c3f
Brian Curtis <Brian.Curtis@noaa.gov>
2022-02-07 10:22:26 -0500

    Updates in testing of specpack

--- tests/test_specpack.f90
+++ tests/test_specpack.f90
@@ -1,36 +1,48 @@
 program test_specpack
+    implicit none
 
     integer, parameter :: idrstmplen = 10
     integer, parameter :: ndpts = 6
-    integer :: idrstmpl(idrstmplen)
-    real(8) :: fld(ndpts), fld2(ndpts)
+    integer(4) :: idrstmpl(idrstmplen)
+    real(8) :: fld(ndpts)
+    ! real(4) :: fld_in(ndpts)
+    real(8) :: fld2(ndpts)
     integer :: JJ, KK, MM
-    character(len = 1) :: cpack
+    character(len=1) :: cpack(24)
     integer :: lcpack
+    integer :: i
 
-    fld = (/ 1.0d0, 1.2d0, 1.4d0, 1.6d0, 1.8d0, 2.0d0 /)
+    ! fld_in = (/ cmplx(1.0000, 2), cmplx(1.2000, 1), cmplx(1.4000, 3), cmplx(1.6000, 4), cmplx(1.8000, 2), cmplx(2.0000, 1) /)
+    fld = (/ 1.0000d0, 1.2000d0, 1.4000d0, 1.6000d0, 1.8000d0, 2.0000d0/)
     JJ = 1
     KK = 1
     MM = 1
 
-    idrstmpl(1) = 1.0d0
-    idrstmpl(2) = 1
-    idrstmpl(3) = 1
-    idrstmpl(4) = 8
-    idrstmpl(5) = 10e6
-    idrstmpl(6) = 2
-    idrstmpl(7) = 2
-    idrstmpl(8) = 2
+    idrstmpl(1) = 1
+    idrstmpl(2) = 0
+    idrstmpl(3) = 0
+    idrstmpl(4) = 64
+    idrstmpl(5) = 1
+    idrstmpl(6) = 1
+    idrstmpl(7) = 1
+    idrstmpl(8) = 1
     idrstmpl(9) = 6
-    idrstmpl(10) = 1
+    idrstmpl(10) = 2
 
-    print *, 'fld going in: ', fld
+    ! fld = fld_in
+    print *, 'fld: ', fld
+
     call specpack(fld, ndpts, JJ, KK, MM, idrstmpl, cpack, lcpack)
 
-    print *, 'Length of cpack: ', lcpack
+    ! print *, 'lcpack: ', lcpack
+    if (lcpack .ne. 24) stop 3
 
     call specunpack(cpack, lcpack, idrstmpl, ndpts, JJ, KK, MM, fld2)
-
     print *, 'fld2: ', fld2
+    print *, 'ndpts: ', ndpts
+    do i = 1, ndpts
+        print *, 'fld, fld2, fld - fld2:', fld(i), fld2(i), fld(i)-fld2(i)
+        if (abs(fld(i) - fld2(i)) .gt. 0.000000001) stop 33
+    end do
 
 end program test_specpack
