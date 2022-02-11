07696aa228a5a03d8c0eebee978a5363a8369544
Brian Curtis <Brian.Curtis@noaa.gov>
2022-02-11 12:53:39 -0500

    Pushing what I have and moving on

--- tests/test_getg2i.f90
+++ tests/test_getg2i.f90
@@ -2,12 +2,12 @@
 !
 ! Brian Curtis 02/07/2022
 program test_getg2i
-    use grib_mod
+!    use grib_mod
     use bacio_module
     implicit none
 
     integer :: lugi = 3
-    character(len=1), pointer, dimension(:) :: cbuf
+    character(len=1), pointer, dimension(:) :: cbuf(:)
     integer :: nlen, nnum, iret
 
     call baopenr(lugi, "gdaswave.t00z.wcoast.0p16.f000.grib2.idx", iret)
@@ -16,8 +16,8 @@ program test_getg2i
         stop 3
     end if
 
+
     call getg2i(lugi, cbuf, nlen, nnum, iret)
-    print *, 'iret: ', iret
     print *, 'nlen, nnum: ', nlen, nnum
     if (iret .ne. 0) then
         print *, 'getg2i failed with iret value: ', iret
