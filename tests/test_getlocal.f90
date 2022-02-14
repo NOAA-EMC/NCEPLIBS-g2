4320ce8389e82c1aaf218d19f601ab3b6c88b0f4
Brian Curtis <Brian.Curtis@noaa.gov>
2022-02-14 15:07:46 -0500

    Fixes to tests using new creategrib module, writing getlocal test

--- tests/test_getlocal.f90
+++ tests/test_getlocal.f90
@@ -3,5 +3,28 @@
 ! Brian Curtis 2/11/2022
 
 program test_getlocal
+    use creategrib
     implicit none
     
+    integer :: lengrib
+    character :: cgrib(200)
+    integer :: localnum = 1
+    character(len=1) :: csec2(3), csec2_comp(3)
+    integer :: lcsec2, ierr, i
+
+    call create_cgrib(cgrib, lengrib)
+    print *, 'Testing call to getlocal and comparing results...'
+    call getlocal(cgrib, lengrib, localnum, csec2, lcsec2, ierr)
+
+    csec2_comp = (/ achar(1), achar(2), achar(3) /)
+
+    if (ierr .ne. 0) stop 3
+    if (lcsec2 .ne. 3) stop 4
+
+    do i = 1, 3
+        if (csec2(i) .ne. csec2_comp(i)) stop 5
+    end do
+
+    print *, 'SUCCESS!!!...'
+
+end program test_getlocal
