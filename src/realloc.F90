3319a8996e1f3900b0acc541e9e8a75912b4d613
Edward Hartnett <edward.hartnett@noaa.gov>
2022-07-21 04:08:09 -0600

    adding test for realloc module

--- src/realloc.F90
+++ src/realloc.F90
@@ -1,8 +1,11 @@
 !> @file
-!> @brief This module contains routines to reorganize the data in
-!> memory.
+!> @brief This module contains routines to reallocate memory and
+!> reorganize the data in memory.
 !> @author Stephen Gilbert @date 2000-10-01
 
+!> @brief This module contains routines to reallocate memory and
+!> reorganize the data in memory.
+!>
 !> This module contains three subroutines to reorganize the integer,
 !> real and character data in memory into one dimensional array and
 !> back to memory.
@@ -21,123 +24,148 @@ module re_alloc
 
 contains
 
-  !> This subroutine reorganize character type data in memory
-  !> into one one dimensional array.
+  !> This subroutine reorganizes character type data in memory
+  !> into one one-dimensional array.
   !>
   !> @param[inout] c pointer for data in memory.
-  !> @param[in] n dimension for data in memory.
-  !> @param[in] m dimension for allocatable array.
+  !> @param[in] n dimension for data in memory. This is how much data
+  !> is currently stored in the array.
+  !> @param[in] m dimension for allocatable array. This is the size
+  !> the array will be after the allocation of memory.
   !> @param[out] istat scalar INTEGER variable for allocate.
+  !>
+  !> @return
+  !> - 0 No error.
+  !> - 10 Incorrect dimension.
+  !> - other Allocation error.
+  !>
   !> @author Stephen Gilbert @date 2000-10-01
-  subroutine realloc_c1(c,n,m,istat)
-    character(len=1),pointer,dimension(:) :: c
-    integer,intent(in) :: n,m
-    integer,intent(out) :: istat
+  subroutine realloc_c1(c, n, m, istat)
+    character(len=1), pointer, dimension(:) :: c
+    integer, intent(in) :: n, m
+    integer, intent(out) :: istat
     integer :: num
-    character(len=1),pointer,dimension(:) :: tmp
+    character(len=1), pointer, dimension(:) :: tmp
 
-    istat=0
-    if ( (n<0) .OR. (m<=0) ) then
-       istat=10
+    istat = 0
+    if (n < 0 .or. m <= 0) then
+       istat = 10
        return
     endif
 
-    if ( .not. associated(c) ) then
-       allocate(c(m),stat=istat)   ! allocate new memory
+    if (.not. associated(c)) then
+       allocate(c(m), stat = istat)   ! allocate new memory
        return
     endif
 
-    tmp=>c                      ! save pointer to original mem
+    tmp => c                      ! save pointer to original mem
     nullify(c)
-    allocate(c(m),stat=istat)   ! allocate new memory
-    if ( istat /= 0 ) then
-       c=>tmp
+    allocate(c(m), stat = istat)   ! allocate new memory
+    if (istat /= 0) then
+       c => tmp
        return
     endif
-    if ( n /= 0 ) then
-       num=min(n,m)
-       c(1:num)=tmp(1:num)      ! copy data from orig mem to new loc.
+    if (n /= 0) then
+       num=min(n, m)
+       c(1:num) = tmp(1:num)      ! copy data from orig mem to new loc.
     endif
     deallocate(tmp)             ! deallocate original memory
     return
   end subroutine realloc_c1
 
-  !> This subroutine reorganize real type data in memory into
-  !> one one dimensional array.
+  !> This subroutine reorganizes real type data in memory into
+  !> one one-dimensional array.
+  !>
   !> @param[inout] c pointer for data in memory.
-  !> @param[in] n dimension for data in memory.
-  !> @param[in] m dimension for allocatable array.
+  !> @param[in] n dimension for data in memory. This is how much data
+  !> is currently stored in the array.
+  !> @param[in] m dimension for allocatable array. This is the size
+  !> the array will be after the allocation of memory.
   !> @param[out] istat scalar INTEGER variable for allocate.
+  !>
+  !> @return
+  !> - 0 No error.
+  !> - 10 Incorrect dimension.
+  !> - other Allocation error.
+  !>
   !> @author Stephen Gilbert @date 2000-10-01
-  subroutine realloc_r(c,n,m,istat)
-    real,pointer,dimension(:) :: c
-    integer,intent(in) :: n,m
-    integer,intent(out) :: istat
+  subroutine realloc_r(c, n, m, istat)
+    real, pointer, dimension(:) :: c
+    integer, intent(in) :: n, m
+    integer, intent(out) :: istat
     integer :: num
-    real,pointer,dimension(:) :: tmp
+    real, pointer, dimension(:) :: tmp
 
-    istat=0
-    if ( (n<0) .OR. (m<=0) ) then
-       istat=10
+    istat = 0
+    if (n < 0 .or. m <= 0) then
+       istat = 10
        return
     endif
 
-    if ( .not. associated(c) ) then
-       allocate(c(m),stat=istat)   ! allocate new memory
+    if (.not. associated(c)) then
+       allocate(c(m), stat = istat)   ! allocate new memory
        return
     endif
 
-    tmp=>c                      ! save pointer to original mem
+    tmp => c                      ! save pointer to original mem
     nullify(c)
-    allocate(c(m),stat=istat)   ! allocate new memory
-    if ( istat /= 0 ) then
-       c=>tmp
+    allocate(c(m), stat = istat)   ! allocate new memory
+    if (istat /= 0) then
+       c => tmp
        return
     endif
-    if ( n /= 0 ) then
-       num=min(n,m)
-       c(1:num)=tmp(1:num)      ! copy data from orig mem to new loc.
+    if (n /= 0) then
+       num = min(n, m)
+       c(1:num) = tmp(1:num)      ! copy data from orig mem to new loc.
     endif
     deallocate(tmp)             ! deallocate original memory
     return
   end subroutine realloc_r
 
-  !> This subroutine reorganize integer type data in memory into
-  !> one one dimensional array.
+  !> This subroutine reorganizes integer type data in memory into
+  !> one one-dimensional array.
   !>
   !> @param[inout] c pointer for data in memory.
-  !> @param[in] n dimension for data in memory.
-  !> @param[in] m dimension for allocatable array.
+  !> @param[in] n dimension for data in memory. This is how much data
+  !> is currently stored in the array.
+  !> @param[in] m dimension for allocatable array. This is the size
+  !> the array will be after the allocation of memory.
   !> @param[out] istat scalar INTEGER variable for allocate.
+  !>
+  !> @return
+  !> - 0 No error.
+  !> - 10 Incorrect dimension.
+  !> - other Allocation error.
+  !>
   !> @author Stephen Gilbert @date 2000-10-01
-  subroutine realloc_i(c,n,m,istat)
-    integer,pointer,dimension(:) :: c
-    integer,intent(in) :: n,m
-    integer,intent(out) :: istat
+  subroutine realloc_i(c, n, m, istat)
+    integer, pointer, dimension(:) :: c
+    integer, intent(in) :: n, m
+    integer, intent(out) :: istat
     integer :: num
-    integer,pointer,dimension(:) :: tmp
+    integer, pointer, dimension(:) :: tmp
 
-    istat=0
-    if ( (n<0) .OR. (m<=0) ) then
-       istat=10
+    istat = 0
+    if (n < 0 .or. m <= 0) then
+       istat = 10
        return
     endif
 
-    if ( .not. associated(c) ) then
-       allocate(c(m),stat=istat)   ! allocate new memory
+    if (.not. associated(c)) then
+       allocate(c(m), stat = istat)   ! allocate new memory
        return
     endif
 
-    tmp=>c                      ! save pointer to original mem
+    tmp => c                      ! save pointer to original mem
     nullify(c)
-    allocate(c(m),stat=istat)   ! allocate new memory
-    if ( istat /= 0 ) then
-       c=>tmp
+    allocate(c(m), stat = istat)   ! allocate new memory
+    if (istat /= 0) then
+       c => tmp
        return
     endif
-    if ( n /= 0 ) then
-       num=min(n,m)
-       c(1:num)=tmp(1:num)      ! copy data from orig mem to new loc.
+    if (n /= 0) then
+       num=min(n, m)
+       c(1:num) = tmp(1:num)      ! copy data from orig mem to new loc.
     endif
     deallocate(tmp)             ! deallocate original memory
     return
