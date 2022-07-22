69eca83de77201373a84cb4da4d1cb1853bd5c65
Edward Hartnett <edward.hartnett@noaa.gov>
2022-07-22 08:35:41 -0600

    more testing

--- tests/test_ixgb2.f90
+++ tests/test_ixgb2.f90
@@ -18,21 +18,32 @@ program test_ixgb2
      end subroutine ixgb2
   end interface
 
+  ! This will not work, because we try to read more bytes than the file holds.
+  lgrib = 1000
+  call baopenr(lugi, "testdata_g2grids", iret)
+  if (iret .ne. 0) stop 3
+  call ixgb2(lugi, lskip, lgrib, cbuf, numfld, mlen, iret)
+  if (iret .ne. 2) stop 4
+  call baclose(lugi, iret)
+  if (iret .ne. 0) stop 5
+  deallocate(cbuf)
+
   ! This will not work, because it's not a GRIB2 file.
+  lgrib = 96
   call baopenr(lugi, "testdata_g2grids", iret)
   if (iret .ne. 0) stop 3
   call ixgb2(lugi, lskip, lgrib, cbuf, numfld, mlen, iret)
   if (iret .ne. 3) stop 4
   call baclose(lugi, iret)
   if (iret .ne. 0) stop 5
-  
+  deallocate(cbuf)
+
   ! Now open a real GRIB2 file.
   call baopenr(lugi, "WW3_Regional_US_West_Coast_20220718_0000.grib2", iret)
   if (iret .ne. 0) stop 100
 
   ! Skip the first 202 bytes of the test file.
   lskip = 202
-  lgrib = 1000
   call ixgb2(lugi, lskip, lgrib, cbuf, numfld, mlen, iret)
   if (iret .ne. 0) stop 101
   if (numfld .ne. 1 .or. mlen .ne. 200) stop 102
