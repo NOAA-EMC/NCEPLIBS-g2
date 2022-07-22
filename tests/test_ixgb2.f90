755fd558ada0fc2586c8bdd558947ab7e913627d
Edward Hartnett <edward.hartnett@noaa.gov>
2022-07-22 08:50:23 -0600

    more testing

--- tests/test_ixgb2.f90
+++ tests/test_ixgb2.f90
@@ -10,6 +10,7 @@ program test_ixgb2
   integer :: lugi = 3
   character(len=1), pointer, dimension(:) :: cbuf(:)
   integer :: lskip, lgrib, numfld, mlen, iret
+  integer :: i
 
   interface
      subroutine ixgb2(lugb, lskip, lgrib, cbuf, numfld, mlen, iret)
@@ -17,6 +18,28 @@ program test_ixgb2
        character(len = 1),pointer,dimension(:) :: cbuf
      end subroutine ixgb2
   end interface
+
+  ! This is the index we expect to be created from the test file.
+  character :: expected_cbuf(200) = (/ char(0),  char(0),  char(0),  char(200),  char(0),  char(0),  char(0),  &
+       char(202),  char(0),  char(0),  char(0),  char(0),  char(0),  char(0),  char(0),  char(37),  char(0),  &
+       char(0),  char(0),  char(109),  char(0),  char(0),  char(0),  char(143),  char(0),  char(0),  char(0),  &
+       char(166),  char(0),  char(0),  char(18),  char(113),  char(0),  char(0),  char(0),  char(0),  char(0),  &
+       char(0),  char(0),  char(96),  char(2),  char(10),  char(0),  char(1),  char(0),  char(0),  char(0),  &
+       char(21),  char(1),  char(0),  char(7),  char(0),  char(0),  char(2),  char(1),  char(1),  char(7),  &
+       char(230),  char(7),  char(18),  char(0),  char(0),  char(0),  char(0),  char(1),  char(0),  char(0),  &
+       char(0),  char(72),  char(3),  char(0),  char(0),  char(0),  char(142),  char(39),  char(0),  char(0),  &
+       char(0),  char(0),  char(6),  char(0),  char(0),  char(0),  char(0),  char(0),  char(0),  char(0),  &
+       char(0),  char(0),  char(0),  char(0),  char(0),  char(0),  char(0),  char(0),  char(0),  char(0),  &
+       char(0),  char(241),  char(0),  char(0),  char(0),  char(151),  char(0),  char(0),  char(0),  char(0),  &
+       char(0),  char(0),  char(0),  char(0),  char(2),  char(250),  char(240),  char(128),  char(12),  char(132),  &
+       char(88),  char(128),  char(48),  char(1),  char(125),  char(120),  char(64),  char(14),  char(230),  char(178),  &
+       char(128),  char(0),  char(2),  char(139),  char(11),  char(0),  char(2),  char(139),  char(11),  char(0),  &
+       char(0),  char(0),  char(0),  char(34),  char(4),  char(0),  char(0),  char(0),  char(0),  char(0),  char(5),  &
+       char(2),  char(0),  char(11),  char(0),  char(0),  char(0),  char(1),  char(0),  char(0),  char(0),  char(0),  &
+       char(1),  char(0),  char(0),  char(0),  char(0),  char(1),  char(255),  char(0),  char(0),  char(0),  char(0),  &
+       char(0),  char(0),  char(0),  char(0),  char(23),  char(5),  char(0),  char(0),  char(33),  char(36),  char(0),  &
+       char(40),  char(65),  char(136),  char(0),  char(0),  char(0),  char(0),  char(0),  char(2),  char(9),  char(0),  &
+       char(0),  char(255),  char(0),  char(0),  char(17),  char(203),  char(6),  char(0) /)
 
   ! This will not work, because we try to read more bytes than the file holds.
   lgrib = 1000
@@ -47,6 +70,10 @@ program test_ixgb2
   call ixgb2(lugi, lskip, lgrib, cbuf, numfld, mlen, iret)
   if (iret .ne. 0) stop 101
   if (numfld .ne. 1 .or. mlen .ne. 200) stop 102
+  do i = 1, mlen
+!     print *, 'char(', ichar(cbuf(i)), '), '
+     if (cbuf(i) .ne. expected_cbuf(i)) stop 103
+  end do
   deallocate(cbuf)
   
   call baclose(lugi, iret)
