929bede5123afb5c1dc8c0f837ca856acec5d3cd
Edward Hartnett <edward.hartnett@noaa.gov>
2022-07-25 07:33:45 -0600

    more working on CI problem

--- src/ixgb2.F90
+++ src/ixgb2.F90
@@ -96,6 +96,7 @@ subroutine ixgb2(lugb, lskip, lgrib, cbuf, numfld, mlen, iret)
   do i = 1, 20
      print *, ichar(cbread(i))
   end do
+  print *, lbread, ibread
   if (lbread .ne. ibread) then
      iret = 2
      return
