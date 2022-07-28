d5b5eedb24ff80d984fb263d098a6494fa0ca7e2
Edward Hartnett <edward.hartnett@noaa.gov>
2022-07-28 14:22:50 -0600

    adding test_gdt2gds

---
+++ tests/test_gdt2gds.f90
@@ -0,0 +1,25 @@
+! This is a test program for NCEPLIBS-g2.
+!
+! This program tests the gdt2gds.F90 file.
+!
+! Ed Hartnett 7/28/22
+program test_gdt2gds
+  use grib_mod, only: G2_VERSION
+  implicit none
+
+  character(len=5) :: version_in
+
+  print *, 'Testing version string...'
+  print *, G2_VERSION
+
+  ! Open the VERSION file and get the value.
+  open(20, file = 'VERSION', status = 'old')
+  read(20,'(A5)') version_in
+  close(20)
+  print *, G2_VERSION, 'g2lib-' // version_in
+
+  ! Check the value of G2_VERSION.
+  if (G2_VERSION .ne. 'g2lib-' // version_in) stop 2
+  
+  print *, 'SUCCESS!'
+end program test_gdt2gds
