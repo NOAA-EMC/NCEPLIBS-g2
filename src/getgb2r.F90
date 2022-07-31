e76203a18b787d866d44d79ab56bc3ea967d754f
Edward Hartnett <edward.hartnett@noaa.gov>
2022-07-31 07:49:46 -0600

    code cleanup

--- src/getgb2r.F90
+++ src/getgb2r.F90
@@ -37,68 +37,67 @@
 !> no longer needed, by a call to subroutine gf_free().
 !>
 !>    @author Stephen Gilbert @date 2002-01-11
-SUBROUTINE GETGB2R(LUGB,CINDEX,GFLD,IRET)
-
+SUBROUTINE GETGB2R(LUGB, CINDEX, GFLD, IRET)
   USE GRIB_MOD
   implicit none
 
-  INTEGER,INTENT(IN) :: LUGB
-  CHARACTER(LEN=1),INTENT(IN) :: CINDEX(*)
-  INTEGER,INTENT(OUT) :: IRET
+  INTEGER, INTENT(IN) :: LUGB
+  CHARACTER(LEN = 1), INTENT(IN) :: CINDEX(*)
+  INTEGER, INTENT(OUT) :: IRET
   TYPE(GRIBFIELD) :: GFLD
 
-  INTEGER :: LSKIP,SKIP6,SKIP7
-  CHARACTER(LEN=1):: CSIZE(4)
-  CHARACTER(LEN=1),ALLOCATABLE :: CTEMP(:)
-  real,pointer,dimension(:) :: newfld
+  INTEGER :: LSKIP, SKIP6, SKIP7
+  CHARACTER(LEN = 1):: CSIZE(4)
+  CHARACTER(LEN = 1), ALLOCATABLE :: CTEMP(:)
+  real, pointer, dimension(:) :: newfld
   integer :: idum, ierr, ilen, iofst, iskip, j, lread, n
 
   interface
-     subroutine gf_unpack6(cgrib,lcgrib,iofst,ngpts,ibmap, &
-          bmap,ierr)
-       character(len=1),intent(in) :: cgrib(lcgrib)
-       integer,intent(in) :: lcgrib,ngpts
-       integer,intent(inout) :: iofst
-       integer,intent(out) :: ibmap
-       integer,intent(out) :: ierr
-       logical*1,pointer,dimension(:) :: bmap
+     subroutine gf_unpack6(cgrib, lcgrib, iofst, ngpts, ibmap, &
+          bmap, ierr)
+       character(len = 1), intent(in) :: cgrib(lcgrib)
+       integer, intent(in) :: lcgrib, ngpts
+       integer, intent(inout) :: iofst
+       integer, intent(out) :: ibmap
+       integer, intent(out) :: ierr
+       logical*1, pointer, dimension(:) :: bmap
      end subroutine gf_unpack6
-     subroutine gf_unpack7(cgrib,lcgrib,iofst,igdsnum,igdstmpl, &
-          idrsnum,idrstmpl,ndpts,fld,ierr)
-       character(len=1),intent(in) :: cgrib(lcgrib)
-       integer,intent(in) :: lcgrib,ndpts,idrsnum,igdsnum
-       integer,intent(inout) :: iofst
-       integer,pointer,dimension(:) :: idrstmpl,igdstmpl
-       integer,intent(out) :: ierr
-       real,pointer,dimension(:) :: fld
+     subroutine gf_unpack7(cgrib, lcgrib, iofst, igdsnum, igdstmpl, &
+          idrsnum, idrstmpl, ndpts, fld, ierr)
+       character(len = 1), intent(in) :: cgrib(lcgrib)
+       integer, intent(in) :: lcgrib, ndpts, idrsnum, igdsnum
+       integer, intent(inout) :: iofst
+       integer, pointer, dimension(:) :: idrstmpl, igdstmpl
+       integer, intent(out) :: ierr
+       real, pointer, dimension(:) :: fld
      end subroutine gf_unpack7
   end interface
 
   !  GET INFO
-  NULLIFY(gfld%bmap,gfld%fld)
-  IRET=0
-  CALL G2_GBYTEC(CINDEX,LSKIP,4*8,4*8)
-  CALL G2_GBYTEC(CINDEX,SKIP6,24*8,4*8)
-  CALL G2_GBYTEC(CINDEX,SKIP7,28*8,4*8)
+  NULLIFY(gfld%bmap, gfld%fld)
+  IRET = 0
+  CALL G2_GBYTEC(CINDEX, LSKIP, 4 * 8, 4 * 8)
+  CALL G2_GBYTEC(CINDEX, SKIP6, 24 * 8, 4 * 8)
+  CALL G2_GBYTEC(CINDEX, SKIP7, 28 * 8, 4 * 8)
 
 
   !  READ AND UNPACK BIT_MAP, IF PRESENT
-  IF ( gfld%ibmap.eq.0.OR.gfld%ibmap.eq.254 ) THEN
-     ISKIP=LSKIP+SKIP6
-     CALL BAREAD(LUGB,ISKIP,4,LREAD,CSIZE)    ! GET LENGTH OF SECTION
-     CALL G2_GBYTEC(CSIZE,ILEN,0,32)
+  IF (gfld%ibmap .eq. 0 .OR. gfld%ibmap .eq. 254) THEN
+     ISKIP = LSKIP+SKIP6
+     CALL BAREAD(LUGB, ISKIP, 4, LREAD, CSIZE)    ! GET LENGTH OF SECTION
+     CALL G2_GBYTEC(CSIZE, ILEN, 0, 32)
      ALLOCATE(CTEMP(ILEN))
-     CALL BAREAD(LUGB,ISKIP,ILEN,LREAD,CTEMP)  ! READ IN SECTION
-     IF (ILEN.NE.LREAD) THEN
-        IRET=97
+     CALL BAREAD(LUGB, ISKIP, ILEN, LREAD, CTEMP)  ! READ IN SECTION
+     IF (ILEN .NE. LREAD) THEN
+        IRET = 97
         DEALLOCATE(CTEMP)
         RETURN
      ENDIF
-     IOFST=0
-     CALL GF_UNPACK6(CTEMP,ILEN,IOFST,gfld%ngrdpts,idum, &
-          gfld%bmap,ierr)
-     IF (IERR.NE.0) THEN
-        IRET=98
+     IOFST = 0
+     CALL GF_UNPACK6(CTEMP, ILEN, IOFST, gfld%ngrdpts, idum, &
+          gfld%bmap, ierr)
+     IF (IERR .NE. 0) THEN
+        IRET = 98
         DEALLOCATE(CTEMP)
         RETURN
      ENDIF
@@ -106,23 +105,23 @@ SUBROUTINE GETGB2R(LUGB,CINDEX,GFLD,IRET)
   ENDIF
 
   !  READ AND UNPACK DATA FIELD
-  ISKIP=LSKIP+SKIP7
-  CALL BAREAD(LUGB,ISKIP,4,LREAD,CSIZE)    ! GET LENGTH OF SECTION
-  CALL G2_GBYTEC(CSIZE,ILEN,0,32)
-  if (ilen.lt.6) ilen=6
+  ISKIP = LSKIP+SKIP7
+  CALL BAREAD(LUGB, ISKIP, 4, LREAD, CSIZE)    ! GET LENGTH OF SECTION
+  CALL G2_GBYTEC(CSIZE, ILEN, 0, 32)
+  if (ilen .lt. 6) ilen = 6
   ALLOCATE(CTEMP(ILEN))
-  CALL BAREAD(LUGB,ISKIP,ILEN,LREAD,CTEMP)  ! READ IN SECTION
-  IF (ILEN.NE.LREAD) THEN
-     IRET=97
+  CALL BAREAD(LUGB, ISKIP, ILEN, LREAD, CTEMP)  ! READ IN SECTION
+  IF (ILEN .NE. LREAD) THEN
+     IRET = 97
      DEALLOCATE(CTEMP)
      RETURN
   ENDIF
-  IOFST=0
-  CALL GF_UNPACK7(CTEMP,ILEN,IOFST,gfld%igdtnum,gfld%igdtmpl, &
-       gfld%idrtnum,gfld%idrtmpl,gfld%ndpts, &
-       gfld%fld,ierr)
-  IF (IERR.NE.0) THEN
-     IRET=98
+  IOFST = 0
+  CALL GF_UNPACK7(CTEMP, ILEN, IOFST, gfld%igdtnum, gfld%igdtmpl, &
+       gfld%idrtnum, gfld%idrtmpl, gfld%ndpts, &
+       gfld%fld, ierr)
+  IF (IERR .NE. 0) THEN
+     IRET = 98
      DEALLOCATE(CTEMP)
      RETURN
   ENDIF
@@ -130,23 +129,23 @@ SUBROUTINE GETGB2R(LUGB,CINDEX,GFLD,IRET)
 
   !  If bitmap is used with this field, expand data field
   !  to grid, if possible.
-  if ( gfld%ibmap .ne. 255 .AND. associated(gfld%bmap) ) then
+  if (gfld%ibmap .ne. 255 .AND. associated(gfld%bmap)) then
      allocate(newfld(gfld%ngrdpts))
-     !newfld=0.0
-     !newfld=unpack(lgfld%fld,lgfld%bmap,newfld)
-     n=1
-     do j=1,gfld%ngrdpts
-        if ( gfld%bmap(j) ) then
-           newfld(j)=gfld%fld(n)
-           n=n+1
+     !newfld = 0.0
+     !newfld = unpack(lgfld%fld, lgfld%bmap, newfld)
+     n = 1
+     do j = 1, gfld%ngrdpts
+        if (gfld%bmap(j)) then
+           newfld(j) = gfld%fld(n)
+           n = n+1
         else
-           newfld(j)=0.0
+           newfld(j) = 0.0
         endif
      enddo
      deallocate(gfld%fld);
-     gfld%fld=>newfld;
-     gfld%expanded=.true.
+     gfld%fld = >newfld;
+     gfld%expanded = .true.
   else
-     gfld%expanded=.true.
+     gfld%expanded = .true.
   endif
 END SUBROUTINE GETGB2R
