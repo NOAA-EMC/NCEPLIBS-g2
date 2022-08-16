315d28ef2274a97f81618381aa84483f2322d48a
Edward Hartnett <edward.hartnett@noaa.gov>
2022-08-16 07:02:29 -0600

    fixing warnings

--- src/set_log_level.F90
+++ src/set_log_level.F90
@@ -13,6 +13,7 @@
 !>
 !> @param[in] log_level Set to -1 to show nothing, 0 for errors only,
 !> and 1 or higher for progressively more verbose logging levels.
+!> @return 0 for success.
 !>
 !> @author Ed Hartnett @date 2022-08-16
 function g2_set_log_level(log_level)
@@ -25,7 +26,7 @@ function g2_set_log_level(log_level)
      function g2c_set_log_level(log_level) bind(c, name="g2c_set_log_level")
        use iso_c_binding
        integer(c_int), value :: log_level
-       integer :: g2c_set_log_level
+       integer(c_int) :: g2c_set_log_level
      end function g2c_set_log_level
   end interface
 
