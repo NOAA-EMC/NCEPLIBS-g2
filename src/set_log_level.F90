34480313aa66d4b6f7c8ab78193b316dc98a8bee
Edward Hartnett <edward.hartnett@noaa.gov>
2022-08-16 06:54:21 -0600

    added set_log_level.F90

---
+++ src/set_log_level.F90
@@ -0,0 +1,34 @@
+!> @file
+!> @brief Set the log level for internal library debugging.
+!> @author Ed Hartnett @date 2022-08-16
+
+!> Set the log level for internal library debugging.
+!>
+
+!> Turn on internal logging for the NCEPLIBS-g2c library. This is only
+!> useful to programmers working on the GRIB libraries.
+!>
+!> If the NCEPLIBS-g2c library was not built with LOGGING=ON, then
+!> setting the log level will have no effect.
+!>
+!> @param[in] log_level Set to -1 to show nothing, 0 for errors only,
+!> and 1 or higher for progressively more verbose logging levels.
+!>
+!> @author Ed Hartnett @date 2022-08-16
+function g2_set_log_level(log_level)
+  implicit none
+
+  integer, intent(in) :: log_level
+  integer g2_set_log_level
+
+  interface
+     function g2c_set_log_level(log_level) bind(c, name="g2c_set_log_level")
+       use iso_c_binding
+       integer(c_int), value :: log_level
+       integer :: g2c_set_log_level
+     end function g2c_set_log_level
+  end interface
+
+  g2_set_log_level = g2c_set_log_level(log_level)
+  
+end function g2_set_log_level
