8c268cf1d5d343f4631604de18e76ec9e1102274
Edward Hartnett <edward.hartnett@noaa.gov>
2022-10-15 20:25:36 +0200

    fixed test_getg2i

--- tests/test_getg2i.F90
+++ tests/test_getg2i.F90
@@ -3,14 +3,15 @@
 ! This program tests getg2i.F90
 !
 ! Brian Curtis 02/07/2022
+! Ed Hartnett
 program test_getg2i
   use bacio_module
   implicit none
-
+  
   integer :: lugi = 3
   character(len=1), pointer, dimension(:) :: cbuf(:)
   integer :: nlen, nnum, iret
-
+  
   interface
      subroutine getg2i(lugi,cbuf,nlen,nnum,iret)
        integer,intent(in) :: lugi
@@ -19,24 +20,23 @@ program test_getg2i
      end subroutine getg2i
   end interface
 
-  call baopenr(lugi, "gdaswave.t00z.wcoast.0p16.f000.grib2.idx", iret)
-  if (iret .ne. 0) then
-     print *, 'baopenr failed with iret value: ', iret
-     stop 3
-  end if
+  print *, 'Testing index file reading with getg2i().'
+  
+  ! Open the test file, generated with the utility grb2index.
+  call baopenr(lugi, "ref_gdaswave.t00z.wcoast.0p16.f000.grb2index", iret)
+  if (iret .ne. 0) stop 3
 
+  ! Check that this is an index file, and read it into buffer cbuf.
   call getg2i(lugi, cbuf, nlen, nnum, iret)
-  print *, 'nlen, nnum: ', nlen, nnum
-  if (iret .ne. 4) then
-      print *, 'getg2i failed with iret value: ', iret
-      stop 4
-  end if
+  if (iret .ne. 0) stop 4
 
+  ! Check results.
+  if (nlen .ne. 3800 .or. nnum .ne. 19) stop 5
+
+  ! Close the file.
   call baclose(lugi, iret)
-  if (iret .ne. 0) then
-     print *, 'baclose failed with iret value: ', iret
-     stop 5
-  end if
+  if (iret .ne. 0) stop 50
+
   print *, 'Success!...'
 
 !  deallocate(cbuf)
