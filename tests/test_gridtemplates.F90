712bd5d4934d3cadf8253b0d1d552079c64cb86e
Edward Hartnett <edward.hartnett@noaa.gov>
2022-10-17 20:09:12 +0200

    improved test

--- tests/test_gridtemplates.F90
+++ tests/test_gridtemplates.F90
@@ -12,7 +12,7 @@ program test_gridtemplates
        1100, 1200, 31, 204, 32768, 32769, 4, 5, 12, 101, 140/)
   integer :: nummap
   integer, parameter :: lat_lon_mapgridlen = 19
-  integer :: map(lat_lon_mapgridlen)
+  integer :: map(20)
   integer :: list_4(lat_lon_mapgridlen) 
   logical :: needext
   integer :: gdtlen
@@ -34,21 +34,24 @@ program test_gridtemplates
   ! An extra 6 values are added to the map.
   call getgridtemplate(4, nummap, map, needext, iret)
   if (nummap .ne. 13 .or. .not. needext) stop 4
-  print *, nummap
-!  print *, map
   list_4(8) = 2
   list_4(9) = 2
   call extgridtemplate(4, list_4, nummap, map)
-  print *, nummap
-  print *, map(13:nummap)
+  if (nummap .ne. 17) stop 5
+  if (map(14) .ne. 4 .or. map(15) .ne. 4 .or. map(16) .ne. -4 .or. map(17) .ne. -4) stop 6
   
   print *, 'Testing getgridtemplate 5, which needs extension...'
   call getgridtemplate(5, nummap, map, needext, iret)
-  if (nummap .ne. 16 .or. .not. needext) stop 4
+  if (nummap .ne. 16 .or. .not. needext) stop 40
+  list_4(8) = 2
+  list_4(9) = 2
+  call extgridtemplate(5, list_4, nummap, map)
+  if (nummap .ne. 20) stop 50
+  if (map(17) .ne. 4 .or. map(18) .ne. 4 .or. map(19) .ne. -4 .or. map(20) .ne. -4) stop 60
 
   print *, 'Testing getgdtlen...'
   gdtlen = getgdtlen(0)
-  if (gdtlen .ne. lat_lon_mapgridlen) stop 40
+  if (gdtlen .ne. lat_lon_mapgridlen) stop 400
   
   print *, 'SUCCESS!'
   
