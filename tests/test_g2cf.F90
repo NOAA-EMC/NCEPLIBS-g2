2d26b934f2b7b2cee59c39aa0393dd5e9e2874fc
Edward Hartnett <edward.hartnett@noaa.gov>
2022-11-21 10:41:04 -0700

    added test_g2cf.F90 file

---
+++ tests/test_g2cf.F90
@@ -0,0 +1,12 @@
+! This is a test program for NCEPLIBS-g2.
+!
+! This program tests the g2cf functions.
+!
+! Ed Hartnett 11/21/22
+program test_g2cf
+  implicit none
+
+  print *, 'Testing g2cf API...'
+
+  print *, 'SUCCESS!'
+end program test_g2cf
