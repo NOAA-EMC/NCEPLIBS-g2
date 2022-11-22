103cbe8ec191bc17b4e31a1ee2a4a38a33ba8838
Edward Hartnett <edward.hartnett@noaa.gov>
2022-11-22 04:47:59 -0700

    more progress

--- tests/test_g2cf.F90
+++ tests/test_g2cf.F90
@@ -6,10 +6,15 @@
 program test_g2cf
   use g2cf
   implicit none
+  character (len = *), parameter :: fileName = "gdaswave.t00z.wcoast.0p16.f000.grib2"
+  integer :: g2cid
   integer :: ierr
 
   print *, 'Testing g2cf API...'
   ierr = g2cf_set_log_level(10)
+
+  ierr = g2cf_open(fileName, 0, g2cid)
+  ierr = g2cf_close(g2cid)
 
   print *, 'SUCCESS!'
 end program test_g2cf
