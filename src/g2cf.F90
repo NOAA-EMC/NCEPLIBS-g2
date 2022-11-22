dbb71eafd36fe0737236c1b8e75267d233abbba5
Edward Hartnett <edward.hartnett@noaa.gov>
2022-11-22 04:45:26 -0700

    more progress

--- src/g2cf.F90
+++ src/g2cf.F90
@@ -45,7 +45,7 @@ contains
        nlen = nlen + 1
     endif
   end function addcnullchar
-
+  
   !> Open a GRIB2 file.
   !>
   !> @param path Path to file.
@@ -59,12 +59,21 @@ contains
   function g2cf_open(path, mode, g2cid) result (status)
     use iso_c_binding
     implicit none
-      
+    
+    interface
+       function g2c_open(path, mode, g2cid) bind(c)
+         use iso_c_binding, only: c_char, c_int
+         character(kind=c_char), intent(in)  :: path(*)
+         integer(c_int), value :: mode
+         integer(c_int), intent(out) :: g2cid
+         integer(c_int) :: g2c_open
+       end function g2c_open
+    end interface
+
     character(len=*), intent(in) :: path
     integer, intent(in) :: mode
     integer, intent(inout) :: g2cid
     integer :: status
-    integer :: g2c_open
     
     integer(c_int) :: cmode, cg2cid, cstatus
     character(len = (len(path) + 1)) :: cpath
@@ -83,6 +92,63 @@ contains
        g2cid   = cg2cid
     endif
     status = cstatus
-    
   end function g2cf_open
+
+  !> Close a GRIB2 file.
+  !>
+  !> @param g2cid File ID.
+  !>
+  !> @return
+  !> - 0 No error
+  !>
+  !> @author Ed Hartnett @date 2022-11-21
+  function g2cf_close(g2cid) result (status)
+    use iso_c_binding
+    implicit none
+      
+    interface
+       function g2c_close(g2cid) bind(c)
+         use iso_c_binding, only: c_int
+         integer(c_int), value :: g2cid
+         integer(c_int) :: g2c_close
+       end function g2c_close
+    end interface
+    
+    integer, intent(in) :: g2cid
+    integer :: status
+    integer(c_int) :: cg2cid, cstatus
+    
+    cg2cid = g2cid
+    cstatus = g2c_close(cg2cid)
+    status = cstatus
+  end function g2cf_close
+
+  !> Turn on logging.
+  !>
+  !> @param log_level Logging level.
+  !>
+  !> @return
+  !> - 0 No error
+  !>
+  !> @author Ed Hartnett @date 2022-11-21
+  function g2cf_set_log_level(log_level) result (status)
+    use iso_c_binding
+    implicit none
+      
+    interface
+       function g2c_set_log_level(log_level) bind(c)
+         use iso_c_binding, only: c_int
+         integer(c_int), value :: log_level
+         integer(c_int) :: g2c_set_log_level
+       end function g2c_set_log_level
+    end interface
+    
+    integer, intent(in) :: log_level
+    integer :: status
+    integer(c_int) :: clog_level, cstatus
+    
+    clog_level = log_level
+    cstatus = g2c_set_log_level(clog_level)
+    status = cstatus
+  end function g2cf_set_log_level
 end module g2cf
--- tests/test_g2cf.F90
+++ tests/test_g2cf.F90
@@ -4,9 +4,12 @@
 !
 ! Ed Hartnett 11/21/22
 program test_g2cf
+  use g2cf
   implicit none
+  integer :: ierr
 
   print *, 'Testing g2cf API...'
+  ierr = g2cf_set_log_level(10)
 
   print *, 'SUCCESS!'
 end program test_g2cf
