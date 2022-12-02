1c1cddcfa4c443c91db6ee555d01a7e8aa384616
Edward Hartnett <edward.hartnett@noaa.gov>
2022-12-02 10:49:53 -0700

    more test cleanup

--- tests/creategrib.F90
+++ tests/creategrib.F90
@@ -20,7 +20,10 @@ contains
     character, intent(out), dimension(lcgrib) :: cgrib
 
     ! Section 0 and 1.
-    integer :: listsec0(2), listsec1(13)
+    ! See https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table0-0.shtml.    
+    integer :: listsec0(2) = (/ 0, 2 /)
+    ! See https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_sect1.shtml.    
+    integer :: listsec1(13) = (/ 7, 4, 2, 24, 0, 2021, 11, 13, 15, 59, 59, 1, 0 /)
 
     ! Section 2.
     integer, parameter :: lcsec2 = 3
@@ -37,13 +40,14 @@ contains
     integer :: ideflist(idefnum)
 
     ! Sections 4-7.
-    integer :: ipdsnum
+    integer :: ipdsnum = 0
     integer, parameter :: ipdstmplen = 15, numcoord = 0
-    integer :: ipdstmpl(ipdstmplen)
+    ! See https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_temp4-0.shtml    
+    integer :: ipdstmpl(ipdstmplen) = (/ 0, 0, 0, 0, 0, 12, 59, 0, 0, 1, 1, 1, 2, 1, 1 /)
     integer :: coordlist(1)
     integer :: idrsnum = 0
     integer, parameter :: idrstmplen = 5
-    integer :: idrstmpl(idrstmplen)
+    integer :: idrstmpl(idrstmplen) = (/ 0, 1, 1, 8, 0 /)
     integer, parameter :: ngrdpts = 4, ibmap = 255
     logical :: bmap(1) = .false.
     real(8) :: fld(ngrdpts) = (/ 1.1, 1.2, 1.3, 1.4 /)
@@ -51,59 +55,6 @@ contains
     ! Section 8
     integer, intent(out) :: lengrib
     integer :: ierr
-
-    print *, 'Testing gribcreate().'
-    !   expected_cpack = (/ char(0), char(93), char(108), char(64) /)
-
-    print *, 'Testing simple call to gribcreate(). Expect and ignore error messages.'
-
-    ! See https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table0-0.shtml.
-    listsec0(1) = 0
-    listsec0(2) = 2
-
-    ! See https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_sect1.shtml.
-    listsec1(1) = 7 ! US National Weather Service - NCEP (WMC)
-    listsec1(2) = 4 ! Environmental Modeling Center
-    listsec1(3) = 2 ! GRIB master tables version number (currently 2)
-    listsec1(4) = 24 ! Version Implemented on 06 November 2019
-    listsec1(5) = 0 ! Local tables not used.
-    listsec1(6) = 2021 ! Year
-    listsec1(7) = 11 ! Month
-    listsec1(8) = 13 ! Day
-    listsec1(9) = 15 ! Hour
-    listsec1(10) = 59 ! Minute
-    listsec1(11) = 59 ! Second
-    listsec1(12) = 1 !  Operational Test Products
-    listsec1(13) = 0 ! Analysis Products
-
-    ! Product definition template 0.
-    ipdsnum = 0
-
-    ! Set up product definition section template.
-    ! See https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_temp4-0.shtml
-    ipdstmpl(1) = 0
-    ipdstmpl(2) = 0
-    ipdstmpl(3) = 0
-    ipdstmpl(4) = 0
-    ipdstmpl(5) = 0
-    ipdstmpl(6) = 12
-    ipdstmpl(7) = 59
-    ipdstmpl(8) = 0
-    ipdstmpl(9) = 0
-    ipdstmpl(10) = 1
-    ipdstmpl(11) = 1
-    ipdstmpl(12) = 1
-    ipdstmpl(13) = 2
-    ipdstmpl(14) = 1
-    ipdstmpl(15) = 1
-
-    ! Set up data representation section template.
-    ! See https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_temp5-0.shtml
-    idrstmpl(1) = 0
-    idrstmpl(2) = 1
-    idrstmpl(3) = 1
-    idrstmpl(4) = 8
-    idrstmpl(5) = 0
 
     ! Create the GRIB2 message, with sections 0 and 1.
     call gribcreate(cgrib, lcgrib, listsec0, listsec1, ierr)
