615f66362485af781dc43365c9057ec23041d645
Edward Hartnett <edward.hartnett@noaa.gov>
2023-05-15 13:18:50 -0600

    debugging

--- tests/test_getgb2p.F90
+++ tests/test_getgb2p.F90
@@ -11,7 +11,7 @@ program test_getgb2p
 #if KIND == 4
   integer :: lugb = 3 ! Use different LU for _4/_d in case tests are run in parallel.
 #else
-  integer :: lugb = 13
+  integer :: lugb = 5
 #endif
   integer :: iret
   integer :: leng
@@ -57,7 +57,7 @@ program test_getgb2p
      jgdt(i) = -9999
   end do
 
-  ! First try with extract true.
+  print *, 'First try with extract true...'
   extract = .true.
   call getgb2p(lugb, lugi, j, jdisc, jids, jpdtn, jpdt, jgdtn, jgdt,  &
        extract, k, gribm, leng, iret)
@@ -67,14 +67,14 @@ program test_getgb2p
   ! Deallocate buffer that got GRIB message.
   deallocate(gribm)
 
-  ! Now try with extract false.
+  print *, 'Now try with extract false...'
   extract = .false.
   call getgb2p(lugb, lugi, j, jdisc, jids, jpdtn, jpdt, jgdtn, jgdt,  &
        extract, k, gribm, leng, iret)
   if (iret .ne. 0) stop 101
   if (k .ne. 1 .or. leng .ne. 11183) stop 110
 
-  ! Deallocate buffer that got GRIB message.
+  print *, 'Deallocate buffer that got GRIB message.'
   deallocate(gribm)
   
   call baclose(lugb, iret)
