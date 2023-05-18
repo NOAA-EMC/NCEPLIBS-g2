3147a2009068bfdb48955599586687be2a0a3454
Edward Hartnett <38856240+edwardhartnett@users.noreply.github.com>
2023-05-18 11:38:09 -0600

    more testing for getg2ir() (#401)
    * more testing for getg2ir()
    
    * test cleanup
    
    * test cleanup
    
    * test cleanup
    
    * fixing parallel build issue
    
    * fixing parallel build issue
    
    * fixing parallel build issue
    
    * fixing parallel build issue
    
    * cmake cleanup
    
    * cmake cleanup
    
    * cmake cleanup
    
    * further test development
    
    * further test development
    
    * further test development
    
    * further test development
    
    * further test development
    
    * further test development
    
    * reverted developer workflow
    
    * documentation cleanup

---
+++ tests/index_rec.F90
@@ -0,0 +1,94 @@
+! This is part of the NCEPLIBS-g2 project.
+!
+! This is a module to help handle index records.
+!
+! Ed Hartnett 5/18/23
+module index_rec
+  implicit none
+  
+  type index_rec_data
+     integer :: index_rec_len, b2s_message, b2s_lus, b2s_gds, b2s_pds, b2s_drs, b2s_bms, b2s_data
+     integer :: total_bytes, grib_version, discipline, field_number
+  end type index_rec_data
+
+contains
+  subroutine init_index(index_rec_len, b2s_message, b2s_lus, b2s_gds, b2s_pds, b2s_drs, b2s_bms, &
+       b2s_data, total_bytes, grib_version, discipline, field_number, idx)
+    implicit none
+    integer, intent(in) :: index_rec_len, b2s_message, b2s_lus, b2s_gds, b2s_pds, b2s_drs, b2s_bms, b2s_data
+    integer, intent(in) :: total_bytes, grib_version, discipline, field_number
+    type (index_rec_data), intent(inout) :: idx
+
+    idx%index_rec_len = index_rec_len
+    idx%b2s_message = b2s_message
+    idx%b2s_lus = b2s_lus
+    idx%b2s_gds = b2s_gds
+    idx%b2s_pds = b2s_pds
+    idx%b2s_drs = b2s_drs
+    idx%b2s_bms = b2s_bms
+    idx%b2s_data = b2s_data
+    idx%total_bytes = total_bytes
+    idx%grib_version = grib_version
+    idx%discipline = discipline
+    idx%field_number = field_number
+    
+  end subroutine init_index
+
+  subroutine print_index(idx)
+    implicit none
+    type (index_rec_data), intent(in) :: idx
+
+    print *, 'index_rec_len = ', idx%index_rec_len, ' b2s_message = ', idx%b2s_message
+    print *, 'b2s_lus, b2s_gds, b2s_pds, b2s_drs, b2s_bms, b2s_data: ', &
+         idx%b2s_lus, idx%b2s_gds, idx%b2s_pds, idx%b2s_drs, idx%b2s_bms, idx%b2s_data
+    print *, 'total_bytes, grib_version, discipline, field_number: ', &
+         idx%total_bytes, idx%grib_version, idx%discipline, idx%field_number
+  end subroutine print_index
+
+  subroutine parse_cbuf(cbuf, idx)
+    implicit none
+    character(len=1), pointer, dimension(:), intent(in) :: cbuf(:)
+    type (index_rec_data), intent(out) :: idx
+
+    ! Break out the index record into component values.
+    call g2_gbytec(cbuf, idx%index_rec_len, 0, 8 * 4)
+    call g2_gbytec(cbuf, idx%b2s_message, 8 * 4, 8 * 4)
+    call g2_gbytec(cbuf, idx%b2s_lus, 8 * 8, 8 * 4)
+    call g2_gbytec(cbuf, idx%b2s_gds, 8 * 12, 8 * 4)
+    call g2_gbytec(cbuf, idx%b2s_pds, 8 * 16, 8 * 4)
+    call g2_gbytec(cbuf, idx%b2s_drs, 8 * 20, 8 * 4)
+    call g2_gbytec(cbuf, idx%b2s_bms, 8 * 24, 8 * 4)
+    call g2_gbytec(cbuf, idx%b2s_data, 8 * 28, 8 * 4)
+    call g2_gbytec(cbuf, idx%total_bytes, 8 * 32, 8 * 8)
+    call g2_gbytec(cbuf, idx%grib_version, 8 * 40, 8 * 1)
+    call g2_gbytec(cbuf, idx%discipline, 8 * 41, 8 * 1)
+    call g2_gbytec(cbuf, idx%field_number, 8 * 42, 8 * 2)
+
+  end subroutine parse_cbuf
+
+  integer function cmp_idx(idx1, idx2)
+    implicit none
+    type (index_rec_data), intent(in) :: idx1, idx2
+    integer res;
+    
+    res = 0
+
+    if (idx1%index_rec_len .ne. idx2%index_rec_len) res = res + 1
+    if (idx1%index_rec_len .ne. idx2%index_rec_len) res = res + 1
+    if (idx1%b2s_message .ne. idx2%b2s_message) res = res + 1
+    if (idx1%b2s_lus .ne. idx2%b2s_lus) res = res + 1
+    if (idx1%b2s_gds .ne. idx2%b2s_gds) res = res + 1
+    if (idx1%b2s_pds .ne. idx2%b2s_pds) res = res + 1
+    if (idx1%b2s_drs .ne. idx2%b2s_drs) res = res + 1
+    if (idx1%b2s_bms .ne. idx2%b2s_bms) res = res + 1
+    if (idx1%b2s_data .ne. idx2%b2s_data) res = res + 1
+    if (idx1%total_bytes .ne. idx2%total_bytes) res = res + 1
+    if (idx1%grib_version .ne. idx2%grib_version) res = res + 1
+    if (idx1%discipline .ne. idx2%discipline) res = res + 1
+    if (idx1%field_number .ne. idx2%field_number) res = res + 1
+
+    ! Return result, 0 if there is no difference between idx1 and idx2.
+    cmp_idx = res
+  end function cmp_idx
+
+end module index_rec
---
+++ tests/test_getg2ir_gdas.F90
@@ -0,0 +1,88 @@
+! This is a test program for NCEPLIBS-g2.
+!
+! This program tests getg2ir() with the test GDAS file.
+!
+! Ed Hartnett 5/17/23
+program test_getg2ir_gdas
+  use bacio_module
+  use index_rec
+  implicit none
+
+  ! These are the test files we will use.
+  character(*) :: TEST_FILE_GDAS
+  parameter (TEST_FILE_GDAS = 'gdaswave.t00z.wcoast.0p16.f000.grib2')
+  character(*) :: TEST_FILE_GDAS_INDEX
+  parameter (TEST_FILE_GDAS_INDEX = 'ref_gdaswave.t00z.wcoast.0p16.f000.grb2index')
+
+  integer :: lugb = 3
+  character(len=1), pointer, dimension(:) :: cbuf(:)
+  integer :: msk1, msk2, mnum
+  integer :: nlen, nnum, nmess, iret
+  integer :: nlen_expected
+  type (index_rec_data) :: idx, expected_idx(19)
+
+  interface
+     subroutine getg2ir(lugb, msk1, msk2, mnum, cbuf, nlen, nnum, nmess, iret)
+       integer, intent(in) :: lugb, msk1, msk2, mnum
+       character(len = 1),pointer,dimension(:) :: cbuf
+       integer, intent(out) :: nlen, nnum, nmess, iret
+     end subroutine getg2ir
+  end interface
+
+  print *, 'Testing getg2ir() with ', TEST_FILE_GDAS
+
+  ! Initialize expected results. These numbers can also be seen in a
+  ! degrib2 of the file.
+  call init_index(200, 0, 0, 37, 109, 143, 166, 4721, 15254, 2, 0, 1, expected_idx(1))
+  call init_index(200, 15254, 0, 37, 109, 143, 166, 4721, 22643, 2, 0, 1, expected_idx(2))
+  call init_index(200, 37897, 0, 37, 109, 143, 166, 4721, 15897, 2, 0, 1, expected_idx(3))
+  call init_index(200, 53794, 0, 37, 109, 143, 166, 4721, 15270, 2, 0, 1, expected_idx(4))
+  call init_index(200, 69064, 0, 37, 109, 143, 166, 4721, 10418, 2, 10, 1, expected_idx(5))
+  call init_index(200, 79482, 0, 37, 109, 143, 166, 4721, 11826, 2, 10, 1, expected_idx(6))
+  call init_index(200, 91308, 0, 37, 109, 143, 166, 4721, 17233, 2, 10, 1, expected_idx(7))
+  call init_index(200, 108541, 0, 37, 109, 143, 166, 4721, 8175, 2, 10, 1, expected_idx(8))
+  call init_index(200, 116716, 0, 37, 109, 143, 166, 4721, 12116, 2, 10, 1, expected_idx(9))
+  call init_index(200, 128832, 0, 37, 109, 143, 166, 4721, 12016, 2, 10, 1, expected_idx(10))
+  call init_index(200, 140848, 0, 37, 109, 143, 166, 4721, 10884, 2, 10, 1, expected_idx(11))
+  call init_index(200, 151732, 0, 37, 109, 143, 166, 4721, 9289, 2, 10, 1, expected_idx(12))
+  call init_index(200, 161021, 0, 37, 109, 143, 166, 4721, 12655, 2, 10, 1, expected_idx(13))
+  call init_index(200, 173676, 0, 37, 109, 143, 166, 4721, 15749, 2, 10, 1, expected_idx(14))
+  call init_index(200, 189425, 0, 37, 109, 143, 166, 4721, 15860, 2, 10, 1, expected_idx(15))
+  call init_index(200, 205285, 0, 37, 109, 143, 166, 4721, 12978, 2, 10, 1, expected_idx(16))
+  call init_index(200, 218263, 0, 37, 109, 143, 166, 4721, 18772, 2, 10, 1, expected_idx(17))
+  call init_index(200, 237035, 0, 37, 109, 143, 166, 4721, 22188, 2, 10, 1, expected_idx(18))
+  call init_index(200, 259223, 0, 37, 109, 143, 166, 4721, 22427, 2, 10, 1, expected_idx(19))
+
+  ! Open a real GRIB2 file.
+  call baopenr(lugb, TEST_FILE_GDAS, iret)
+  if (iret .ne. 0) stop 100
+
+  msk1 = 1000
+  msk2 = 1000
+  nlen_expected = 3800
+  do mnum = 0, 18
+     call getg2ir(lugb, msk1, msk2, mnum, cbuf, nlen, nnum, nmess, iret)
+     print *, 'mnum, iret, nlen, nnum, nmess: ', mnum, iret, nlen, nnum, nmess
+     if (iret .ne. 0) stop 101
+     if (nlen .ne. nlen_expected .or. nnum .ne. 19 - mnum .or. nmess .ne. 19) stop 102
+     nlen_expected = nlen_expected - 200
+
+     ! Parse the index record into special type.
+     call parse_cbuf(cbuf, idx)
+     !call print_index(idx)
+
+     ! Is this what we expected?
+     if (cmp_idx(idx, expected_idx(mnum + 1)) .ne. 0) stop 300
+     
+     ! Free memory.
+     deallocate(cbuf)
+  end do
+  
+  call baclose(lugb, iret)
+  if (iret .ne. 0) stop 199
+
+  print *, 'SUCCESS!...'
+
+end program test_getg2ir_gdas
+
+
