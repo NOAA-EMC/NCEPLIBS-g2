137c8091fdc8e3579a187ad676b7e5c66527e441
Edward Hartnett <edward.hartnett@noaa.gov>
2023-05-18 07:48:24 -0600

    test cleanup

---
+++ tests/index_rec.F90
@@ -0,0 +1,69 @@
+! This is part of the NCEPLIBS-g2 project.
+!
+! This is a module to help handle index records.
+!
+! Ed Hartnett 5/18/23
+module index_rec
+  implicit none
+  
+  type index_rec_data
+     integer :: index_rec_len, b2s_message, b2s_lus, b2s_gds, b2s_pds, b2s_drs, b2s_bms, b2s_data
+     integer :: total_bytes, grib_version, discipline, field_number
+  end type index_rec_data
+
+contains
+  subroutine print_index(idx)
+    implicit none
+    type (index_rec_data), intent(in) :: idx
+
+    print *, 'index_rec_len = ', idx%index_rec_len, ' b2s_message = ', idx%b2s_message
+    print *, 'b2s_lus, b2s_gds, b2s_pds, b2s_drs, b2s_bms, b2s_data: ', &
+         idx%b2s_lus, idx%b2s_gds, idx%b2s_pds, idx%b2s_drs, idx%b2s_bms, idx%b2s_data
+    print *, 'total_bytes, grib_version, discipline, field_number: ', &
+         idx%total_bytes, idx%grib_version, idx%discipline, idx%field_number
+  end subroutine print_index
+
+  subroutine parse_cbuf(cbuf, idx)
+    implicit none
+    character(len=1), pointer, dimension(:), intent(in) :: cbuf(:)
+    type (index_rec_data), intent(out) :: idx
+
+    ! Break out the index record into component values.
+    call g2_gbytec(cbuf, idx%index_rec_len, 0, 8 * 4)
+    call g2_gbytec(cbuf, idx%b2s_message, 8 * 4, 8 * 4)
+    call g2_gbytec(cbuf, idx%b2s_lus, 8 * 8, 8 * 4)
+    call g2_gbytec(cbuf, idx%b2s_gds, 8 * 12, 8 * 4)
+    call g2_gbytec(cbuf, idx%b2s_pds, 8 * 16, 8 * 4)
+    call g2_gbytec(cbuf, idx%b2s_drs, 8 * 20, 8 * 4)
+    call g2_gbytec(cbuf, idx%b2s_bms, 8 * 24, 8 * 4)
+    call g2_gbytec(cbuf, idx%b2s_data, 8 * 28, 8 * 4)
+    call g2_gbytec(cbuf, idx%total_bytes, 8 * 32, 8 * 8)
+    call g2_gbytec(cbuf, idx%grib_version, 8 * 40, 8 * 1)
+    call g2_gbytec(cbuf, idx%discipline, 8 * 41, 8 * 1)
+    call g2_gbytec(cbuf, idx%field_number, 8 * 42, 8 * 2)
+
+  end subroutine parse_cbuf
+
+  integer function cmp_idx(idx1, idx2)
+    implicit none
+    type (index_rec_data), intent(in) :: idx1, idx2
+
+    cmp_idx = 0
+
+    if (idx1%index_rec_len .ne. idx2%index_rec_len) cmp_idx = cmp_idx + 1
+    if (idx1%index_rec_len .ne. idx2%index_rec_len) cmp_idx = cmp_idx + 1
+    if (idx1%b2s_message .ne. idx2%b2s_message) cmp_idx = cmp_idx + 1
+    if (idx1%b2s_lus .ne. idx2%b2s_lus) cmp_idx = cmp_idx + 1
+    if (idx1%b2s_gds .ne. idx2%b2s_gds) cmp_idx = cmp_idx + 1
+    if (idx1%b2s_pds .ne. idx2%b2s_pds) cmp_idx = cmp_idx + 1
+    if (idx1%b2s_drs .ne. idx2%b2s_drs) cmp_idx = cmp_idx + 1
+    if (idx1%b2s_bms .ne. idx2%b2s_bms) cmp_idx = cmp_idx + 1
+    if (idx1%b2s_data .ne. idx2%b2s_data) cmp_idx = cmp_idx + 1
+    if (idx1%total_bytes .ne. idx2%total_bytes) cmp_idx = cmp_idx + 1
+    if (idx1%grib_version .ne. idx2%grib_version) cmp_idx = cmp_idx + 1
+    if (idx1%discipline .ne. idx2%discipline) cmp_idx = cmp_idx + 1
+    if (idx1%field_number .ne. idx2%field_number) cmp_idx = cmp_idx + 1
+
+  end function cmp_idx
+
+end module index_rec
--- tests/test_getg2ir_gdas.F90
+++ tests/test_getg2ir_gdas.F90
@@ -5,6 +5,7 @@
 ! Ed Hartnett 5/17/23
 program test_getg2ir_gdas
   use bacio_module
+  use index_rec
   implicit none
 
   integer :: lugb = 3
@@ -14,6 +15,7 @@ program test_getg2ir_gdas
 
   integer :: index_rec_len, b2s_message, b2s_lus, b2s_gds, b2s_pds, b2s_drs, b2s_bms, b2s_data
   integer :: total_bytes, grib_version, discipline, field_number
+  type (index_rec_data) :: idx, expected_idx1, expected_idx2
 
   ! These are the test files we will use.
   character(*) :: TEST_FILE_GDAS
@@ -29,8 +31,34 @@ program test_getg2ir_gdas
      end subroutine getg2ir
   end interface
 
-  print *, 'Testing getg2ir() with ', TEST_FILE_GDAS, &
-       ' - expect and ignore error messages during test...'
+  print *, 'Testing getg2ir() with ', TEST_FILE_GDAS
+
+  ! Initialize expected results.
+  expected_idx1%index_rec_len = 200
+  expected_idx1%b2s_message = 0
+  expected_idx1%b2s_lus = 0
+  expected_idx1%b2s_gds = 37
+  expected_idx1%b2s_pds = 109
+  expected_idx1%b2s_drs = 143
+  expected_idx1%b2s_bms = 166
+  expected_idx1%b2s_data = 4721
+  expected_idx1%total_bytes = 15254
+  expected_idx1%grib_version = 2
+  expected_idx1%discipline = 0
+  expected_idx1%field_number = 1
+
+  expected_idx2%index_rec_len = 200
+  expected_idx2%b2s_message = 37897
+  expected_idx2%b2s_lus = 0
+  expected_idx2%b2s_gds = 37
+  expected_idx2%b2s_pds = 109
+  expected_idx2%b2s_drs = 143
+  expected_idx2%b2s_bms = 166
+  expected_idx2%b2s_data = 4721
+  expected_idx2%total_bytes = 15897
+  expected_idx2%grib_version = 2
+  expected_idx2%discipline = 0
+  expected_idx2%field_number = 1
 
   ! Open a real GRIB2 file.
   call baopenr(lugb, TEST_FILE_GDAS, iret)
@@ -43,74 +71,25 @@ program test_getg2ir_gdas
   if (iret .ne. 0) stop 101
   if (nlen .ne. 3800 .or. nnum .ne. 19 .or. nmess .ne. 19) stop 102
   !  print *, 'iret, nlen, nnum, nmess: ', iret, nlen, nnum, nmess
-  
-  ! Break out the index record into component values.
-  call g2_gbytec(cbuf, index_rec_len, 0, 8 * 4)
-  if (index_rec_len .ne. 200) stop 105
-  call g2_gbytec(cbuf, b2s_message, 8 * 4, 8 * 4)
-  if (b2s_message .ne. 0) stop 106
-  call g2_gbytec(cbuf, b2s_lus, 8 * 8, 8 * 4)
-  if (b2s_lus .ne. 0) stop 107
-  call g2_gbytec(cbuf, b2s_gds, 8 * 12, 8 * 4)
-  if (b2s_gds .ne. 37) stop 108
-  call g2_gbytec(cbuf, b2s_pds, 8 * 16, 8 * 4)
-  if (b2s_pds .ne. 109) stop 109
-  call g2_gbytec(cbuf, b2s_drs, 8 * 20, 8 * 4)
-  if (b2s_drs .ne. 143) stop 110
-  call g2_gbytec(cbuf, b2s_bms, 8 * 24, 8 * 4)
-  if (b2s_bms .ne. 166) stop 111
-  call g2_gbytec(cbuf, b2s_data, 8 * 28, 8 * 4)
-  if (b2s_data .ne. 4721) stop 112
-  call g2_gbytec(cbuf, total_bytes, 8 * 32, 8 * 8)
-  if (total_bytes .ne. 15254) stop 113
-  call g2_gbytec(cbuf, grib_version, 8 * 40, 8 * 1)
-  if (grib_version .ne. 2) stop 113
-  call g2_gbytec(cbuf, discipline, 8 * 41, 8 * 1)
-  if (discipline .ne. 0) stop 113
-  call g2_gbytec(cbuf, field_number, 8 * 42, 8 * 2)
-  if (field_number .ne. 1) stop 113
-  ! print *, 'index_rec_len = ', index_rec_len, ' b2s_message = ', b2s_message
-  ! print *, 'b2s_lus, b2s_gds, b2s_pds, b2s_drs, b2s_bms, b2s_data: ', b2s_lus, b2s_gds, b2s_pds, b2s_drs, b2s_bms, b2s_data
-  ! print *, 'total_bytes, grib_version, discipline, field_number: ', total_bytes, grib_version, discipline, field_number
 
+  call parse_cbuf(cbuf, idx)
+  call print_index(idx)
+  if (cmp_idx(idx, expected_idx1) .ne. 0) stop 300
+
+  ! Free memory.
   deallocate(cbuf)
   
   ! Get a index for a different message.
   mnum = 2
   call getg2ir(lugb, msk1, msk2, mnum, cbuf, nlen, nnum, nmess, iret)
-  print *, 'iret, nlen, nnum, nmess: ', iret, nlen, nnum, nmess
+!  print *, 'iret, nlen, nnum, nmess: ', iret, nlen, nnum, nmess
   if (iret .ne. 0) stop 101
   if (nlen .ne. 3400 .or. nnum .ne. 17 .or. nmess .ne. 19) stop 102
   
-  ! Break out the index record into component values.
-  call g2_gbytec(cbuf, index_rec_len, 0, 8 * 4)
-  call g2_gbytec(cbuf, b2s_message, 8 * 4, 8 * 4)
-  call g2_gbytec(cbuf, b2s_lus, 8 * 8, 8 * 4)
-  call g2_gbytec(cbuf, b2s_gds, 8 * 12, 8 * 4)
-  call g2_gbytec(cbuf, b2s_pds, 8 * 16, 8 * 4)
-  call g2_gbytec(cbuf, b2s_drs, 8 * 20, 8 * 4)
-  call g2_gbytec(cbuf, b2s_bms, 8 * 24, 8 * 4)
-  call g2_gbytec(cbuf, b2s_data, 8 * 28, 8 * 4)
-  call g2_gbytec(cbuf, total_bytes, 8 * 32, 8 * 8)
-  call g2_gbytec(cbuf, grib_version, 8 * 40, 8 * 1)
-  call g2_gbytec(cbuf, discipline, 8 * 41, 8 * 1)
-  call g2_gbytec(cbuf, field_number, 8 * 42, 8 * 2)
-  print *, 'index_rec_len = ', index_rec_len, ' b2s_message = ', b2s_message
-  print *, 'b2s_lus, b2s_gds, b2s_pds, b2s_drs, b2s_bms, b2s_data: ', b2s_lus, b2s_gds, b2s_pds, b2s_drs, b2s_bms, b2s_data
-  print *, 'total_bytes, grib_version, discipline, field_number: ', total_bytes, grib_version, discipline, field_number
-  if (index_rec_len .ne. 200) stop 105
-  if (b2s_message .ne. 37897) stop 106
-  if (b2s_lus .ne. 0) stop 107
-  if (b2s_gds .ne. 37) stop 108
-  if (b2s_pds .ne. 109) stop 109
-  if (b2s_drs .ne. 143) stop 110
-  if (b2s_bms .ne. 166) stop 111
-  if (b2s_data .ne. 4721) stop 112
-  if (total_bytes .ne. 15897) stop 113
-  if (grib_version .ne. 2) stop 113
-  if (discipline .ne. 0) stop 113
-  if (field_number .ne. 1) stop 113
-  
+  call parse_cbuf(cbuf, idx)
+  call print_index(idx)
+  if (cmp_idx(idx, expected_idx2) .ne. 0) stop 300
+
   deallocate(cbuf)
   
   call baclose(lugb, iret)
@@ -119,3 +98,5 @@ program test_getg2ir_gdas
   print *, 'SUCCESS!...'
 
 end program test_getg2ir_gdas
+
+
