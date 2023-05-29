0ae00e8fcb712b4c10127dfdbcbc5dc8d9eacc71
Edward Hartnett <38856240+edwardhartnett@users.noreply.github.com>
2023-05-29 07:04:04 -0600

    converted params.f to F90, add more testing of params. (#422)
    * converted params.f to F90
    
    * more testing of params.F90

--- tests/test_params.F90
+++ tests/test_params.F90
@@ -13,7 +13,7 @@ program test_params
   character(len = 8) :: g2_abbrev
   integer :: ios, i
 
-  print *, 'Testing the params module.'
+  print *, 'Testing the params module, ignore error messages.'
 
   print *, 'Testing param_g1_to_g2...'
   call param_g1_to_g2(1, 2, g2disc, g2cat, g2num)
@@ -22,12 +22,23 @@ program test_params
   if (g2disc .ne. 10 .or. g2cat .ne. 1 .or. g2num .ne. 0) stop 3
   
   print *, 'Testing param_get_abbrev...'
-  abbrev = param_get_abbrev(0, 3, 1)
-  if (abbrev .ne. 'PRMSL') stop 4
-  abbrev = param_get_abbrev(0, 3, 8)
-  if (abbrev .ne. 'PRESA') stop 4
+  abbrev = param_get_abbrev(0, 3, 999)
+  if (abbrev .ne. 'UNKNOWN') stop 4
+  abbrev = param_get_abbrev(0, 999, 8)
+  if (abbrev .ne. 'UNKNOWN') stop 4
+  abbrev = param_get_abbrev(999, 3, 8)
+  if (abbrev .ne. 'UNKNOWN') stop 4
   
   print *, 'Testing param_g2_to_g1...'
+  ! These won't be found.
+  call param_g2_to_g1(999, 3, 1, g1val, g1ver)
+  if (g1val .ne. 255 .or. g1ver .ne. 255) stop 6
+  call param_g2_to_g1(0, 999, 1, g1val, g1ver)
+  if (g1val .ne. 255 .or. g1ver .ne. 255) stop 6
+  call param_g2_to_g1(0, 3, 999, g1val, g1ver)
+  if (g1val .ne. 255 .or. g1ver .ne. 255) stop 6
+
+  ! These will be found.
   call param_g2_to_g1(0, 3, 1, g1val, g1ver)
   if (g1val .ne. 2 .or. g1ver .ne. 2) stop 6
   call param_g2_to_g1(0, 2, 0, g1val, g1ver)
@@ -2068,9 +2079,13 @@ program test_params
   if (abbrev .ne. 'SOILL') stop 5
 
   print *, 'Testing all parameters with param_g1_to_g2()...'
-  call param_g1_to_g2(1, 2, g2disc, g2cat, g2num)
-  if (g2disc .ne. 0 .or. g2cat .ne. 3 .or. g2num .ne. 0) stop 21
+  ! These don't exist.
+  call param_g1_to_g2(2000, 2, g2disc, g2cat, g2num)
+  if (g2disc .ne. 255 .or. g2cat .ne. 255 .or. g2num .ne. 255) stop 21
+  call param_g1_to_g2(1, 999, g2disc, g2cat, g2num)
+  if (g2disc .ne. 255 .or. g2cat .ne. 255 .or. g2num .ne. 255) stop 21
 
+  ! These do exist.
   call param_g1_to_g2(1, 2, g2disc, g2cat, g2num)
   if (g2disc .ne. 0 .or. g2cat .ne. 3 .or. g2num .ne. 0) stop 21
   call param_g1_to_g2(1, 2, g2disc, g2cat, g2num)
