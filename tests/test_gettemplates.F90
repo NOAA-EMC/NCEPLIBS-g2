6f357e65e4a4b1bcc096780eff072d167553ad4c
andrew <anki0301@colorado.edu>
2023-06-20 12:00:57 -0600

    Adding test_gettemplates file

---
+++ tests/test_gettemplates.F90
@@ -0,0 +1,10 @@
+! This is a test program for NCEPLIBS-g2.
+!
+! This program tests the gettemplates function.
+!
+! Andrew King 6/20/23
+program test_gettemplates
+    implicit none
+  
+    print *, 'SUCCESS!'
+  end program test_gettemplates
