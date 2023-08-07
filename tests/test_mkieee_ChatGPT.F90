161d02eb500356ddb5f508e5909e62c293826102
Aidan <hartnettaidan@gmail.com>
2023-08-07 12:25:59 -0600

    used chatGPT to write a test file for mkieee

---
+++ tests/test_mkieee_ChatGPT.F90
@@ -0,0 +1,54 @@
+program ieee_array_large_conversion_test
+    implicit none
+
+    ! Declare constants
+    integer, parameter :: n_values = 8
+
+    ! Declare variables
+    real(kind=4) :: ibm_floats(n_values), ieee_floats(n_values)
+    integer :: ierr, i
+
+    ! Initialize IBM floating-point values
+    ibm_floats = [1.0, -2.5, 3.14159, -0.123456, 10.0, 0.0, 1.0E38, -1.0E38]
+
+    ! Convert IBM array to IEEE
+    do i = 1, n_values
+        call mkieee(ibm_floats(i), ieee_floats(i), ierr)
+    end do
+
+    ! Display results
+    write(*,*) 'IBM Floats:'
+    do i = 1, n_values
+        write(*,*) ibm_floats(i)
+    end do
+    write(*,*)
+    write(*,*) 'IEEE Floats:'
+    do i = 1, n_values
+        write(*,*) ieee_floats(i)
+    end do
+    write(*,*)
+
+    ! Convert IEEE array back to IBM
+    do i = 1, n_values
+        call rdieee(ieee_floats(i), ibm_floats(i), ierr)
+    end do
+
+    ! Display results
+    write(*,*) 'Converted IBM Floats:'
+    do i = 1, n_values
+        write(*,*) ibm_floats(i)
+    end do
+    write(*,*)
+
+    ! Verify conversion
+    write(*,*)
+    write(*,*) 'Verification:'
+    do i = 1, n_values
+        if (abs(ibm_floats(i) - ibm_floats(i)) < 1E-6) then
+            write(*,*) 'Value', i, '- Conversion successful!'
+        else
+            write(*,*) 'Value', i, '- Conversion failed!'
+        end if
+    end do
+
+end program ieee_array_large_conversion_test
