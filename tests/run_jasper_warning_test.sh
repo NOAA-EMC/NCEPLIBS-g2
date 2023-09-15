5c15185d7d110e10621cdc213ccd9b0f439bd03a
Edward Hartnett <edward.hartnett@noaa.gov>
2023-09-15 01:46:57 -0600

    added test for jasper warnings

---
+++ tests/run_jasper_warning_test.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+# This is a test script for the NCEPLIBS-g2 project.
+#
+# This test checks that the output of test_jpcpack_4 has not
+# changed. This is a test that involves jasper, and jasper sometimes
+# starts issuing warnings. This test ensures that jasper warnings are
+# detected automatically, since we are not running our tests
+# manually (of course). 
+#
+# Ed Hartnett, 9/15/23
+
+set -e
+echo ""
+echo "*** Running jasper warning test"
+set -x
+
+# Run the test and capture output.
+../tests/test_jpcpack_4 &> test_jpcpack_4_output.txt
+cat test_jpcpack_4_output.txt
+
+# Check against expected output.
+diff -w test_jpcpack_4_output.txt ref_test_jpcpack_4_output.txt
+
+set -x
+echo "*** SUCCESS!"
+exit 0
