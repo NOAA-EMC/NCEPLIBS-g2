3e283e569561ffaa1799077b0edeba137416ab7c
Edward Hartnett <edward.hartnett@noaa.gov>
2023-09-15 20:57:14 -0600

    trying to add script-based test

--- tests/run_jasper_warning_test.sh
+++ tests/run_jasper_warning_test.sh
@@ -15,7 +15,7 @@ echo "*** Running jasper warning test"
 set -x
 
 # Run the test and capture output.
-# ../tests/test_jpcpack_4 &> test_jpcpack_4_output.txt
+./test_jpcpack_4 &> test_jpcpack_4_output.txt
 # cat test_jpcpack_4_output.txt
 
 # # Check against expected output.
