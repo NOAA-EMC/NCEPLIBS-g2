e97583e7d543d454cbec6d763c5797dd25eaa798
Edward Hartnett <edward.hartnett@noaa.gov>
2024-02-04 06:23:49 -0700

    more testing

--- tests/test_gbytec.F90
+++ tests/test_gbytec.F90
@@ -214,6 +214,11 @@ program test_gbytec
      endif
   end do
 
+  print *, '   now unpack into 1 64-bit int with g2_gbytesc()...'
+  call g2_gbytesc(out8, in8_1, iskip, 64, 0, 1)
+  print *, in8_1
+  if (in8_1(1) .ne. in8(1)) stop 150
+
   print *, 'SUCCESS!'
 
 end program test_gbytec
