2cd7610dca47c0ab98ddfeb2c4c1d62094089510
Edward Hartnett <edward.hartnett@noaa.gov>
2024-02-05 10:20:59 -0700

    combined subroutines into files

--- src/g2spec.F90
+++ src/g2spec.F90
@@ -1,5 +1,5 @@
 !> @file
-!> @brief Pack a spectral data field using the complex
+!> @brief Pack/unpack a spectral data field using the complex
 !> packing algorithm for spherical harmonic data as defined in 
 !> [Data Representation Template
 !> 5.51](https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_temp5-51.shtml).
@@ -104,3 +104,82 @@ subroutine specpack(fld,ndpts,JJ,KK,MM,idrstmpl,cpack,lcpack)
   idrstmpl(10)=1 ! Unpacked spectral data is 32-bit IEEE
 
 end subroutine specpack
+
+!> Unpack a spectral data field using the complex packing algorithm
+!> for spherical harmonic data, [Data Representation Template
+!> 5.51](https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_temp5-51.shtml).
+!>
+!> @param[in] cpack The packed data field (character*1 array).
+!> @param[in] len length of packed field cpack.
+!> @param[in] idrstmpl Contains the array of values for Data
+!> Representation Template 5.51.
+!> @param[in] ndpts The number of data values in array fld.
+!> @param[in] JJ J pentagonal resolution parameter.
+!> @param[in] KK K pentagonal resolution parameter.
+!> @param[in] MM M pentagonal resolution parameter.
+!> @param[out] fld Contains the unpacked data values.
+!>
+!> @author Stephen Gilbert @date 2002-12-19
+subroutine specunpack(cpack,len,idrstmpl,ndpts,JJ,KK,MM,fld)
+
+  character(len=1),intent(in) :: cpack(len)
+  integer,intent(in) :: ndpts,len,JJ,KK,MM
+  integer,intent(in) :: idrstmpl(*)
+  real,intent(out) :: fld(ndpts)
+
+  integer :: ifld(ndpts),Ts
+  integer(4) :: ieee
+  real :: ref,bscale,dscale,unpk(ndpts)
+  real,allocatable :: pscale(:)
+
+  ieee = idrstmpl(1)
+  call rdieee(ieee,ref,1)
+  bscale = 2.0**real(idrstmpl(2))
+  dscale = 10.0**real(-idrstmpl(3))
+  nbits = idrstmpl(4)
+  Js=idrstmpl(6)
+  Ks=idrstmpl(7)
+  Ms=idrstmpl(8)
+  Ts=idrstmpl(9)
+
+  if (idrstmpl(10).eq.1) then           ! unpacked floats are 32-bit IEEE
+     call rdieee(cpack,unpk,Ts)          ! read IEEE unpacked floats
+     iofst=32*Ts
+     call g2_gbytesc(cpack,ifld,iofst,nbits,0,ndpts-Ts)  ! unpack scaled data
+
+     ! Calculate Laplacian scaling factors for each possible wave number.
+     allocate(pscale(JJ+MM))
+     tscale=real(idrstmpl(5))*1E-6
+     do n=Js,JJ+MM
+        pscale(n)=real(n*(n+1))**(-tscale)
+     enddo
+
+     ! Assemble spectral coeffs back to original order.
+     inc=1
+     incu=1
+     incp=1
+     do m=0,MM
+        Nm=JJ      ! triangular or trapezoidal
+        if (KK .eq. JJ+MM) Nm=JJ+m          ! rhombodial
+        Ns=Js      ! triangular or trapezoidal
+        if (Ks .eq. Js+Ms) Ns=Js+m          ! rhombodial
+        do n=m,Nm
+           if (n.le.Ns .AND. m.le.Ms) then    ! grab unpacked value
+              fld(inc)=unpk(incu)         ! real part
+              fld(inc+1)=unpk(incu+1)     ! imaginary part
+              inc=inc+2
+              incu=incu+2
+           else                         ! Calc coeff from packed value
+              fld(inc)=((real(ifld(incp))*bscale)+ref)*dscale*pscale(n)           ! real part
+              fld(inc+1)=((real(ifld(incp+1))*bscale)+ref)*dscale*pscale(n)           ! imaginary part
+              inc=inc+2
+              incp=incp+2
+           endif
+        enddo
+     enddo
+     deallocate(pscale)
+  else
+     print *,'specunpack: Cannot handle 64 or 128-bit floats.'
+     fld=0.0
+  endif
+end subroutine specunpack
