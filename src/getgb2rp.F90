8405853154c8b6ff9a1978c8c8b06e4eb7f1d140
Edward Hartnett <edward.hartnett@noaa.gov>
2024-02-13 06:57:51 -0700

    more index work

--- src/getgb2rp.F90
+++ src/getgb2rp.F90
@@ -3,8 +3,11 @@
 !> field.
 !> @author Stephen Gilbert @date 2003-12-31
 
-!> Extract a grib message from a file given the index of the requested
-!> field.
+!> Extract a grib message from a file given the index (index format 1)
+!> of the requested field.
+!>
+!> This subroutine is maintained for backward compatibility. New code
+!> should use getgb2rp2().
 !>
 !> The GRIB message returned can contain only the requested field
 !> (extract=.true.), or the complete GRIB message originally
@@ -22,7 +25,7 @@
 !> (https://noaa-emc.github.io/NCEPLIBS-bacio/) before calling this
 !> routine.
 !> @param[in] cindex Index record of the grib field (see docunentation of
-!> subroutine ixgb2() for description of an index record.)
+!> subroutine ix2gb2() for description of an index record.)
 !> @param[in] extract Logical value indicating whether to return a
 !> GRIB2 message with just the requested field, or the entire
 !> GRIB2 message containing the requested field.
@@ -34,7 +37,7 @@
 !> - 0 No error.
 !> - 97 Error reading grib file.
 !>
-!> @author Stephen Gilbert @date 2003-12-31
+!> @author Stephen Gilbert, Ed Hartnett @date 2003-12-31
 subroutine getgb2rp(lugb, cindex, extract, gribm, leng, iret)
   implicit none
 
@@ -58,8 +61,8 @@ subroutine getgb2rp(lugb, cindex, extract, gribm, leng, iret)
 
 end subroutine getgb2rp
 
-!> Extract a grib message from a file given the index of the requested
-!> field.
+!> Extract a grib message from a file given the version 1 or 2 index
+!> of the requested field.
 !>
 !> The GRIB message returned can contain only the requested field
 !> (extract=.true.), or the complete GRIB message originally
@@ -69,13 +72,12 @@ end subroutine getgb2rp
 !> If the GRIB field is not found, then the return code will be
 !> nonzero.
 !>
-!> @note For files greater than 2 GB, this subroutine will only work
-!> if the message is within the first 2 GB of the file.
-!>
 !> @param[in] lugb integer unit of the unblocked grib data file.  file
 !> must be opened with [baopen() or baopenr()]
 !> (https://noaa-emc.github.io/NCEPLIBS-bacio/) before calling this
 !> routine.
+!> @param[in] idxver Version of index, use 1 for legacy, 2 if files
+!> may be > 2 GB.
 !> @param[in] cindex Index record of the grib field (see docunentation of
 !> subroutine ixgb2() for description of an index record.)
 !> @param[in] extract Logical value indicating whether to return a
@@ -89,7 +91,7 @@ end subroutine getgb2rp
 !> - 0 No error.
 !> - 97 Error reading grib file.
 !>
-!> @author Stephen Gilbert @date 2003-12-31
+!> @author Edward Hartnett, Stephen Gilbert @date Feb 13, 2024
 subroutine getgb2rp2(lugb, idxver, cindex, extract, gribm, leng, iret)
   implicit none
 
@@ -105,6 +107,8 @@ subroutine getgb2rp2(lugb, idxver, cindex, extract, gribm, leng, iret)
   integer :: lencur, lread, len0, ibmap, ipos, iskip
   integer :: len7, len8, len3, len4, len5, len6, len1, len2
   integer :: iskp2, iskp6, iskp7
+  integer :: INT1_BITS, INT2_BITS, INT4_BITS, INT8_BITS
+  parameter(INT1_BITS = 8, INT2_BITS = 16, INT4_BITS = 32, INT8_BITS = 64)
 
   iret = 0
 
@@ -112,37 +116,37 @@ subroutine getgb2rp2(lugb, idxver, cindex, extract, gribm, leng, iret)
   if (extract) then
      len0 = 16
      len8 = 4
-     call g2_gbytec(cindex, iskip, 4*8, 4*8)    ! bytes to skip in file
-     call g2_gbytec(cindex, iskp2, 8*8, 4*8)    ! bytes to skip for section 2
+     call g2_gbytec(cindex, iskip, INT4_BITS, INT4_BITS)    ! bytes to skip in file
+     call g2_gbytec(cindex, iskp2, 8*8, INT4_BITS)    ! bytes to skip for section 2
      if (iskp2 .gt. 0) then
         call baread(lugb, iskip + iskp2, 4, lread, ctemp)
-        call g2_gbytec(ctemp, len2, 0, 4*8)      ! length of section 2
+        call g2_gbytec(ctemp, len2, 0, INT4_BITS)      ! length of section 2
         allocate(csec2(len2))
         call baread(lugb, iskip + iskp2, len2, lread, csec2)
      else
         len2 = 0
      endif
-     call g2_gbytec(cindex, len1, 44*8, 4*8)      ! length of section 1
+     call g2_gbytec(cindex, len1, 44*8, INT4_BITS)      ! length of section 1
      ipos = 44 + len1
-     call g2_gbytec(cindex, len3, ipos*8, 4*8)      ! length of section 3
+     call g2_gbytec(cindex, len3, ipos*8, INT4_BITS)      ! length of section 3
      ipos = ipos + len3
-     call g2_gbytec(cindex, len4, ipos*8, 4*8)      ! length of section 4
+     call g2_gbytec(cindex, len4, ipos*8, INT4_BITS)      ! length of section 4
      ipos = ipos + len4
-     call g2_gbytec(cindex, len5, ipos*8, 4*8)      ! length of section 5
+     call g2_gbytec(cindex, len5, ipos*8, INT4_BITS)      ! length of section 5
      ipos = ipos + len5
-     call g2_gbytec(cindex, len6, ipos*8, 4*8)      ! length of section 6
+     call g2_gbytec(cindex, len6, ipos*8, INT4_BITS)      ! length of section 6
      ipos = ipos + 5
      call g2_gbytec(cindex, ibmap, ipos*8, 1*8)      ! bitmap indicator
      if (ibmap .eq. 254) then
-        call g2_gbytec(cindex, iskp6, 24*8, 4*8)    ! bytes to skip for section 6
+        call g2_gbytec(cindex, iskp6, 24*8, INT4_BITS)    ! bytes to skip for section 6
         call baread(lugb, iskip + iskp6, 4, lread, ctemp)
-        call g2_gbytec(ctemp, len6, 0, 4*8)      ! length of section 6
+        call g2_gbytec(ctemp, len6, 0, INT4_BITS)      ! length of section 6
      endif
 
      ! Read in section 7 from file.
-     call g2_gbytec(cindex, iskp7, 28*8, 4*8)    ! bytes to skip for section 7
+     call g2_gbytec(cindex, iskp7, 28*8, INT4_BITS)    ! bytes to skip for section 7
      call baread(lugb, iskip + iskp7, 4, lread, ctemp)
-     call g2_gbytec(ctemp, len7, 0, 4*8)      ! length of section 7
+     call g2_gbytec(ctemp, len7, 0, INT4_BITS)      ! length of section 7
      allocate(csec7(len7))
      call baread(lugb, iskip + iskp7, len7, lread, csec7)
 
@@ -162,7 +166,7 @@ subroutine getgb2rp2(lugb, idxver, cindex, extract, gribm, leng, iret)
      gribm(10) = char(0)
      gribm(11) = char(0)
      gribm(12) = char(0)
-     call g2_sbytec(gribm, leng, 12*8, 4*8)
+     call g2_sbytec(gribm, leng, 12*8, INT4_BITS)
 
      ! Copy Section 1
      gribm(17:16 + len1) = cindex(45:44 + len1)
@@ -185,9 +189,9 @@ subroutine getgb2rp2(lugb, idxver, cindex, extract, gribm, leng, iret)
         gribm(lencur + 1:lencur + len6) = cindex(ipos + 1:ipos + len6)
         lencur = lencur + len6
      else
-        call g2_gbytec(cindex, iskp6, 24*8, 4*8)    ! bytes to skip for section 6
+        call g2_gbytec(cindex, iskp6, 24*8, INT4_BITS)    ! bytes to skip for section 6
         call baread(lugb, iskip + iskp6, 4, lread, ctemp)
-        call g2_gbytec(ctemp, len6, 0, 4*8)      ! length of section 6
+        call g2_gbytec(ctemp, len6, 0, INT4_BITS)      ! length of section 6
         allocate(csec6(len6))
         call baread(lugb, iskip + iskp6, len6, lread, csec6)
         gribm(lencur + 1:lencur + len6) = csec6(1:len6)
@@ -209,8 +213,8 @@ subroutine getgb2rp2(lugb, idxver, cindex, extract, gribm, leng, iret)
      if (allocated(csec2)) deallocate(csec2)
      if (allocated(csec7)) deallocate(csec7)
   else    ! do not extract field from message :  get entire message
-     call g2_gbytec(cindex, iskip, 4*8, 4*8)    ! bytes to skip in file
-     call g2_gbytec(cindex, leng, 36*8, 4*8)      ! length of grib message
+     call g2_gbytec(cindex, iskip, INT4_BITS, INT4_BITS)    ! bytes to skip in file
+     call g2_gbytec(cindex, leng, 36*8, INT4_BITS)      ! length of grib message
      if (.not. associated(gribm)) allocate(gribm(leng))
      call baread(lugb, iskip, leng, lread, gribm)
      if (leng .ne. lread ) then
