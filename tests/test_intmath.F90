25f28bcf7e9da8e8d70864b329a80cdc3022be2e
Edward Hartnett <edward.hartnett@noaa.gov>
2024-02-14 18:47:27 -0700

    more testing for intmath

--- tests/test_intmath.F90
+++ tests/test_intmath.F90
@@ -9,6 +9,13 @@ program test_intmath
   integer(kind = 4) :: ival4 = 10, iret4
   integer(kind = 2) :: ival2 = 10_2, iret2
   integer(kind = 1) :: ival1 = 10_1, iret1
+  real(kind=16) :: temp
+  integer(kind=8) :: big, check
+  integer(kind=8), parameter  :: one = 1
+  data big /Z'7FFFFFFFFFFFFFFF'/
+  data check /Z'1FFFFFFF'/
+  integer(kind=8), parameter  :: small = -2000000_8
+  integer(kind=8) :: ival, iret
 
   print *, 'Testing intmath...'
   
@@ -34,40 +41,28 @@ program test_intmath
   if (iret1 .ne. 4) stop 2
   print *, 'ok'
 
+  ! Here is some legacy test code that could be recovered...
+  print *, 'running slow legacy tests...'
+  do ival = big - 50000, big - 1
+10   format(Z16,' -- MISMATCH: ',I0,'=>',I0,' (',I0,' = ',F0.10,')')
+20   format(Z16,' -- OKAY:     ',I0,'=>',I0,' (',I0,' = ',F0.10,')')
+     if (ival + one < ival) then
+        temp = log(real(max(ival, one), kind=16)) / alog2
+     else
+        temp = log(real(max(ival + one, one), kind = 16)) / alog2
+     endif
+     iret = i1log2(ival)
+     if (iret /= ceiling(temp) .or. ival == 0 .or. ival == check) then
+        if (iret /= ceiling(temp)) then
+           print 10, ival, ival, iret, ceiling(temp),temp
+        else
+           print 20, ival, ival, iret, ceiling(temp),temp
+        endif
+     endif
+  enddo
+  print *, 'ok'
+  
   print *, 'SUCCESS!'
 
-  ! Here is some legacy test code that could be recovered...
-!       program test_intmath
-!       use intmath
-!       implicit none
-!       real(kind=16) :: temp
-!       real(kind=16), parameter :: alog2=log(2.0_16)
-!       integer(kind=8), parameter  :: &
-!      &     one=1,big=Z'7FFFFFFFFFFFFFFF',small=-2000000_8, &
-!      &     check=Z'1FFFFFFF'
-!       integer(kind=8) :: ival, iret
-!       !$OMP PARALLEL DO PRIVATE(ival,temp,iret)
-!       do ival=small,big
-!  10      format(Z16,' -- MISMATCH: ',I0,'=>',I0,' (',I0,' = ',F0.10,')')
-!  20      format(Z16,' -- OKAY:     ',I0,'=>',I0,' (',I0,' = ',F0.10,')')
-!          if(ival+one<ival) then
-!             temp=log(real(max(ival,one),kind=16))/alog2
-!          else
-!             temp=log(real(max(ival+one,one),kind=16))/alog2
-!          endif
-!          iret=i1log2(ival)
-!          if(iret/=ceiling(temp) .or. ival==0 .or. ival==check) then
-!             !$OMP CRITICAL
-!             if(iret/=ceiling(temp)) then
-!                print 10, ival, ival, iret,ceiling(temp),temp
-!             else
-!                print 20, ival, ival, iret,ceiling(temp),temp
-!             endif
-!             !$OMP END CRITICAL
-!          endif
-!       enddo
-!       !$OMP END PARALLEL DO
-!       end program test_intmath
-  
 end program test_intmath
 
