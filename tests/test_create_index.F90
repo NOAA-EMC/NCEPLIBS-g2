2ce20d6c4cfd13a8df17e3b108b6a78d5c067718
Ed <edward.hartnett@noaa.gov>
2024-02-16 09:47:14 -0700

    uncommented more test code, again

--- tests/test_create_index.F90
+++ tests/test_create_index.F90
@@ -53,7 +53,7 @@ program test_create_index
   ! Read the index file.
   call getg2i2(lugi, cbuf, idxver, nlen, nnum, iret)
   print *, nlen, nnum, iret
-!  if (nlen .ne. 3800 .or. nnum .ne. 59 .or. iret .ne. 0) stop 80
+  if (nlen .ne. 3800 .or. nnum .ne. 59 .or. iret .ne. 0) stop 80
 
   ! Close the index file.
   call baclose(lugi, iret)
