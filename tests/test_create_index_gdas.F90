ab15c4d5054a06c382a546ad927f9ce080acb3a9
Ed <edward.hartnett@noaa.gov>
2024-02-17 14:45:23 -0700

    new test

--- tests/test_create_index_gdas.F90
+++ tests/test_create_index_gdas.F90
@@ -11,7 +11,7 @@ program test_create_index_gdas
 
   ! These are the test files we will use.
   character(*) :: TEST_FILE_GDAS
-  parameter (TEST_FILE_GDAS = 'gdaswave.t00z.wcoast.0p16.f000.grib2')
+  parameter (TEST_FILE_GDAS = 'gdas.t12z.pgrb2.1p00.anl.grib2')
   character(*) :: TEST_FILE_GDAS_INDEX
   parameter (TEST_FILE_GDAS_INDEX = 'test_index_gdas.grb2index')
   character(len=1), pointer, dimension(:) :: cbuf(:)
@@ -21,56 +21,22 @@ program test_create_index_gdas
   integer :: i
   integer :: k, lpos, iret
   type(gribfield) :: gfld
-  integer :: expected_idsect(13) = (/ 7, 0, 2, 1, 1, 2021, 11, 30, 0, 0, 0, 0, 1 /)
-  integer :: expected_igdtmpl(19) = (/ 6, 0, 0, 0, 0, 0, 0, 241, 151, 0, 0, 50000000, &
-       210000000, 48, 25000000, 250000000, 166667, 166667, 0 /)
+  integer :: expected_idsect(13) = (/ 7, 0, 2, 1, 0, 2022, 11, 6, 12, 0, 0, 0, 0 /)
+  integer :: expected_igdtmpl(19) = (/ 6, 0, 0, 0, 0, 0, 0, 360, 181, 0, -1, &
+       90000000, 0, 48, -90000000, 359000000, 1000000, 1000000, 0 /)
 
   ! These are the PDT templates of the 19 messages in the test file,
   ! verified with degrib2.
-  integer :: expected_ipdtmpl(15, 19) = reshape((/ &
-       2, 1, 2, 0, 11, 0, 0, 1, 0, 1, 0, 1, 255, 0, 0, &
-       2, 0, 2, 0, 11, 0, 0, 1, 0, 1, 0, 1, 255, 0, 0, &
-       2, 2, 2, 0, 11, 0, 0, 1, 0, 1, 0, 1, 255, 0, 0, &
-       2, 3, 2, 0, 11, 0, 0, 1, 0, 1, 0, 1, 255, 0, 0, &
-       0, 3, 2, 0, 11, 0, 0, 1, 0, 1, 0, 1, 255, 0, 0, &
-       0, 11, 2, 0, 11, 0, 0, 1, 0, 1, 0, 1, 255, 0, 0, &
-       0, 10, 2, 0, 11, 0, 0, 1, 0, 1, 0, 1, 255, 0, 0, &
-       0, 5, 2, 0, 11, 0, 0, 1, 0, 1, 0, 1, 255, 0, 0, &
-       0, 8, 2, 0, 11, 0, 0, 1, 0, 241, 0, 1, 255, 0, 0, &
-       0, 8, 2, 0, 11, 0, 0, 1, 0, 241, 0, 2, 255, 0, 0, &
-       0, 8, 2, 0, 11, 0, 0, 1, 0, 241, 0, 3, 255, 0, 0, &
-       0, 6, 2, 0, 11, 0, 0, 1, 0, 1, 0, 1, 255, 0, 0, &
-       0, 9, 2, 0, 11, 0, 0, 1, 0, 241, 0, 1, 255, 0, 0, &
-       0, 9, 2, 0, 11, 0, 0, 1, 0, 241, 0, 2, 255, 0, 0, &
-       0, 9, 2, 0, 11, 0, 0, 1, 0, 241, 0, 3, 255, 0, 0, &
-       0, 4, 2, 0, 11, 0, 0, 1, 0, 1, 0, 1, 255, 0, 0, &
-       0, 7, 2, 0, 11, 0, 0, 1, 0, 241, 0, 1, 255, 0, 0, &
-       0, 7, 2, 0, 11, 0, 0, 1, 0, 241, 0, 2, 255, 0, 0, &
-       0, 7, 2, 0, 11, 0, 0, 1, 0, 241, 0, 3, 255, 0, 0 /), &
+  integer :: expected_ipdtmpl(15, 2) = reshape((/ &
+       2, 10, 0, 0, 81, 0, 0, 1, 0, 100, 0, 80000, 255, 0, 0, &
+       2, 10, 0, 0, 81, 0, 0, 1, 0, 100, 0, 85000, 255, 0, 0 /), &
        shape(expected_ipdtmpl))
   
   ! These are the DRT templates of the 19 messages in the test file,
   ! verified with degrib2.
-  integer :: expected_idrtmpl(7, 19) = reshape((/ &
-        1092616192, 0, 2, 11, 0, 0, 255, &
-        1065353216, 0, 2, 16, 0, 0, 255, &
-        -1006403584, 0, 2, 11, 0, 0, 255, &
-        -996777984, 0, 2, 12, 0, 0, 255, &
-        1102053376, 0, 2, 9, 0, 0, 255, &
-        1144815616, 0, 2, 10, 0, 0, 255, &
-        1185159680, 0, 2, 14, 0, 0, 255, &
-        1086324736, 0, 2, 9, 0, 0, 255, &
-        1095761920, 0, 2, 9, 0, 0, 255, &
-        1086324736, 0, 2, 8, 0, 0, 255, &
-        1084227584, 0, 2, 7, 0, 0, 255, &
-        1125908480, 0, 2, 11, 0, 0, 255, &
-        1136328704, 0, 2, 11, 0, 0, 255, &
-        1135411200, 0, 2, 11, 0, 0, 255, &
-        1133510656, 0, 2, 11, 0, 0, 255, &
-        0, 0, 2, 16, 0, 0, 255, &
-        1183603200, 0, 2, 14, 0, 0, 255, &
-        1140424704, 0, 2, 16, 0, 0, 255, &
-        1092616192, 0, 2, 16, 0, 0, 255 /), &
+  integer :: expected_idrtmpl(18, 2) = reshape((/ &
+       -1000579487, 0, 6, 12, 0, 1, 0, 1649987994, -1, 2178, 0, 4, 1, 1, 104, 8, 2, 2, &
+       -997588251, 0, 6, 12, 0, 1, 0, 1649987994, -1, 2159, 0, 4, 1, 1, 104, 7, 2, 2 /), &
         shape(expected_idrtmpl))
 
   integer :: ios
@@ -93,7 +59,7 @@ program test_create_index_gdas
      print *, 'testing g2_create_index version ', idxver
 
      ! Open GRIB2 file for reading.
-     call baopenr(lugb, TEST_FILE_GDAS, ios)
+     call baopenr(lugb, 'data/' // TEST_FILE_GDAS, ios)
      if (ios .ne. 0) stop 2
 
      ! Open output file where index will be written.
@@ -117,14 +83,14 @@ program test_create_index_gdas
 
      ! Read the index file.
      call getg2i2(lugi, cbuf, myidxver, nlen, nnum, iret)
-     !print *, myidxver, nlen, nnum, iret
+     print *, myidxver, nlen, nnum, iret
      if (myidxver .ne. idxver) stop 79
      if (idxver .eq. 1) then
-        if (nlen .ne. 3800) stop 80
+        if (nlen .ne. 452) stop 80
      else
-        if (nlen .ne. 3876) stop 80
+        if (nlen .ne. 460) stop 80
      endif
-     if (nnum .ne. 19 .or. iret .ne. 0) stop 81
+     if (nnum .ne. 2 .or. iret .ne. 0) stop 81
 
      ! Close the index file.
      call baclose(lugi, iret)
@@ -146,11 +112,19 @@ program test_create_index_gdas
      do i = 1, 250
         jgdt(i) = -9999
      end do
-     do j = 1, 19
+     do j = 1, nnum
         print *, '   testing unpacking index buffer with getgb2s2() for message', j
         call getgb2s2(cbuf, idxver, nlen, nnum, j - 1, jdisc, jids, jpdtn, jpdt, jgdtn, &
              jgdt, k, gfld, lpos, iret)
         if (iret .ne. 0) stop 101
+
+        print *, gfld%version, gfld%discipline, gfld%idsectlen, gfld%ifldnum, gfld%griddef
+        print *, gfld%ngrdpts, gfld%numoct_opt, gfld%interp_opt, gfld%num_opt
+        print *, gfld%igdtnum, gfld%igdtlen
+        print *, gfld%ipdtnum, gfld%ipdtlen, gfld%num_coord
+        print *, gfld%unpacked, gfld%ibmap
+        print *, 'sec1:', gfld%idsect
+        print *, 'gdt:', gfld%igdtmpl
 
         ! Check that the information is correct for the first record.
         if (gfld%version .ne. 2) stop 102
@@ -162,12 +136,12 @@ program test_create_index_gdas
         if (gfld%idsectlen .ne. 13) stop 110
         if (gfld%ifldnum .ne. 1) stop 111
         if (gfld%griddef .ne. 0) stop 112
-        if (gfld%ngrdpts .ne. 36391) stop 120
+        if (gfld%ngrdpts .ne. 65160) stop 120
         if (gfld%numoct_opt .ne. 0 .or. gfld%interp_opt .ne. 0 .or. gfld%num_opt .ne. 0) stop 122
         if (gfld%igdtnum .ne. 0 .or. gfld%igdtlen .ne. 19) stop 123
         if (gfld%ipdtnum .ne. 0 .or. gfld%ipdtlen .ne. 15 .or. gfld%num_coord .ne. 0) stop 130
         if (gfld%unpacked .neqv. .FALSE.) stop 131
-        if (gfld%ibmap .ne. 0) stop 132
+        if (gfld%ibmap .ne. 255) stop 132
         do i = 1, gfld%idsectlen
            if (gfld%idsect(i) .ne. expected_idsect(i)) stop 200
         end do
