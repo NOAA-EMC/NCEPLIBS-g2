3d6f82d1482c931da177219f2b2cce9273fb931f
Edward Hartnett <edward.hartnett@noaa.gov>
2024-03-06 06:51:13 -0700

    combined files into g2get.F90

--- src/g2get.F90
+++ src/g2get.F90
@@ -1,7 +1,6 @@
 !> @file
-!> @brief Return the Grid Definition, Product Definition, Bit-map (if
-!> applicable), and the unpacked data for a data field.
-!> @author Stephen Gilbert @date 2000-05-26
+!> @brief Get information from a GRIB2 message.
+!> @author Edward Hartnett @date Mar 6, 2024
 
 !> Return the Grid Definition, Product Definition, Bit-map (if
 !> applicable), and the unpacked data for a data field. Since there
@@ -746,3 +745,494 @@ subroutine unpack6(cgrib, lcgrib, iofst, ngpts, ibmap, bmap, ierr)
      ierr = 4
   endif
 end subroutine unpack6
+
+!> Return the dimensions and scanning mode of a grid
+!> definition packed in the Grid Definition Section.
+!>
+!> @param[in] csec3 Character array that contains the packed GRIB2
+!> GDS.
+!> @param[in] lcsec3 Length (in octets) of section 3.
+!> @param[out] width x (or i) dimension of the grid.
+!> @param[out] height y (or j) dimension of the grid.
+!> @param[out] iscan Scanning mode (see [Code Table 3.4]
+!> (https://www.nco.ncep.noaa.gov/pmb/docs/grib2/grib2_doc/grib2_table3-4.shtml)).
+!>
+!> @note Returns width and height set to zero, if grid template not
+!> recognized.
+!>
+!> @author Stephen Gilbert @date 2002-12-11
+subroutine getdim(csec3,lcsec3,width,height,iscan)
+  implicit none
+
+  character(len=1),intent(in) :: csec3(*)
+  integer,intent(in) :: lcsec3
+  integer,intent(out) :: width,height,iscan
+
+  integer,pointer,dimension(:) :: igdstmpl,list_opt
+  integer :: igds(5)
+  integer iofst,igdtlen,num_opt,jerr
+
+  interface
+     subroutine gf_unpack3(cgrib,lcgrib,iofst,igds,igdstmpl, &
+          mapgridlen,ideflist,idefnum,ierr)
+       character(len=1),intent(in) :: cgrib(lcgrib)
+       integer,intent(in) :: lcgrib
+       integer,intent(inout) :: iofst
+       integer,pointer,dimension(:) :: igdstmpl,ideflist
+       integer,intent(out) :: igds(5)
+       integer,intent(out) :: ierr,idefnum
+     end subroutine gf_unpack3
+  end interface
+
+  nullify(igdstmpl,list_opt)
+
+  iofst=0       ! set offset to beginning of section
+  call gf_unpack3(csec3,lcsec3,iofst,igds,igdstmpl, &
+       igdtlen,list_opt,num_opt,jerr)
+  if (jerr.eq.0) then
+     selectcase( igds(5) )     !  Template number
+     case (0:3)   ! Lat/Lon
+        width=igdstmpl(8)
+        height=igdstmpl(9)
+        iscan=igdstmpl(19)
+     case (10)   ! Mercator
+        width=igdstmpl(8)
+        height=igdstmpl(9)
+        iscan=igdstmpl(16)
+     case (20)   ! Polar Stereographic
+        width=igdstmpl(8)
+        height=igdstmpl(9)
+        iscan=igdstmpl(18)
+     case (30)   ! Lambert Conformal
+        width=igdstmpl(8)
+        height=igdstmpl(9)
+        iscan=igdstmpl(18)
+     case (40:43)   ! Gaussian
+        width=igdstmpl(8)
+        height=igdstmpl(9)
+        iscan=igdstmpl(19)
+     case (90)   ! Space View/Orthographic
+        width=igdstmpl(8)
+        height=igdstmpl(9)
+        iscan=igdstmpl(17)
+     case (110)   ! Equatorial Azimuthal
+        width=igdstmpl(8)
+        height=igdstmpl(9)
+        iscan=igdstmpl(16)
+     case default
+        width=0
+        height=0
+        iscan=0
+     end select
+  else
+     width=0
+     height=0
+  endif
+
+  if (associated(igdstmpl)) deallocate(igdstmpl)
+  if (associated(list_opt)) deallocate(list_opt)
+
+  return
+end subroutine getdim
+
+!> Return the contents of Section 2 (Local Use) from a GRIB2 message.
+!>
+!> Since there can be multiple occurrences of Section 2 within a GRIB
+!> message, the calling routine indicates which occurrence is being
+!> requested with the localnum argument.
+!>
+!> @note Note that subroutine gb_info() can be used to first determine
+!> how many Local Use sections exist in a given GRIB message.
+!>
+!> @param[in] cgrib Character array that contains the GRIB2 message.
+!> @param[in] lcgrib Length (in bytes) of GRIB message array cgrib.
+!> @param[in] localnum The nth occurrence of Section 2 requested.
+!> @param[out] csec2 Character array containing information read from
+!> Section 2. The dimension of this array can be obtained in advance
+!> from argument maxlocal, which is returned from subroutine gb_info().
+!> @param[out] lcsec2 Number of bytes of character array csec2 read
+!> from Section 2.
+!> @param[out] ierr Error return code.
+!> - 0 no error.
+!> - 1 Beginning characters "GRIB" not found.
+!> - 2 GRIB message is not Edition 2.
+!> - 3 The data field request number was not positive.
+!> - 4 End string "7777" found, but not where expected.
+!> - 5 End string "7777" not found at end of message.
+!> - 6 GRIB message did not contain the requested number of data fields.
+!>
+!> @author Stephen Gilbert @date 2000-05-25
+subroutine getlocal(cgrib, lcgrib, localnum, csec2, lcsec2, ierr)
+  implicit none
+
+  character(len = 1), intent(in) :: cgrib(lcgrib)
+  integer, intent(in) :: lcgrib, localnum
+  character(len = 1), intent(out) :: csec2(*)
+  integer, intent(out) :: lcsec2, ierr
+
+  character(len = 4), parameter :: grib = 'GRIB', c7777 = '7777'
+  character(len = 4) :: ctemp
+  integer :: listsec0(2)
+  integer iofst, istart, numlocal
+  integer :: lengrib, lensec, lensec0, j, ipos, isecnum
+
+  ierr = 0
+  numlocal = 0
+
+  ! Check for valid request number.
+  if (localnum .le. 0) then
+     print *, 'getlocal: Request for local section must be positive.'
+     ierr = 3
+     return
+  endif
+
+  ! Check for beginning of GRIB message in the first 100 bytes
+  istart = 0
+  do j = 1, 100
+     ctemp = cgrib(j)//cgrib(j+1)//cgrib(j+2)//cgrib(j+3)
+     if (ctemp .eq. grib) then
+        istart = j
+        exit
+     endif
+  enddo
+  if (istart .eq. 0) then
+     print *, 'getlocal:  Beginning characters GRIB not found.'
+     ierr = 1
+     return
+  endif
+
+  ! Unpack Section 0 - Indicator Section
+  iofst = 8 * (istart + 5)
+  call g2_gbytec(cgrib, listsec0(1), iofst, 8)     ! Discipline
+  iofst = iofst + 8
+  call g2_gbytec(cgrib, listsec0(2), iofst, 8)     ! GRIB edition number
+  iofst = iofst + 8
+  iofst = iofst + 32
+  call g2_gbytec(cgrib, lengrib, iofst, 32)        ! Length of GRIB message
+  iofst = iofst + 32
+  lensec0 = 16
+  ipos = istart + lensec0
+
+  ! Currently handles only GRIB Edition 2.
+  if (listsec0(2) .ne. 2) then
+     print *, 'getlocal: can only decode GRIB edition 2.'
+     ierr = 2
+     return
+  endif
+
+  ! Loop through the remaining sections keeping track of the length of
+  ! each. Also check to see that if the current occurrence of Section
+  ! 2 is the same as the one requested.
+  do
+     ! Check to see if we are at end of GRIB message
+     ctemp = cgrib(ipos) // cgrib(ipos + 1) // cgrib(ipos + 2) // cgrib(ipos + 3)
+     if (ctemp .eq. c7777) then
+        ipos = ipos + 4
+        
+        ! If end of GRIB message not where expected, issue error
+        if (ipos .ne. (istart + lengrib)) then
+           print *, 'getlocal: "7777" found, but not where expected.'
+           ierr = 4
+           return
+        endif
+        exit
+     endif
+
+     ! Get length of Section and Section number
+     iofst = (ipos - 1) * 8
+     call g2_gbytec(cgrib, lensec, iofst, 32)        ! Get Length of Section
+     iofst = iofst + 32
+     call g2_gbytec(cgrib, isecnum, iofst, 8)         ! Get Section number
+     iofst = iofst + 8
+     
+     ! If found the requested occurrence of Section 2, 
+     ! return the section contents.
+     if (isecnum .eq. 2) then
+        numlocal = numlocal + 1
+        if (numlocal.eq.localnum) then
+           lcsec2 = lensec - 5
+           csec2(1:lcsec2) = cgrib(ipos + 5:ipos + lensec - 1)
+           return
+        endif
+     endif
+     
+     ! Check to see if we read pass the end of the GRIB
+     ! message and missed the terminator string '7777'.
+     ipos = ipos + lensec                 ! Update beginning of section pointer
+     if (ipos .gt. (istart + lengrib)) then
+        print *, 'getlocal: "7777"  not found at end of GRIB message.'
+        ierr = 5
+        return
+     endif
+  enddo
+
+  ! If exited from above loop, the end of the GRIB message was reached
+  ! before the requested occurrence of section 2 was found.
+  print *, 'getlocal: GRIB message contained ', numlocal, ' local sections.'
+  print *, 'getlocal: The request was for the ', localnum,  ' occurrence.'
+  ierr = 6
+
+end subroutine getlocal
+
+!> Return the J, K, and M pentagonal resolution
+!> parameters specified in a GRIB2 Grid Definition Section used
+!> spherical harmonic coefficients using GDT 5.50 through 5.53.
+!>
+!> @param[in] csec3 Character array containing the packed GRIB2 GDS
+!> @param[in] lcsec3 Length (in octets) of section 3
+!> @param[out] JJ =J pentagonal resolution parameter
+!> @param[out] KK =K pentagonal resolution parameter
+!> @param[out] MM =M pentagonal resolution parameter
+!>
+!> @note Returns JJ, KK, and MM set to zero, if grid template not
+!> recognized.
+!>
+!> @author Stephen Gilbert @date 2002-12-11
+subroutine getpoly(csec3,lcsec3,jj,kk,mm)
+  implicit none
+
+  character(len=1),intent(in) :: csec3(*)
+  integer,intent(in) :: lcsec3
+  integer,intent(out) :: jj,kk,mm
+
+  integer,pointer,dimension(:) :: igdstmpl,list_opt
+  integer :: igds(5)
+  integer iofst,igdtlen,num_opt,jerr
+
+  interface
+     subroutine gf_unpack3(cgrib,lcgrib,iofst,igds,igdstmpl, &
+          mapgridlen,ideflist,idefnum,ierr)
+       character(len=1),intent(in) :: cgrib(lcgrib)
+       integer,intent(in) :: lcgrib
+       integer,intent(inout) :: iofst
+       integer,pointer,dimension(:) :: igdstmpl,ideflist
+       integer,intent(out) :: igds(5)
+       integer,intent(out) :: ierr,idefnum
+     end subroutine gf_unpack3
+  end interface
+
+  nullify(igdstmpl,list_opt)
+
+  iofst=0       ! set offset to beginning of section
+  call gf_unpack3(csec3,lcsec3,iofst,igds,igdstmpl, &
+       igdtlen,list_opt,num_opt,jerr)
+  if (jerr.eq.0) then
+     selectcase( igds(5) )     !  Template number
+     case (50:53)   ! Spherical harmonic coefficients
+        jj=igdstmpl(1)
+        kk=igdstmpl(2)
+        mm=igdstmpl(3)
+     case default
+        jj=0
+        kk=0
+        mm=0
+     end select
+  else
+     jj=0
+     kk=0
+     mm=0
+  endif
+
+  if (associated(igdstmpl)) deallocate(igdstmpl)
+  if (associated(list_opt)) deallocate(list_opt)
+
+end subroutine getpoly
+
+!> Return the Grid Definition and Product Definition for a data field.
+!> Since there can be multiple data fields packed into a GRIB2
+!> message, the calling routine indicates which field is being
+!> requested with the ifldnum argument.
+!>
+!> @note Note that subroutine gribinfo() can be used to first
+!> determine how many data fields exist in a given GRIB message.
+!>
+!> @param[in] cgrib Character array that contains the GRIB2 message
+!> @param[in] lcgrib Length (in bytes) of GRIB message array cgrib.
+!> @param[in] ifldnum Specifies which field in the GRIB2 message to return.
+!> @param[out] igds Contains information read from the appropriate GRIB Grid
+!> Definition Section 3 for the field being returned. Must be dimensioned >= 5.
+!> - igds(1) Source of grid definition (see Code Table 3.0)
+!> - igds(2) Number of grid points in the defined grid.
+!> - igds(3) Number of octets needed for each additional grid points definition.
+!> Used to define number of points in each row (or column) for non-regular
+!> grids. = 0, if using regular grid.
+!> - igds(4) Interpretation of list for optional points definition. (Code Table 3.11)
+!> - igds(5) Grid Definition Template Number (Code Table 3.1)
+!> @param[out] igdstmpl Contains the data values for the specified Grid Definition
+!> Template (NN=igds(5)). Each element of this integer array contains an entry
+!> (in the order specified) of Grid Defintion Template 3.NN.
+!> @param[out] igdslen Number of elements in igdstmpl. i.e. number of entries
+!> in Data Representation Template 5.N (N=idrsnum).
+!> @param[out] ideflist (Used if igds(3) .ne. 0) This array contains the
+!> number of grid points contained in each row (or column). (part of Section 3).
+!> @param[out] idefnum (Used if igds(3) .ne. 0) The number of entries in array
+!> ideflist. i.e. number of rows (or columns) for which optional grid points are defined.
+!> @param[out] ipdsnum Product Definition Template Number (see Code Table 4.0).
+!> @param[out] ipdstmpl Contains the data values for the specified Product Definition
+!> Template (N=ipdsnum). Each element of this integer array contains an entry
+!> (in the order specified) of Product Defintion Template 4.N. A safe
+!> dimension for this array can be obtained in advance from maxvals(4),
+!> which is returned from subroutine gribinfo().
+!> @param[out] ipdslen Number of elements in ipdstmpl. i.e. number of entries
+!> in Product Defintion Template 4.N (N=ipdsnum).
+!> @param[out] coordlist Array containg floating point values intended to
+!> document the vertical discretisation associated to model data on hybrid
+!> coordinate vertical levels. (part of Section 4) The dimension of this
+!> array can be obtained in advance from maxvals(5), which is returned
+!> from subroutine gribinfo().
+!> @param[out] numcoord number of values in array coordlist.
+!> @param[out] ierr Error return code.
+!> - 0 no error.
+!> - 1 Beginning characters "GRIB" not found.
+!> - 2 GRIB message is not Edition 2.
+!> - 3 The data field request number was not positive.
+!> - 4 End string "7777" found, but not where expected.
+!> - 5 End string "7777" not found at end of message.
+!> - 6 GRIB message did not contain the requested number of data fields.
+!> - 7 End string "7777" not found at end of message.
+!> - 9 Data Representation Template 5.NN not yet implemented.
+!> - 10 Error unpacking Section 3.
+!> - 11 Error unpacking Section 4.
+!> - 12 Error unpacking Section 5.
+!> - 13 Error unpacking Section 6.
+!> - 14 Error unpacking Section 7.
+!>
+!> @author Stephen Gilbert @date 2000-05-26
+subroutine gettemplates(cgrib, lcgrib, ifldnum, igds, igdstmpl, &
+     igdslen, ideflist, idefnum, ipdsnum, ipdstmpl, ipdslen, coordlist, numcoord, ierr)
+  implicit none
+
+  character(len = 1), intent(in) :: cgrib(lcgrib)
+  integer, intent(in) :: lcgrib, ifldnum
+  integer, intent(out) :: igds(*), igdstmpl(*), ideflist(*)
+  integer, intent(out) :: ipdsnum, ipdstmpl(*)
+  integer, intent(out) :: idefnum, numcoord
+  integer, intent(out) :: ierr
+  real, intent(out) :: coordlist(*)
+
+  character(len = 4), parameter :: grib = 'GRIB', c7777 = '7777'
+  character(len = 4) :: ctemp
+  integer:: listsec0(2)
+  integer iofst, istart
+  logical have3, have4
+  integer :: igdslen, ipdslen, ipos, isecnum, j, jerr, lengrib, lensec, lensec0, numfld
+
+  have3 = .false.
+  have4 = .false.
+  ierr = 0
+  numfld = 0
+
+  ! Check for valid request number.
+  if (ifldnum .le. 0) then
+     print *, 'gettemplates: Request for field number must be positive.'
+     ierr = 3
+     return
+  endif
+
+  ! Check for beginning of GRIB message in the first 100 bytes.
+  istart = 0
+  do j = 1, 100
+     ctemp = cgrib(j) // cgrib(j + 1) // cgrib(j + 2) // cgrib(j + 3)
+     if (ctemp .eq. grib ) then
+        istart = j
+        exit
+     endif
+  enddo
+  if (istart .eq. 0) then
+     print *, 'gettemplates:  Beginning characters GRIB not found.'
+     ierr = 1
+     return
+  endif
+
+  ! Unpack Section 0 - Indicator Section.
+  iofst = 8 * (istart + 5)
+  call g2_gbytec(cgrib, listsec0(1), iofst, 8)     ! Discipline
+  iofst = iofst + 8
+  call g2_gbytec(cgrib, listsec0(2), iofst, 8)     ! GRIB edition number
+  iofst = iofst + 8
+  iofst = iofst + 32
+  call g2_gbytec(cgrib, lengrib, iofst, 32)        ! Length of GRIB message
+  iofst = iofst + 32
+  lensec0 = 16
+  ipos = istart + lensec0
+
+  ! Currently handles only GRIB Edition 2.
+  if (listsec0(2) .ne. 2) then
+     print *, 'gettemplates: can only decode GRIB edition 2.'
+     ierr = 2
+     return
+  endif
+
+  ! Loop through the remaining sections keeping track of the length of
+  ! each. Also keep the latest Grid Definition Section info. Unpack
+  ! the requested field number.
+  do
+     ! Check to see if we are at end of GRIB message.
+     ctemp = cgrib(ipos) // cgrib(ipos + 1) // cgrib(ipos + 2) // cgrib(ipos + 3)
+     if (ctemp .eq. c7777 ) then
+        ipos = ipos+4
+        ! If end of GRIB message not where expected, issue error
+        if (ipos .ne. (istart + lengrib)) then
+           print *, 'gettemplates: "7777" found, but not where expected.'
+           ierr = 4
+           return
+        endif
+        exit
+     endif
+     ! Get length of Section and Section number.
+     iofst = (ipos - 1) * 8
+     call g2_gbytec(cgrib, lensec, iofst, 32)        ! Get Length of Section
+     iofst = iofst + 32
+     call g2_gbytec(cgrib, isecnum, iofst, 8)         ! Get Section number
+     iofst = iofst + 8
+     !print *, ' lensec =  ', lensec, '    secnum =  ', isecnum
+
+     ! If found Section 3, unpack the GDS info using the appropriate
+     ! template. Save in case this is the latest grid before the
+     ! requested field.
+     if (isecnum .eq. 3) then
+        iofst = iofst - 40       ! reset offset to beginning of section
+        call unpack3(cgrib, lcgrib, iofst, igds, igdstmpl, igdslen, ideflist, idefnum, jerr)
+        if (jerr .eq. 0) then
+           have3 = .true.
+        else
+           ierr = 10
+           return
+        endif
+     endif
+
+     ! If found Section 4, check to see if this field is the one
+     ! requested.
+     if (isecnum .eq. 4) then
+        numfld = numfld + 1
+        if (numfld .eq. ifldnum) then
+           iofst = iofst - 40       ! reset offset to beginning of section
+           call unpack4(cgrib, lcgrib, iofst, ipdsnum, ipdstmpl, ipdslen, coordlist, numcoord, jerr)
+           if (jerr .eq. 0) then
+              have4 = .true.
+           else
+              ierr = 11
+              return
+           endif
+        endif
+     endif
+
+     ! Check to see if we read pass the end of the GRIB message and
+     ! missed the terminator string '7777'.
+     ipos = ipos + lensec                 ! Update beginning of section pointer
+     if (ipos .gt. (istart + lengrib)) then
+        print *, 'gettemplates: "7777"  not found at end of GRIB message.'
+        ierr = 7
+        return
+     endif
+
+     if (have3.and.have4) return
+  enddo
+
+  ! If exited from above loop, the end of the GRIB message was reached
+  ! before the requested field was found.
+  print *, 'gettemplates: GRIB message contained ', numfld, ' different fields.'
+  print *, 'gettemplates: The request was for the ', ifldnum,  ' field.'
+  ierr = 6
+
+end subroutine gettemplates
