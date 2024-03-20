89d8be44d391140197f0fa0e5a0b85ca75164611
Edward Hartnett <edward.hartnett@noaa.gov>
2024-03-20 12:26:32 -0600

    more testing

--- tests/test_files.F90
+++ tests/test_files.F90
@@ -17,6 +17,11 @@ program test_files
        210000000, 48, 25000000, 250000000, 166667, 166667, 0 /)
   integer :: expected_ipdtmpl(15) = (/ 2, 1, 2, 0, 11, 0, 0, 1, 0, 1, 0, 1, 255, 0, 0 /)
   integer :: expected_idrtmpl(7) = (/ 1092616192, 0, 2, 11, 0, 0, 255 /)
+  integer :: expected_19_idsect(13) = (/ 7, 0, 2, 1, 1, 2021, 11, 30, 0, 0, 0, 0, 1 /)
+  integer :: expected_19_igdtmpl(19) = (/ 6, 0, 0, 0, 0, 0, 0, 241, 151, 0, 0, 50000000, &
+       210000000, 48, 25000000, 250000000, 166667, 166667, 0 /)
+  integer :: expected_19_ipdtmpl(15) = (/ 0, 7, 2, 0, 11, 0, 0, 1, 0, 241, 0, 3, 255, 0, 0 /)
+  integer :: expected_19_idrtmpl(7) = (/ 1092616192, 0, 2, 16, 0, 0, 255 /)
   integer :: i, idxver, iret, j, k, idx_test, lugi_val(4) = (/ 11, 0, -11, 10 /)
   
   print *, 'Testing reading GRIB2 file gdaswave.t00z.wcoast.0p16.f000.grib2...'
@@ -45,6 +50,8 @@ program test_files
         jgdt = -9999
 
         ! Find and unpack the first GRIB2 message in the file.
+        print *, '         testing with message 1'
+        jskp = 0
         call getgb2i2(lugb, lugi_val(idx_test), jskp, jdisc, jids, jpdtn, jpdt, jgdtn, jgdt,  &
              unpack, idxver, k, gfld, iret)
         if (iret .ne. 0) stop 3
@@ -80,20 +87,55 @@ program test_files
         !    print *, gfld%fld(1)
         ! end do
 
-        ! Close the files.
-        call baclose(lugb, iret)  
-        if (iret .ne. 0) stop 200
-        call baclose(lugi, iret)  
-        if (iret .ne. 0) stop 201
+        ! Free memory.
+        call gf_free(gfld)
+
+        ! Find and unpack the first GRIB2 message in the file.
+        print *, '         testing with message 19'
+        jskp = 18
+        call getgb2i2(lugb, lugi_val(idx_test), jskp, jdisc, jids, jpdtn, jpdt, jgdtn, jgdt,  &
+             unpack, idxver, k, gfld, iret)
+        if (iret .ne. 0) stop 3
+
+        ! Make sure we found message 1 in the file.
+        if (k .ne. 19) stop 4
+
+        ! Check all the metadata.
+        if (gfld%version .ne. 2 .or. gfld%discipline .ne. 10 .or. gfld%idsectlen .ne. 13 .or. &
+             gfld%locallen .ne. 0 .or. gfld%ifldnum .ne. 1 .or. gfld%griddef .ne. 0 .or. &
+             gfld%ngrdpts .ne. 36391 .or. gfld%numoct_opt .ne. 0 .or. gfld%interp_opt .ne. 0 .or. &
+             gfld%num_opt .ne. 0 .or. gfld%igdtnum .ne. 0 .or. gfld%ipdtnum .ne. 0 .or. &
+             gfld%ipdtlen .ne. 15 .or. gfld%ndpts .ne. 10347 .or. gfld%idrtnum .ne. 40 .or. &
+             gfld%idrtlen .ne. 7 .or. gfld%unpacked .neqv. .false. .or. gfld%expanded .neqv. .true. .or. &
+             gfld%ibmap .ne. 0) stop 1010
+        do i = 1, 13
+           if (gfld%idsect(i) .ne. expected_19_idsect(i)) stop 1020
+        end do
+        do i = 1, 19
+           if (gfld%igdtmpl(i) .ne. expected_19_igdtmpl(i)) stop 1030
+        end do
+        do i = 1, 15
+           if (gfld%ipdtmpl(i) .ne. expected_19_ipdtmpl(i)) stop 1040
+        end do
+        do i = 1, 7
+           if (gfld%idrtmpl(i) .ne. expected_19_idrtmpl(i)) stop 1050
+        end do
 
         ! Free memory.
         call gf_free(gfld)
+        
+        ! Close the files.
+        call baclose(lugb, iret)  
+        if (iret .ne. 0) stop 2000
+        call baclose(lugi, iret)  
+        if (iret .ne. 0) stop 2010
+
      end do
   end do
   
   ! Free internal library memory.
   call gf_finalize(iret)
-  if (iret .ne. 0) stop 5
+  if (iret .ne. 0) stop 5000
 
   print *, 'OK!'
   print *, 'SUCCESS!'
