f9722a0cb2e0bdc7dd1f911e207ea13ba9d4548b
Edward Hartnett <edward.hartnett@noaa.gov>
2024-03-20 07:38:35 -0600

    more test development

--- tests/test_files_fv3.F90
+++ tests/test_files_fv3.F90
@@ -1,9 +1,10 @@
 ! This is a test program for NCEPLIBS-g2.
 !
-! This program tests reading a GRIB2 file.
+! This program tests reading the very large FV3 GRIB2 file, using
+! getgb2i2().
 !
-! Ed Hartnett 9/3/22
-program test_files
+! Ed Hartnett 3/20/24
+program test_files_fv3
   use grib_mod
   implicit none
   
@@ -19,15 +20,15 @@ program test_files
   integer :: expected_idrtmpl(7) = (/ 1092616192, 0, 2, 11, 0, 0, 255 /)
   integer :: i, idxver, iret, j, k
   
-  print *, 'Testing reading GRIB2 file gdaswave.t00z.wcoast.0p16.f000.grib2...'
+  print *, 'Testing reading GRIB2 file fv3lam.t00z.prslev.f000.grib2...'
 
   do j = 1, 2
      idxver = j
      ! Open the file.
-     call baopenr(lugb, 'gdaswave.t00z.wcoast.0p16.f000.grib2', iret)
+     call baopenr(lugb, 'data/fv3lam.t00z.prslev.f000.grib2', iret)
      if (iret .ne. 0) stop 2
 
-     ! Learn about the file.
+     ! Find the first message in the file.
      jids = -9999
      jpdt = -9999
      jgdt = -9999
@@ -36,29 +37,29 @@ program test_files
      if (iret .ne. 0) stop 3
      ! Make sure we found message 1 in the file.
      if (k .ne. 1) stop 4
-     if (gfld%version .ne. 2 .or. gfld%discipline .ne. 0 .or. gfld%idsectlen .ne. 13 .or. &
-          gfld%locallen .ne. 0 .or. gfld%ifldnum .ne. 1 .or. gfld%griddef .ne. 0 .or. &
-          gfld%ngrdpts .ne. 36391 .or. gfld%numoct_opt .ne. 0 .or. gfld%interp_opt .ne. 0 .or. &
-          gfld%num_opt .ne. 0 .or. gfld%igdtnum .ne. 0 .or. gfld%ipdtnum .ne. 0 .or. &
-          gfld%ipdtlen .ne. 15 .or. gfld%ndpts .ne. 11041 .or. gfld%idrtnum .ne. 40 .or. &
-          gfld%idrtlen .ne. 7 .or. gfld%unpacked .neqv. .false. .or. gfld%expanded .neqv. .true. .or. &
-          gfld%ibmap .ne. 0) stop 10
-     do i = 1, 13
-        if (gfld%idsect(i) .ne. expected_idsect(i)) stop 20
-     end do
-     do i = 1, 19
-        if (gfld%igdtmpl(i) .ne. expected_igdtmpl(i)) stop 30
-     end do
-     do i = 1, 15
-        if (gfld%ipdtmpl(i) .ne. expected_ipdtmpl(i)) then
-           print *, 'got gfld%ipdtmpl', gfld%ipdtmpl
-           print *, 'expected ', expected_ipdtmpl
-           stop 40
-        endif
-     end do
-     do i = 1, 7
-        if (gfld%idrtmpl(i) .ne. expected_idrtmpl(i)) stop 50
-     end do
+     ! if (gfld%version .ne. 2 .or. gfld%discipline .ne. 0 .or. gfld%idsectlen .ne. 13 .or. &
+     !      gfld%locallen .ne. 0 .or. gfld%ifldnum .ne. 1 .or. gfld%griddef .ne. 0 .or. &
+     !      gfld%ngrdpts .ne. 36391 .or. gfld%numoct_opt .ne. 0 .or. gfld%interp_opt .ne. 0 .or. &
+     !      gfld%num_opt .ne. 0 .or. gfld%igdtnum .ne. 0 .or. gfld%ipdtnum .ne. 0 .or. &
+     !      gfld%ipdtlen .ne. 15 .or. gfld%ndpts .ne. 11041 .or. gfld%idrtnum .ne. 40 .or. &
+     !      gfld%idrtlen .ne. 7 .or. gfld%unpacked .neqv. .false. .or. gfld%expanded .neqv. .true. .or. &
+     !      gfld%ibmap .ne. 0) stop 10
+     ! do i = 1, 13
+     !    if (gfld%idsect(i) .ne. expected_idsect(i)) stop 20
+     ! end do
+     ! do i = 1, 19
+     !    if (gfld%igdtmpl(i) .ne. expected_igdtmpl(i)) stop 30
+     ! end do
+     ! do i = 1, 15
+     !    if (gfld%ipdtmpl(i) .ne. expected_ipdtmpl(i)) then
+     !       print *, 'got gfld%ipdtmpl', gfld%ipdtmpl
+     !       print *, 'expected ', expected_ipdtmpl
+     !       stop 40
+     !    endif
+     ! end do
+     ! do i = 1, 7
+     !    if (gfld%idrtmpl(i) .ne. expected_idrtmpl(i)) stop 50
+     ! end do
      ! do i = 1, 100
      !    print *, gfld%fld(1)
      ! end do
@@ -78,4 +79,4 @@ program test_files
 
   print *, 'OK!'
   print *, 'SUCCESS!'
-end program test_files
+end program test_files_fv3
