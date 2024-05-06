7afb210d8eaa6cc9fedb4f125f9b1d866cfea58e
Edward Hartnett <edward.hartnett@noaa.gov>
2024-05-06 09:07:07 -0600

    more progress

--- src/g2getgb2.F90
+++ src/g2getgb2.F90
@@ -1096,6 +1096,7 @@ subroutine getgb2rp2(lugb, idxver, cindex, extract, gribm, leng, iret)
   integer :: lencur, len0, ibmap = 0, ipos, iskip
   integer :: len7 = 0, len8 = 0, len3 = 0, len4 = 0, len5 = 0, len6 = 0, len1 = 0, len2 = 0
   integer :: iskp2, iskp6, iskp7
+  integer (kind = 8) :: iskp2_8
   integer :: INT1_BITS, INT2_BITS, INT4_BITS, INT8_BITS
   parameter(INT1_BITS = 8, INT2_BITS = 16, INT4_BITS = 32, INT8_BITS = 64)
   integer :: mypos, inc = 0
@@ -1112,20 +1113,23 @@ subroutine getgb2rp2(lugb, idxver, cindex, extract, gribm, leng, iret)
         call g2_gbytec(cindex, iskip, mypos, INT4_BITS)    ! bytes to skip in file
         mypos = mypos + INT4_BITS
         iskip8 = iskip
+        call g2_gbytec(cindex, iskp2, mypos, INT4_BITS)    ! bytes to skip for section 2
+        mypos = mypos + INT4_BITS
+        iskp2_8 = iskp2
      else
         inc = 4
         call g2_gbytec8(cindex, iskip8, mypos, INT8_BITS)    ! bytes to skip in file
         mypos = mypos + INT8_BITS
         iskip = int(iskip8, kind(4))
+        call g2_gbytec8(cindex, iskp2_8, mypos, INT8_BITS)    ! bytes to skip for section 2
+        mypos = mypos + INT8_BITS
      endif
-     call g2_gbytec(cindex, iskp2, mypos, INT4_BITS)    ! bytes to skip for section 2
-     mypos = mypos + INT4_BITS
-     if (iskp2 .gt. 0) then
-        call bareadl(lugb, iskip8 + iskp2, 4_8, lread8, ctemp)
+     if (iskp2_8 .gt. 0) then
+        call bareadl(lugb, iskip8 + iskp2_8, 4_8, lread8, ctemp)
         call g2_gbytec(ctemp, len2, 0, INT4_BITS)      ! length of section 2
         allocate(csec2(len2))
         len2_8 = len2
-        call bareadl(lugb, iskip8 + iskp2, len2_8, lread8, csec2)
+        call bareadl(lugb, iskip8 + iskp2_8, len2_8, lread8, csec2)
      else
         len2 = 0
      endif
@@ -1187,7 +1191,7 @@ subroutine getgb2rp2(lugb, idxver, cindex, extract, gribm, leng, iret)
      ipos = 44 + inc + len1
 
      ! Copy Section 2, if necessary
-     if (iskp2 .gt. 0) then
+     if (iskp2_8 .gt. 0) then
         gribm(lencur + 1:lencur + len2) = csec2(1:len2)
         lencur = lencur + len2
      endif
