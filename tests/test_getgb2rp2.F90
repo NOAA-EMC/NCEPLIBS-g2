d3fb0d485bc835e4f8d972807e5ada02fd8bef3d
Edward Hartnett <edward.hartnett@noaa.gov>
2024-05-14 07:15:11 -0600

    fixed index documentation

--- tests/test_getgb2rp2.F90
+++ tests/test_getgb2rp2.F90
@@ -14,7 +14,7 @@ program test_getgb2rp2
   logical :: extract
   integer (kind = 8) :: leng8
   character(len=1), pointer, dimension(:) :: gribm
-  integer :: idxver = 1
+  integer :: idxver, i
   
   ! Interfaces are needed due to pointers in the parameter lists.
   interface
@@ -36,40 +36,54 @@ program test_getgb2rp2
 
   print *, 'Testing the getgb2rp() subroutine - expect and ignore error messages during test...'
 
-  ! Open a real GRIB2 file.
-  print *, 'Indexing a real GRIB2 file WW3_Regional_US_West_Coast_20220718_0000.grib2...'
-  call baopenr(lugb, "data/WW3_Regional_US_West_Coast_20220718_0000.grib2", iret)
-  if (iret .ne. 0) stop 100
+  do i = 1, 1
+     ! Open a real GRIB2 file.
+     print *, 'Indexing a real GRIB2 file WW3_Regional_US_West_Coast_20220718_0000.grib2...'
+     call baopenr(lugb, "data/WW3_Regional_US_West_Coast_20220718_0000.grib2", iret)
+     if (iret .ne. 0) stop 100
 
-  lugi = 0
-  call getidx2(lugb, lugi, idxver, cbuf, nlen, nnum, iret)
-  if (iret .ne. 0) stop 101
-  if (nlen .ne. 137600 .or. nnum .ne. 688) stop 102
-  print *, 'nlen, nnum: ', nlen, nnum
+     idxver = 2
+     lugi = 0
+     call getidx2(lugb, lugi, idxver, cbuf, nlen, nnum, iret)
+     if (iret .ne. 0) stop 101
+     if (nnum .ne. 688) stop 102
+     if (idxver .eq. 1) then
+        if (nlen .ne. 137600) then
+           print *, nlen
+           stop 103
+        endif
+     else
+        if (nlen .ne. 145856) then
+           print *, nlen
+           stop 103
+        endif
+     endif
+     print *, 'nlen, nnum: ', nlen, nnum
 
-  ! Extract the whole message.
-  extract = .false.
-  nullify(gribm)
-  call getgb2rp2(lugb, idxver, cbuf, extract, gribm, leng8, iret)
-  print *, 'leng8 ', leng8
-  if (leng8 .ne. 11183) stop 110
-  ! Deallocate buffer that got GRIB message.
-  deallocate(gribm)
-  
-  ! Extract just the field (same result).
-  extract = .true.
-  call getgb2rp2(lugb, idxver, cbuf, extract, gribm, leng8, iret)
-  print *, 'leng8 ', leng8
-  if (leng8 .ne. 11183) stop 110
-  ! Deallocate buffer that got GRIB message.
-  deallocate(gribm)
-  
-  ! Deallocate the buffer that holds index.
-  deallocate(cbuf)
-  
-  call baclose(lugb, iret)
-  if (iret .ne. 0) stop 199
+     ! Extract the whole message.
+     extract = .false.
+     nullify(gribm)
+     call getgb2rp2(lugb, idxver, cbuf, extract, gribm, leng8, iret)
+     print *, 'leng8 ', leng8
+     if (leng8 .ne. 11183) stop 110
+     ! Deallocate buffer that got GRIB message.
+     deallocate(gribm)
 
+     ! Extract just the field (same result).
+     extract = .true.
+     call getgb2rp2(lugb, idxver, cbuf, extract, gribm, leng8, iret)
+     print *, 'leng8 ', leng8
+     if (leng8 .ne. 11183) stop 110
+     ! Deallocate buffer that got GRIB message.
+     deallocate(gribm)
+
+     ! Deallocate the buffer that holds index.
+     deallocate(cbuf)
+
+     call baclose(lugb, iret)
+     if (iret .ne. 0) stop 199
+
+  end do
   print *, 'SUCCESS!...'
 
 end program test_getgb2rp2
