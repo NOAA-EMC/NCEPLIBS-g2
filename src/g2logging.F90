caa7f87a6dbb159057136d5839eea730bb19f4e7
Edward Hartnett <edward.hartnett@noaa.gov>
2024-05-15 02:59:11 -0600

    more logging

--- src/g2logging.F90
+++ src/g2logging.F90
@@ -7,9 +7,15 @@
 !> @author Edward Hartnett @date 5/15/24
 module g2logging
   integer g2_log_level !< 0 for no logging.
-  character* 120 g2_log_msg !< For messages.
+  character* 120 g2_log_msg !< Fill this with the logging message.
 
 contains
+  !> Print a debug message for the g2 library.
+  !>
+  !> @param[in] level If this is lower or equal to the current setting
+  !> of g2_log_level, print this log message.
+  !>
+  !> @author Edward Hartnett @date 5/15/24
   subroutine g2_log(level)
     integer, intent(in) :: level
 
