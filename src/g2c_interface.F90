fb5b7967f9639b3ac2721d856b9da880c70dbc49
Edward Hartnett <edward.hartnett@noaa.gov>
2024-06-14 01:17:45 -0600

    adding open_index

--- src/g2c_interface.F90
+++ src/g2c_interface.F90
@@ -15,11 +15,18 @@ module g2c_interface
        integer(c_int) :: g2c_open
      end function g2c_open
 
+     function g2c_open_index(data_file, index_file, mode, g2cid) bind(c)
+       use iso_c_binding
+       character(kind=c_char), intent(in)  :: data_file(*), index_file(*)
+       integer(c_int), value :: mode
+       integer(c_int), intent(out) :: g2cid
+       integer(c_int) :: g2c_open_index
+     end function g2c_open_index
+
      function g2c_close(g2id) result(status)
        use iso_c_binding
        integer(c_int), intent(in) :: g2id
        integer(c_int) :: status
      end function g2c_close
-     
   end interface
 end module g2c_interface
