fbca471bb7caf94f2f1ec8e041b8d29010a86ad2
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-08-08 10:23:43 -0700

    remove debug print statement

--- tests/test_pdstemplates.F90
+++ tests/test_pdstemplates.F90
@@ -266,8 +266,6 @@ program test_pdstemplates
   list(24) = 2
   call extpdstemplate(12, list, nummap, map)
   if (nummap .ne. 37) stop 112
-  print *, map
-  print *, exp_extmap12
   do i = 1, nummap
     if (map(i) .ne. exp_extmap12(i)) stop 113
   end do
