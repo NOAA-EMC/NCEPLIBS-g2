9a8152e1a4e9da9abbe765df3561145352a0356b
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-09-24 12:33:08 -0700

    debug

--- spack/package.py
+++ spack/package.py
@@ -46,7 +46,7 @@ class G2(CMakePackage):
     depends_on("jasper@:2.0.32", when="@:3.4.7")
     depends_on("jasper")
     depends_on("g2c", when="@develop")
-    depends_on("g2c +aec", when="+aec")
+    depends_on("g2c@develop +aec", when="+aec")
     depends_on("libpng")
     depends_on("zlib-api")
     depends_on("bacio", when="@3.4.6:")
