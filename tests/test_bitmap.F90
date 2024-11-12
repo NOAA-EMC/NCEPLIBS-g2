77d3b41b66a7358ab2f5d049dd57f6f6efa99a5d
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-11-12 13:11:51 -0800

    Update test_bitmap.F90

--- tests/test_bitmap.F90
+++ tests/test_bitmap.F90
@@ -10,13 +10,56 @@ program test_bitmap
 
   character(*) :: BITMAP_FILE
   parameter(BITMAP_FILE = 'data/ref_png_bitmap.png')
+  character(*) :: BITMAP_FILE_INDEX
+  parameter(BITMAP_FILE_INDEX = 'test_bitmap_index.grb2index')
 
-  integer :: LUGB, LUGI
-  parameter(LUGB = 3, LUGI = 4)
-  integer :: iret
+  integer :: lugi, lugb
+  parameter(lugi = 11, lugb = 10)
 
-  call baopenr(LUGI, BITMAP_FILE, iret)
+  character(len=1), pointer, dimension(:) :: cbuf(:)
+  integer :: idxver, myidxver, nlen, nnum, ifldnum, iret
+  type(gribfield) :: gfld
+
+  interface
+    subroutine getg2i2(lugi, cbuf, idxver, nlen, nnum, iret)
+      integer, intent(in) :: lugi
+      character(len=1), pointer, dimension(:) :: cbuf
+      integer, intent(out) :: idxver, nlen, nnum, iret
+    end subroutine getg2i2
+    subroutine g2_create_index(lugb, lugi, idxver, filename, iret)
+      integer, intent(in) :: lugb, lugi, idxver
+      character*(*) :: filename
+      integer, intent(out) :: iret
+    end subroutine g2_create_index
+  end interface
+
+  ! Open GRIB2 file for reading.
+  call baopenr(lugb, BITMAP_FILE, iret)
   if (iret .ne. 0) stop 3
+
+  ! Open output file where index will be written.
+  call baopen(lugi, BITMAP_FILE_INDEX, iret)
+  if (iret .ne. 0) stop 4
+
+  call g2_create_index(lugb, lugi, idxver, BITMAP_FILE, iret)
+  if (iret .ne. 0) stop 5
+
+  call baclose(lugb, iret)
+  if (iret .ne. 0) stop 6
+
+  ! Read the index file.
+  call getg2i2(lugi, cbuf, myidxver, nlen, nnum, iret)
+  if (iret .ne. 0) stop 7
+
+  call baclose(lugi, iret)
+  if (iret .ne. 0) stop 8
+
+  ifldnum = 6
+  call gf_getfld(cbuf, nlen, ifldnum, .true., .true., gfld, iret)
+  if (iret .ne. 0) stop 9
+
+  ! Free resources.
+  call gf_free(gfld)
 
   print *, 'SUCCESS!'
 end program test_bitmap
