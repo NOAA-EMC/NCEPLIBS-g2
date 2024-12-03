1f7a65800a0ecaafab203db7065e707fa92c90dc
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2024-12-03 12:09:58 -0800

    Update test_bitmap.F90

--- tests/test_bitmap.F90
+++ tests/test_bitmap.F90
@@ -53,24 +53,30 @@ program test_bitmap
        jgdt, unpack, idxver, k, gfld, iret)
   if (iret .ne. 0) stop 10
   if (k .ne. 1) stop 11
-  !if (gfld%version .ne. 2 .or. gfld%discipline .ne. 0 .or. gfld%idsectlen .ne. 13 .or. &
-  !     gfld%locallen .ne. 0 .or. gfld%ifldnum .ne. 1 .or. gfld%griddef .ne. 0 .or. &
-  !     gfld%ngrdpts .ne. 16600320 .or. gfld%numoct_opt .ne. 0 .or. gfld%interp_opt .ne. 0 .or. &
-  !     gfld%num_opt .ne. 0 .or. gfld%igdtnum .ne. 0 .or. gfld%igdtlen .ne. 19 .or. &
-  !     gfld%ipdtnum .ne. 0 .or. gfld%ipdtlen .ne. 15 .or. gfld%ndpts .ne. 16600303 .or. &
-  !     gfld%idrtnum .ne. 0 .or. gfld%idrtlen .ne. 5 .or. gfld%unpacked .neqv. .false. .or. &
-  !     gfld%expanded .neqv. .true. .or. gfld%ibmap .ne. 0) stop 12
+  if (gfld%version .ne. 2 .or. gfld%discipline .ne. 0 .or. gfld%idsectlen .ne. 13 .or. &
+       gfld%locallen .ne. 0 .or. gfld%ifldnum .ne. 1 .or. gfld%griddef .ne. 0 .or. &
+       gfld%ngrdpts .ne. 36391 .or. gfld%numoct_opt .ne. 0 .or. gfld%interp_opt .ne. 0 .or. &
+       gfld%num_opt .ne. 0 .or. gfld%igdtnum .ne. 0 .or. gfld%igdtlen .ne. 19 .or. &
+       gfld%ipdtnum .ne. 0 .or. gfld%ipdtlen .ne. 15 .or. gfld%ndpts .ne. 11041 .or. &
+       gfld%idrtnum .ne. 40 .or. gfld%idrtlen .ne. 7 .or. gfld%unpacked .neqv. .false. .or. &
+       gfld%expanded .neqv. .true. .or. gfld%ibmap .ne. 0) stop 12
+  do i = 1, 13
+    if (gfld%idsect(i) .ne. expected_idsect(i)) stop 13
+  end do
+  do i = 1, 19
+    if (gfld%igdtmpl(i) .ne. expected_igdtmpl(i)) stop 14
+  end do
+  do i = 1, 15
+    if (gfld%ipdtmpl(i) .ne. expected_ipdtmpl(i)) then
+        print *, 'got gfld%ipdtmpl', gfld%ipdtmpl
+        print *, 'expected ', expected_ipdtmpl
+        stop 15
+    endif
+  end do
+  do i = 1, 7
+    if (gfld%idrtmpl(i) .ne. expected_idrtmpl(i)) stop 16
+  end do
 
-  print *, "version ", gfld%version
-  print *, "locallen ", gfld%locallen
-  print *, "ifldnum ", gfld%ifldnum
-  print *, "griddef ", gfld%griddef
-  print *, "ngrdpts ", gfld%ngrdpts
-  print *, "numoct_opt ", gfld%numoct_opt
-  print *, "interp_opt ", gfld%interp_opt
-  print *, "num_opt ", gfld%num_opt
-  print *, "ibmap ", gfld%ibmap
-  print *, "unpacked ", gfld%unpacked
   call baclose(lugb, iret)
   if (iret .ne. 0) stop 100
   call baclose(lugi, iret)
