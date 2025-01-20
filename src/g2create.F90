cb07339131bd4baf81112e72d906d840a4bfc6d9
Edward Hartnett <edward.hartnett@noaa.gov>
2025-01-20 04:47:51 -0700

    fixed warning

--- src/g2create.F90
+++ src/g2create.F90
@@ -443,7 +443,7 @@ subroutine addfield(cgrib, lcgrib, ipdsnum, ipdstmpl, ipdstmplen, &
   elseif (idrsnum.eq.2.or.idrsnum.eq.3) then ! Complex Packing
      call cmplxpack(pfld, ndpts, idrsnum, idrstmpl, cpack, lcpack)
   elseif (idrsnum.eq.50) then ! Sperical Harmonic Simple Packing
-     call simpack(pfld(2), ndpts-1, idrstmpl, cpack, lcpack)
+     call simpack(pfld(2:), ndpts-1, idrstmpl, cpack, lcpack)
      tmpfld(1) = real(pfld(1), 4)
      call mkieee(tmpfld, tmpre00, 1) ! ensure RE(0,0) value is IEEE format
      re00 = tmpre00(1)
