9f1b1d165ba36aa493fbe14f1fc79518f29fe709
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-01-21 16:05:12 -0800

    testing

--- src/pdstemplates.F90
+++ src/pdstemplates.F90
@@ -128,7 +128,22 @@ contains
     integer :: iret, i, extlen, ext(200)
     logical :: needext
 
-    iret = g2c_get_pds_template(number, nummap, NULL(), needext)
+    iret = g2c_get_pdt_len(number, nummap)
+
+    if (iret .ne. 0) return
+    
+    call getpdstemplate(number, nummap, map(1:nummap), needext, iret)
+
+    if (iret .ne. 0) return
+    if (.not. needext) return
+
+    iret = g2c_get_pds_template_extension(number, list, extlen, map(nummap+1:))
+    !map(1:nummap) = temp_map(1:nummap)
+    nummap = nummap + extlen
+
+    !do i=1,extlen
+    !  map(nummap+i) = ext(i)
+    !enddo
 
   end subroutine extpdstemplate
 
