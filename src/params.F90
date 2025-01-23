379bdc82a4dd5a2f895b74e0a837c20fc00a9391
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-01-23 12:13:17 -0800

    Update params.F90

--- src/params.F90
+++ src/params.F90
@@ -96,10 +96,7 @@ contains
     integer :: iret
 
     iret = g2c_param_abbrev(g2disc, g2cat, g2num, param_get_abbrev)
-    
-    if (iret .ne. 0) then
-      param_get_abbrev = 'UNKNOWN '
-    endif
+
   end function param_get_abbrev
 
   !> This subroutine returns the GRIB 1 parameter number for
