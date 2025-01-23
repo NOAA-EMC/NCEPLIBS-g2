171cb576a8d9c4d0086ba5b3fe6b4342d32aa88e
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-01-23 14:18:33 -0800

    Update params.F90

--- src/params.F90
+++ src/params.F90
@@ -74,12 +74,12 @@ contains
 
     iret = g2c_param_g1tog2(g1val, g1ver, g2disc, g2cat, g2num)
 
-    print *, "iret ", iret
-    print *, "g2num", g2num
     if (iret .ne. 0) then
       print *, 'param_g1_to_g2:GRIB1 param ', g1val, ' not found.', &
           ' for table version ', g1ver
     end if
+
+    print *, "g2num", g2num
 
   end subroutine param_g1_to_g2
 
