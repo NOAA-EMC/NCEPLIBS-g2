fc31b37baaa0687a35ab4ec0e1c61f19e18d7f90
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-01-29 12:05:13 -0800

    cut off top 40 bytes in simple packing comparison

--- test_utils/run_cnvgrib_tests.sh
+++ test_utils/run_cnvgrib_tests.sh
@@ -46,8 +46,9 @@ cmp -i 120 test_gdaswave.t00z.wcoast.0p16.f000_2.grib2.idx data/ref_gdaswave.t00
 # Create an index of the new GRIB2 file.
 ../utils/grb2index 1 test_gdaswave.t00z.wcoast.0p16.f000_simple.grib2 test_gdaswave.t00z.wcoast.0p16.f000_simple.grib2.idx
 
-# Check against expected output. 
-cmp test_gdaswave.t00z.wcoast.0p16.f000_simple.grib2.idx data/ref_gdaswave.t00z.wcoast.0p16.f000_simple.grib2.idx
+# Check against expected output. First 40 bytes contain differences,
+# so ignore them.
+cmp -i 40 test_gdaswave.t00z.wcoast.0p16.f000_simple.grib2.idx data/ref_gdaswave.t00z.wcoast.0p16.f000_simple.grib2.idx
 
 echo "*** SUCCESS!"
 exit 0
