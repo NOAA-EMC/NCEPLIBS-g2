20ff8b03021571cefc0b5e8833dda2d834f01309
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-01-29 13:31:30 -0800

    more conversion tests

--- test_utils/run_cnvgrib_tests.sh
+++ test_utils/run_cnvgrib_tests.sh
@@ -63,5 +63,25 @@ cmp -i 120 test_gdaswave.t00z.wcoast.0p16.f000_simple.grib2.idx data/ref_gdaswav
 # so ignore them.
 cmp -i 120 test_gdaswave.t00z.wcoast.0p16.f000_complex.grib2.idx data/ref_gdaswave.t00z.wcoast.0p16.f000_complex.grib2.idx
 
+# Convert test file to another GRIB2 file with complex packing (1st order diffs).
+../utils/cnvgrib -g22 -p31 data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave.t00z.wcoast.0p16.f000_complex1.grib2
+
+# Create an index of the new GRIB2 file.
+../utils/grb2index 1 test_gdaswave.t00z.wcoast.0p16.f000_complex1.grib2 test_gdaswave.t00z.wcoast.0p16.f000_complex1.grib2.idx
+
+# Check against expected output. First 120 bytes contain differences,
+# so ignore them.
+cmp -i 120 test_gdaswave.t00z.wcoast.0p16.f000_complex1.grib2.idx data/ref_gdaswave.t00z.wcoast.0p16.f000_complex1.grib2.idx
+
+# Convert test file to another GRIB2 file with complex packing (2nd order diffs).
+../utils/cnvgrib -g22 -p32 data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave.t00z.wcoast.0p16.f000_complex2.grib2
+
+# Create an index of the new GRIB2 file.
+../utils/grb2index 1 test_gdaswave.t00z.wcoast.0p16.f000_complex1.grib2 test_gdaswave.t00z.wcoast.0p16.f000_complex2.grib2.idx
+
+# Check against expected output. First 120 bytes contain differences,
+# so ignore them.
+cmp -i 120 test_gdaswave.t00z.wcoast.0p16.f000_complex2.grib2.idx data/ref_gdaswave.t00z.wcoast.0p16.f000_complex2.grib2.idx
+
 echo "*** SUCCESS!"
 exit 0
