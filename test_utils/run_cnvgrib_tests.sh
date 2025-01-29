f56666a0f450f5f43ed91c9b07d6f1a3c6e0cd38
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-01-29 13:37:17 -0800

    conversion test with png

--- test_utils/run_cnvgrib_tests.sh
+++ test_utils/run_cnvgrib_tests.sh
@@ -77,11 +77,21 @@ cmp -i 120 test_gdaswave.t00z.wcoast.0p16.f000_complex1.grib2.idx data/ref_gdasw
 ../utils/cnvgrib -g22 -p32 data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave.t00z.wcoast.0p16.f000_complex2.grib2
 
 # Create an index of the new GRIB2 file.
-../utils/grb2index 1 test_gdaswave.t00z.wcoast.0p16.f000_complex1.grib2 test_gdaswave.t00z.wcoast.0p16.f000_complex2.grib2.idx
+../utils/grb2index 1 test_gdaswave.t00z.wcoast.0p16.f000_complex2.grib2 test_gdaswave.t00z.wcoast.0p16.f000_complex2.grib2.idx
 
 # Check against expected output. First 120 bytes contain differences,
 # so ignore them.
 cmp -i 120 test_gdaswave.t00z.wcoast.0p16.f000_complex2.grib2.idx data/ref_gdaswave.t00z.wcoast.0p16.f000_complex2.grib2.idx
+
+# Convert test file to another GRIB2 file with png packing.
+../utils/cnvgrib -g22 -p41 data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave.t00z.wcoast.0p16.f000_png.grib2
+
+# Create an index of the new GRIB2 file.
+../utils/grb2index 1 test_gdaswave.t00z.wcoast.0p16.f000_png.grib2 test_gdaswave.t00z.wcoast.0p16.f000_png.grib2.idx
+
+# Check against expected output. First 120 bytes contain differences,
+# so ignore them.
+cmp -i 120 test_gdaswave.t00z.wcoast.0p16.f000_png.grib2.idx data/ref_gdaswave.t00z.wcoast.0p16.f000_png.grib2.idx
 
 echo "*** SUCCESS!"
 exit 0
