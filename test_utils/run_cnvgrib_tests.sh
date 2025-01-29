93a3ab499d7e74b47f80f700238c95509ccdcc74
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-01-29 11:58:56 -0800

    more tests for cnvgrib

--- test_utils/run_cnvgrib_tests.sh
+++ test_utils/run_cnvgrib_tests.sh
@@ -2,19 +2,23 @@
 # This is a test script for the NCEPLIBS-grib_util project.
 #
 # Ed Hartnett, 12/25/21
+# Alyson Stahl, 1/29/25
 
 set -e
 echo ""
 echo "*** Running cnvgrib test"
 
+# Run with zero arguments
+../utils/cnvgrib && exit 2
+
+# Run with incorrect number of arguments
+../utils/cnvgrib -g21 data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 && exit 2
+
+# Invalid input file
+../utils/cnvgrib -g21 - test_gdaswave.t00z.wcoast.0p16.f000.grib1 && exit 3
+
 # Convert test file to GRIB1.
 ../utils/cnvgrib -g21 data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave.t00z.wcoast.0p16.f000.grib1
-
-# Generate an inventory of the GRIB1 file.
-#../src/wgrib/wgrib test_gdaswave.t00z.wcoast.0p16.f000.grib1 &> test_gdaswave.t00z.wcoast.0p16.f000.grib1.inventory.txt
-
-# Check against expected output.
-#cmp test_gdaswave.t00z.wcoast.0p16.f000.grib1.inventory.txt data/ref_gdaswave_grib1_inventory.txt
 
 # Convert GRIB1 output back to GRIB2.
 ../utils/cnvgrib -g12 test_gdaswave.t00z.wcoast.0p16.f000.grib1 test_gdaswave.t00z.wcoast.0p16.f000.grib2
@@ -35,6 +39,15 @@ cmp -i 120 test_gdaswave.t00z.wcoast.0p16.f000.grib2.idx data/ref_gdaswave.t00z.
 # Check against expected output. First 120 bytes contain differences,
 # so ignore them.
 cmp -i 120 test_gdaswave.t00z.wcoast.0p16.f000_2.grib2.idx data/ref_gdaswave.t00z.wcoast.0p16.f000_2.grib2.idx
+
+# Convert test file to another GRIB2 file with simple packing.
+../utils/cnvgrib -g22 -p0 data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave.t00z.wcoast.0p16.f000_simple.grib2
+
+# Create an index of the new GRIB2 file.
+../utils/grb2index 1 test_gdaswave.t00z.wcoast.0p16.f000_simple.grib2 test_gdaswave.t00z.wcoast.0p16.f000_simple.grib2.idx
+
+# Check against expected output. 
+cmp test_gdaswave.t00z.wcoast.0p16.f000_simple.grib2.idx data/ref_gdaswave.t00z.wcoast.0p16.f000_simple.grib2.idx
 
 echo "*** SUCCESS!"
 exit 0
