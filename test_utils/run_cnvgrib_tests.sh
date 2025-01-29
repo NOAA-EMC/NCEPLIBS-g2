ebabb807c5acf16cc8b74d3226aae3108a013b4f
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-01-29 12:54:43 -0800

    test conversion to complex packing

--- test_utils/run_cnvgrib_tests.sh
+++ test_utils/run_cnvgrib_tests.sh
@@ -53,5 +53,15 @@ cmp -i 120 test_gdaswave.t00z.wcoast.0p16.f000_2.grib2.idx data/ref_gdaswave.t00
 # so ignore them.
 cmp -i 120 test_gdaswave.t00z.wcoast.0p16.f000_simple.grib2.idx data/ref_gdaswave.t00z.wcoast.0p16.f000_simple.grib2.idx
 
+# Convert test file to another GRIB2 file with complex packing.
+../utils/cnvgrib -g22 -p2 data/ref_gdaswave.t00z.wcoast.0p16.f000.grib2 test_gdaswave.t00z.wcoast.0p16.f000_complex.grib2
+
+# Create an index of the new GRIB2 file.
+../utils/grb2index 1 test_gdaswave.t00z.wcoast.0p16.f000_complex.grib2 test_gdaswave.t00z.wcoast.0p16.f000_complex.grib2.idx
+
+# Check against expected output. First 120 bytes contain differences,
+# so ignore them.
+cmp -i 120 test_gdaswave.t00z.wcoast.0p16.f000_complex.grib2.idx data/ref_gdaswave.t00z.wcoast.0p16.f000_complex.grib2.idx
+
 echo "*** SUCCESS!"
 exit 0
