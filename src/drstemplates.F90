75697afea6fbdfdcc6734d1aff03b46c4cc39e80
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-04-10 14:19:32 -0700

    use g2c drs templates

--- src/drstemplates.F90
+++ src/drstemplates.F90
@@ -29,110 +29,28 @@
 !> store the data value in this case would be the absolute value of
 !> the negative value in mapdrs.
 !>
+!> ### Program History Log
+!> Date | Programmer | Comments
+!> -----|------------|---------
+!> 2003-04-03 | Gilbert | Initial
+!> 2025-04-10 | Stahl | Replaced subroutines w/ call to g2c, removed template array
+!>
 !> @author Stephen Gilbert @date 2001-04-03
 module drstemplates
   implicit none
 
-  integer, parameter :: MAXLEN = 200 !< maximum number of octets in mapdrs
-  integer, parameter :: MAXTEMP = 9 !< maximum number of entries in the template
-  integer :: j !< loop iterator
-
-  !> This type holds information about a DRS template.
-  type drstemplate
-     integer :: template_num !< Template number.
-     integer :: mapdrslen !< The number of entries in the template.
-     integer, dimension(MAXLEN) :: mapdrs !< Number of octets in which to pack each value.
-     logical :: needext !< Does template need to be extended?
-  end type drstemplate
-
-  type(drstemplate), dimension(MAXTEMP) :: templates !< template in type of drstemplate
-
-  data templates(1)%template_num /0/     !<     Simple Packing
-  data templates(1)%mapdrslen /5/
-  data templates(1)%needext /.false./
-  data (templates(1)%mapdrs(j), j = 1, 5)  &
-       /4,-2,-2,1,1/
-
-  data templates(2)%template_num /2/     !<     Complex Packing
-  data templates(2)%mapdrslen /16/
-  data templates(2)%needext /.false./
-  data (templates(2)%mapdrs(j), j = 1, 16) &
-       /4, -2, -2, 1, 1, 1, 1, 4, 4, 4, 1, 1, 4, 1, 4, 1/
-
-  data templates(3)%template_num /3/     !<     Complex Packing - Spatial Diff
-  data templates(3)%mapdrslen /18/
-  data templates(3)%needext /.false./
-  data (templates(3)%mapdrs(j), j = 1, 18) &
-       /4, -2, -2, 1, 1, 1, 1, 4, 4, 4, 1, 1, 4, 1, 4, 1, 1, 1/
-
-  data templates(4)%template_num /50/     !<     Simple Packing - Spectral Data
-  data templates(4)%mapdrslen /5/
-  data templates(4)%needext /.false./
-  data (templates(4)%mapdrs(j), j = 1, 5) &
-       /4, -2, -2, 1, 4/
-
-  data templates(5)%template_num /51/    !<     Complex Packing - Spectral Data
-  data templates(5)%mapdrslen /10/
-  data templates(5)%needext /.false./
-  data (templates(5)%mapdrs(j), j = 1, 10) &
-       /4, -2, -2, 1, -4, 2, 2, 2, 4, 1/
-
-  data templates(6)%template_num /40000/     !<     JPEG2000 Encoding
-  data templates(6)%mapdrslen /7/
-  data templates(6)%needext /.false./
-  data (templates(6)%mapdrs(j), j = 1, 7)  &
-       /4, -2, -2, 1, 1, 1, 1/
-
-  data templates(7)%template_num /40010/     !<     PNG Encoding
-  data templates(7)%mapdrslen /5/
-  data templates(7)%needext /.false./
-  data (templates(7)%mapdrs(j), j = 1, 5)  &
-       /4, -2, -2, 1, 1/
-
-  data templates(8)%template_num /40/     !<     JPEG2000 Encoding
-  data templates(8)%mapdrslen /7/
-  data templates(8)%needext /.false./
-  data (templates(8)%mapdrs(j), j = 1, 7)  &
-       /4, -2, -2, 1, 1, 1, 1/
-
-  data templates(9)%template_num /41/     !<     PNG Encoding
-  data templates(9)%mapdrslen /5/
-  data templates(9)%needext /.false./
-  data (templates(9)%mapdrs(j), j = 1, 5)  &
-       /4, -2, -2, 1, 1/
-
-  !         data templates(5)%template_num /1/      !<     Simple Packing - Matrix
-  !         data templates(5)%mapdrslen /15/
-  !         data templates(5)%needext /.true./
-  !         data (templates(5)%mapdrs(j),j=1,15) &
-  !                                /4,-2,-2,1,1,1,4,2,2,1,1,1,1,1,1/
+  interface
+     function g2c_get_drs_template(number, nummap, map, needext) bind(c)
+      use, intrinsic :: iso_c_binding
+      integer(c_int), value, intent(in) :: number
+      integer(c_int), intent(out) :: nummap
+      integer(c_int), intent(out) :: map(*)
+      integer(c_int), intent(out) :: needext
+      integer(c_int) :: g2c_get_drs_template
+     end function g2c_get_drs_template
+  end interface
 
 contains
-
-  !> Return the index of specified Data Representation Template in
-  !> array templates.
-  !>
-  !> @param[in] number the Data Representation Template number.
-  !>
-  !> @return Index of DRT 5.NN in array templates, if template exists.
-  !> = -1, otherwise.
-  !>
-  !> @author Stephen Gilbert @date 2001-06-28
-  integer function getdrsindex(number)
-    implicit none
-
-    integer, intent(in) :: number
-    integer :: j
-
-    getdrsindex = -1
-
-    do j = 1, MAXTEMP
-       if (number .eq. templates(j)%template_num) then
-          getdrsindex = j
-          return
-       endif
-    enddo
-  end function getdrsindex
 
   !> Return DRS template information for a specified Data
   !> Representation Template.
@@ -154,28 +72,19 @@ contains
   !>
   !> @author Stephen Gilbert @date 2000-05-11
   subroutine getdrstemplate(number, nummap, map, needext, iret)
+
+    use, intrinsic :: iso_c_binding
     implicit none
 
     integer, intent(in) :: number
     integer, intent(out) :: nummap, map(*), iret
     logical, intent(out) :: needext
-    integer :: index
+    integer :: needext_int
 
-    iret = 0
+    iret = g2c_get_drs_template(number, nummap, map, needext_int)
 
-    index = getdrsindex(number)
+    needext = needext_int > 0
 
-    if (index .ne. -1) then
-       nummap = templates(index)%mapdrslen
-       needext = templates(index)%needext
-       map(1 : nummap) = templates(index)%mapdrs(1 : nummap)
-    else
-       nummap = 0
-       needext = .false.
-       print *, 'getdrstemplate: DRS Template ', number, &
-            ' not defined.'
-       iret = 1
-    endif
   end subroutine getdrstemplate
 
   !> Generate the remaining octet map for a given Data
@@ -203,23 +112,15 @@ contains
 
     integer, intent(in) :: number, list(*)
     integer, intent(out) :: nummap, map(*)
-    integer :: index, N, i
+    integer :: iret
+    logical :: needext
 
-    index = getdrsindex(number)
-    if (index .eq. -1) return
+    call getdrstemplate(number, nummap, map, needext, iret)
+
+    if (iret .gt. 0) return
 
     ! No implemented DRS templates need extensions.
-    if (.not. templates(index)%needext) return
+    if (.not. needext) return
 
-    nummap = templates(index)%mapdrslen
-    map(1 : nummap) = templates(index)%mapdrs(1 : nummap)
-
-    if (number .eq. 1) then
-       N = list(11) + list(13)
-       do i = 1, N
-          map(nummap + i)=4
-       enddo
-       nummap = nummap + N
-    endif
   end subroutine extdrstemplate
 end module drstemplates
