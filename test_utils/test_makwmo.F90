044d5712f08f9e77a59bb5fba45caecae85ee133
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-05-28 16:17:23 -0700

    Update test_makwmo.F90

--- test_utils/test_makwmo.F90
+++ test_utils/test_makwmo.F90
@@ -5,11 +5,11 @@ program test_makwmo
     character(len = 6) :: BULHED = "YAAK37"
     character(len = 4) :: KWBX = "KWBB"
     integer :: IDAY = 28, IHOUR = 0, IMIN = 0
-    character(*) :: HEADER, EXP_HEADER = "YAAK37 KWBB 280000"
+    character(*21) :: HEADER
     
     call makwmo(BULHED, IDAY, IHOUR, IMIN, KWBX, HEADER)
 
-    if (trim(HEADER) .ne. EXP_HEADER) stop 10
+    if (trim(HEADER) .ne. "YAAK37 KWBB 280000") stop 10
 
   print *, 'OK!'
   print *, 'SUCCESS!'
