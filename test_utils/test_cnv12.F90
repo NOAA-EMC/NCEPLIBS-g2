a4b5e0c22350b4afb562b5b4e2dd82020249bf22
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-05-29 16:28:58 -0700

    Update test_cnv12.F90

--- test_utils/test_cnv12.F90
+++ test_utils/test_cnv12.F90
@@ -6,8 +6,8 @@ program test_cnv12
    integer, parameter :: MAXKGDS = 22, MAXIGDS = 22, MAXLEN = 200
    integer :: igds(5), igdstmpl(MAXIGDS), ideflist(1), idefnum
    integer :: kgds(MAXKGDS) = (/ 0, 1000, 1000, 1, 1, 136, 1, 1, 1, 1, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 255, 0, 0 /)
-   integer :: exp_gds_latlon(MAXIGDS) = (/ 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, &
-      1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000 /)
+   integer :: exp_gds_latlon(MAXIGDS) = (/ 0, 0, 0, 0, 0, 0, 0, 1000, 1000, 0, 0, 1000, 0, 56, 1000, 1000, 1000, &
+      1000, 1000, 0, 2, 0 /)
    integer :: exp_ngrd = 1000000, exp_ngrdi = 65535
    integer :: exp_gdt_latlon = 0, exp_gdt_merc = 10, exp_gdt_lamb = 30, exp_gdt_gauss = 40, exp_gdt_polar = 20, &
       exp_gdt_curv = 204, exp_gdt_rot = 32768, exp_gdt_rot2 = 32769 
@@ -17,9 +17,10 @@ program test_cnv12
    call gds2gdt(kgds, igds, igdstmpl, idefnum, ideflist, iret)
 
    if (iret .ne. 0) stop 11
-
+   if (igds(2) .ne. exp_ngrd) stop 21
+   if (igds(5) .ne. 0) stop 31
    do i = 1, MAXIGDS
-      print *, igdstmpl(i)
+      if (igdstmpl(i) .ne. exp_gds_latlon(i)) stop 41
    end do
 
    print *, 'SUCCESS!'
