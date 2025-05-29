a4968ecaf56bf1d80db41fdcd13f47a838cd90f1
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-05-29 15:08:42 -0700

    Create test_cnv12.F90

---
+++ test_utils/test_cnv12.F90
@@ -0,0 +1,22 @@
+! This is a test program for NCEPLIBS-grib_util.
+!
+! This program tests the cnv12 subroutines.
+program test_cnv12
+   implicit none
+   integer, parameter :: MAXKGDS = 22, MAXIGDS = 22, MAXLEN = 200
+   integer :: igds(5), igdstmpl(MAXIGDS), ideflist(1), idefnum
+   integer :: kgds(MAXKGDS) = (/ 0, 1000, 1000, 1, 1, 136, 1, 1, 1, 1, 1000, 0, 0, 0, 0, 0, 0, 0, 0, 255, 0, 0 /)
+   integer :: exp_gds_latlon(MAXIGDS) = (/ 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, &
+      1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000 /)
+   integer :: exp_ngrd = 1000000, exp_ngrdi = 65535
+   integer :: exp_gdt_latlon = 0, exp_gdt_merc = 10, exp_gdt_lamb = 30, exp_gdt_gauss = 40, exp_gdt_polar = 20, &
+      exp_gdt_curv = 204, exp_gdt_rot = 32768, exp_gdt_rot2 = 32769 
+   integer :: i, iret
+
+   print *, 'testing with Lat/lon grid...'
+   call gds2gdt(kgds, igds, igdstmpl, idefnum, ideflist, iret)
+   
+   if (iret .ne. 0) stop 10
+
+   print *, 'SUCCESS!'
+end program test_cnv12
