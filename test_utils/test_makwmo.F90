50b2e4b72ecb1d15cb10af4791de5adfc1a1a949
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-05-28 20:28:36 -0700

    add date / name

--- test_utils/test_makwmo.F90
+++ test_utils/test_makwmo.F90
@@ -1,17 +1,20 @@
 ! This is a test program for NCEPLIBS-grib_util.
 !
 ! This program tests the makwmo subroutine.
+!
+! Alyson Stahl 5/28/25
 program test_makwmo
-    character(len = 6) :: BULHED = "YAAK37"
-    character(len = 4) :: KWBX = "KWBB"
-    integer :: IDAY = 28, IHOUR = 0, IMIN = 0
-    character(len = 21) :: HEADER
+  implicit none
+  character(len = 6) :: BULHED = "YAAK37"
+  character(len = 4) :: KWBX = "KWBB"
+  integer :: IDAY = 28, IHOUR = 0, IMIN = 0
+  character(len = 21) :: HEADER
 
-    call makwmo(BULHED, IDAY, IHOUR, IMIN, KWBX, HEADER)
+  call makwmo(BULHED, IDAY, IHOUR, IMIN, KWBX, HEADER)
 
-    if (HEADER(1:6) .ne. BULHED) stop 10
-    if (HEADER(8:11) .ne. KWBX) stop 11
-    if (HEADER(13:18) .ne. "280000") stop 12
+  if (HEADER(1:6) .ne. BULHED) stop 10
+  if (HEADER(8:11) .ne. KWBX) stop 11
+  if (HEADER(13:18) .ne. "280000") stop 12
 
   print *, 'OK!'
   print *, 'SUCCESS!'
