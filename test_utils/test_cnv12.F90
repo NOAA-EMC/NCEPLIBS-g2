b4abaeecde7fba48b1f47799fe08155ee6945f46
AlysonStahl-NOAA <166434581+AlysonStahl-NOAA@users.noreply.github.com>
2025-05-29 18:15:27 -0700

    more tests

--- test_utils/test_cnv12.F90
+++ test_utils/test_cnv12.F90
@@ -39,8 +39,108 @@ program test_cnv12
    if (igds(2) .ne. exp_ngrd) stop 22
    if (igds(5) .ne. exp_gdt_merc) stop 32
    do i = 1, MAXIGDS
-      if (igdstmpl(i) .ne. exp_gds_mercator(i)) stop 42
+      print *, igdstmpl(i)
    end do
+
+   print *, 'testing with Lambert conformal grid...'
+   kgds(1) = 3
+   kgds(10) = 1000
+   kgds(14) = 1
+   kgds(15) = 1
+   igdstmpl = 0
+   call gds2gdt(kgds, igds, igdstmpl, idefnum, ideflist, iret)
+
+   if (iret .ne. 0) stop 13
+   if (igds(2) .ne. exp_ngrd) stop 23
+   if (igds(5) .ne. exp_gdt_lamb) stop 33
+   do i = 1, MAXIGDS
+      print *, igdstmpl(i)
+   end do
+
+   print *, 'testing with Gaussian grid...'
+   kgds(1) = 4
+   kgds(10) = 1000
+   kgds(12) = 0
+   kgds(13) = 0
+   kgds(14) = 0
+   kgds(15) = 0
+   igdstmpl = 0
+   call gds2gdt(kgds, igds, igdstmpl, idefnum, ideflist, iret)
+
+   if (iret .ne. 0) stop 14
+   if (igds(2) .ne. exp_ngrd) stop 24
+   if (igds(5) .ne. exp_gdt_gauss) stop 34
+   do i = 1, MAXIGDS
+      print *, igdstmpl(i)
+   end do
+
+   print *, 'testing with polar stereographic grid...'
+   kgds(1) = 5
+   igdstmpl = 0
+   call gds2gdt(kgds, igds, igdstmpl, idefnum, ideflist, iret)
+
+   if (iret .ne. 0) stop 14
+   if (igds(2) .ne. exp_ngrd) stop 24
+   if (igds(5) .ne. exp_gdt_polar) stop 34
+   do i = 1, MAXIGDS
+      print *, igdstmpl(i)
+   end do
+
+   print *, 'testing with curvilinear orthogonal grid...'
+   kgds(1) = 204
+   kgds(4) = 0
+   kgds(5) = 0
+   kgds(7) = 0
+   kgds(8) = 0
+   kgds(9) = 0
+   kgds(10) = 0
+   igdstmpl = 0
+   call gds2gdt(kgds, igds, igdstmpl, idefnum, ideflist, iret)
+
+   if (iret .ne. 0) stop 15
+   if (igds(2) .ne. exp_ngrd) stop 25
+   if (igds(5) .ne. exp_gdt_curv) stop 35
+   do i = 1, MAXIGDS
+      print *, igdstmpl(i)
+   end do
+
+   print *, 'testing with rotate lat/lon grid...'
+   kgds(1) = 203
+   kgds(4) = 1
+   kgds(5) = 1
+   kgds(7) = 1
+   kgds(8) = 1
+   kgds(9) = 1
+   kgds(10) = 1
+   igdstmpl = 0
+   call gds2gdt(kgds, igds, igdstmpl, idefnum, ideflist, iret)
+
+   if (iret .ne. 0) stop 16
+   if (igds(2) .ne. exp_ngrd) stop 26
+   if (igds(5) .ne. exp_gdt_rot) stop 36
+   do i = 1, MAXIGDS
+      print *, igdstmpl(i)
+   end do
+
+   print *, 'testing with second rotate lat/lon grid...'
+   kgds(1) = 205
+   kgds(12) = 1
+   kgds(13) = 1
+   igdstmpl = 0
+   call gds2gdt(kgds, igds, igdstmpl, idefnum, ideflist, iret)
+
+   if (iret .ne. 0) stop 17
+   if (igds(2) .ne. exp_ngrd) stop 27
+   if (igds(5) .ne. exp_gdt_rot2) stop 37
+   do i = 1, MAXIGDS
+      print *, igdstmpl(i)
+   end do
+
+   print *, 'testing with incorrect grid number...'
+   kgds(1) = 999
+   igdstmpl = 0
+   call gds2gdt(kgds, igds, igdstmpl, idefnum, ideflist, iret)
+   if (iret .ne. 1) stop 4
 
    print *, 'SUCCESS!'
 end program test_cnv12
