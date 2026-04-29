d679882db127803c89ac998dbddd8e3661129b24
Alex Richert <alexander.richert@noaa.gov>
2026-04-28 19:14:16 -0700

    add unit testing that would trigger sec6 segfault

--- tests/test_getgb2.F90
+++ tests/test_getgb2.F90
@@ -112,7 +112,7 @@ program test_getgb2
   call baopenw(2, "test_getgb2_copy.grib2", iret)
   if (iret .ne. 0) stop 100
 
-  allocate(gfld%bmap(1))
+  if (.not. associated(gfld%bmap)) allocate(gfld%bmap(1))
   call putgb2(2, gfld, iret)
   if (iret .ne. 0) stop 107
 
@@ -122,6 +122,11 @@ program test_getgb2
 
   ! Free the memory.
   call gf_free(gfld)
+
+  ! Trigger malformed-index len6 path for valgrind regression checks.
+  call run_corrupt_len6_prev_bitmap_extract("test_getgb2.grib2", iret)
+  if (iret .ne. 0) stop 530
+
   call gf_finalize(iret)
   if (iret .ne. 0) stop 5
 
@@ -131,5 +136,110 @@ program test_getgb2
 
   print *, 'OK!'
   print *, 'SUCCESS!'
+
+contains
+
+  subroutine run_corrupt_len6_prev_bitmap_extract(filename, iret)
+    use bacio_module
+    implicit none
+
+    character(*), intent(in) :: filename
+    integer, intent(out) :: iret
+
+    integer, parameter :: INT1_BITS = 8
+    integer, parameter :: INT4_BITS = 32
+    integer, parameter :: lugb_reg = 7
+    integer :: lugi_reg
+    integer :: idxver
+    integer :: nlen, nnum
+    integer :: mypos, sec6_pos
+    integer :: len1, len3, len4, len5, len6
+    integer :: iret_extract
+    logical :: extract
+    integer(kind = 8) :: leng8
+    character(len = 1), pointer, dimension(:) :: cindex
+    character(len = 1), pointer, dimension(:) :: gribm
+
+    interface
+       subroutine getidx2(lugb, lugi, idxver, cindex, nlen, nnum, iret)
+         integer, intent(in) :: lugb, lugi
+         integer, intent(inout) :: idxver
+         character(len = 1), pointer, dimension(:) :: cindex
+         integer, intent(out) :: nlen, nnum, iret
+       end subroutine getidx2
+       subroutine getgb2rp2(lugb, idxver, cindex, extract, gribm, leng8, iret)
+         integer, intent(in) :: lugb, idxver
+         character(len = 1), intent(in) :: cindex(*)
+         logical, intent(in) :: extract
+         character(len = 1), pointer, dimension(:) :: gribm
+         integer(kind = 8), intent(out) :: leng8
+         integer, intent(out) :: iret
+       end subroutine getgb2rp2
+       subroutine g2_gbytec1(input, siout, iskip, nbits)
+         character*1, intent(in) :: input(*)
+         integer, intent(inout) :: siout
+         integer, intent(in) :: iskip, nbits
+       end subroutine g2_gbytec1
+       subroutine g2_sbytec1(out, input, iskip, nbits)
+         character*1, intent(inout) :: out(*)
+         integer, intent(in) :: input
+         integer, intent(in) :: iskip, nbits
+       end subroutine g2_sbytec1
+    end interface
+
+    iret = 0
+    lugi_reg = 0
+    idxver = 1
+    extract = .true.
+    nullify(cindex)
+    nullify(gribm)
+
+    call baopenr(lugb_reg, filename, iret)
+    if (iret .ne. 0) return
+
+    call getidx2(lugb_reg, lugi_reg, idxver, cindex, nlen, nnum, iret)
+    if (iret .ne. 0) then
+       call baclose(lugb_reg, iret_extract)
+       return
+    endif
+
+    ! Parse idxver=1 record up to section 6 and corrupt its indexed length.
+    mypos = INT4_BITS
+    mypos = mypos + INT4_BITS + INT4_BITS
+    mypos = mypos + 32 * INT1_BITS
+
+    call g2_gbytec1(cindex, len1, mypos, INT4_BITS)
+    mypos = mypos + len1 * INT1_BITS
+    call g2_gbytec1(cindex, len3, mypos, INT4_BITS)
+    mypos = mypos + len3 * INT1_BITS
+    call g2_gbytec1(cindex, len4, mypos, INT4_BITS)
+    mypos = mypos + len4 * INT1_BITS
+    call g2_gbytec1(cindex, len5, mypos, INT4_BITS)
+    mypos = mypos + len5 * INT1_BITS
+    call g2_gbytec1(cindex, len6, mypos, INT4_BITS)
+    sec6_pos = mypos
+
+    ! Corrupt the len6 field to a huge value. With prior versions and a large 
+    ! bitmap, this will cause reads past buffer bounds when sec6 data is accessed.
+    ! The bitmap indicator (byte after length) determines if there's a bitmap.
+    ! Set it to indicate a bitmap exists (e.g., 254), then corrupt len6 to cause
+    ! out-of-bounds access when the bitmap is read.
+    call g2_sbytec1(cindex, 999999999, sec6_pos, INT4_BITS)
+    ! Set bitmap indicator to 254 (bitmap present) - this causes code to try
+    ! to read the (nonexistent) large bitmap based on corrupted len6
+    call g2_sbytec1(cindex, 254, sec6_pos + 5 * INT1_BITS, INT1_BITS)
+
+    ! Attempt extraction with corrupted index
+    call getgb2rp2(lugb_reg, idxver, cindex, extract, gribm, leng8, iret_extract)
+
+    if (associated(gribm)) deallocate(gribm)
+    ! cindex is owned by getidx2() internal cache and freed by gf_finalize().
+    nullify(cindex)
+    call baclose(lugb_reg, iret_extract)
+
+    ! No strict checks here; this is a valgrind-focused memory regression path.
+    iret = 0
+  end subroutine run_corrupt_len6_prev_bitmap_extract
+
 end program test_getgb2
 
